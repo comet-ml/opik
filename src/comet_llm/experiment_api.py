d5beadc01213cf993d73e0b083776883b612089f
Alexander Kuzmik <alexander.kuzmik99@gmail.com>
2023-05-14 15:14:37 +0200

    Implement experiment_api.py

---
+++ src/comet_llm/experiment_api.py
@@ -0,0 +1,22 @@
+import json
+
+from typing import Optional, Union, IO
+from comet_llm import rest_api_client
+
+
+class ExperimentAPI:
+    def __init__(self, api_key: Optional[str] = None, workspace: Optional[str] = None, project_name: Optional[str] = None):
+        self._client = rest_api_client.get(api_key)
+        self._initialize_experiment(workspace, project_name)
+    
+    def _initialize_experiment(self, workspace: Optional[str] = None, project_name: Optional[str] = None) -> None:
+        response = self._client.create_experiment(workspace, project_name)
+        content = json.loads(response.text)
+        self._experiment_key = content["experimentKey"]
+
+    def log_asset(self, file_name: str, file_data: Union[str, IO]) -> None:
+        self._client.log_experiment_asset(
+            self._experiment_key,
+            file_name=file_name,
+            file_data=file_data
+        )
---
+++ tests/unit/test_experiment_api.py
@@ -0,0 +1,43 @@
+import json
+import pytest
+import box
+from testix import *
+
+from comet_llm import experiment_api
+
+
+@pytest.fixture(autouse=True)
+def mock_imports(patch_module):
+    patch_module(experiment_api, "rest_api_client")
+
+
+def _construct(experiment_key):
+    with Scenario() as s:
+        s.rest_api_client.get("api-key") >> Fake("client_instance")
+        s.client_instance.create_experiment(
+            "the-workspace",
+            "project-name"
+        ) >> box.Box(text=json.dumps({"experimentKey": experiment_key }))
+
+        tested = experiment_api.ExperimentAPI(
+            api_key="api-key",
+            workspace="the-workspace",
+            project_name="project-name",
+        )
+        
+    return tested
+
+
+def test_log_asset():
+    tested = _construct("experiment-key")
+
+    with Scenario() as s:
+        s.client_instance.log_experiment_asset(
+            "experiment-key",
+            file_name="file-name",
+            file_data="file-data",
+        )
+        tested.log_asset(
+            file_name="file-name",
+            file_data="file-data"
+        )
