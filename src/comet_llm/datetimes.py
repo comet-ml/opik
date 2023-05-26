7037e35b41c20eec940ae45999f01c0c0746416a
Alexander Kuzmik <alexander.kuzmik99@gmail.com>
2023-05-26 11:42:11 +0200

    Add datetimes.py

---
+++ src/comet_llm/datetimes.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# *******************************************************
+#   ____                     _               _
+#  / ___|___  _ __ ___   ___| |_   _ __ ___ | |
+# | |   / _ \| '_ ` _ \ / _ \ __| | '_ ` _ \| |
+# | |__| (_) | | | | | |  __/ |_ _| | | | | | |
+#  \____\___/|_| |_| |_|\___|\__(_)_| |_| |_|_|
+#
+#  Sign up for free at https://www.comet.com
+#  Copyright (C) 2015-2023 Comet ML INC
+#  This file can not be copied and/or distributed without the express
+#  permission of Comet ML Inc.
+# *******************************************************
+
+import calendar
+import datetime
+
+
+class UTCTimestamp:
+    def __init__(self):
+        self.value = local_timestamp()
+
+
+def local_timestamp() -> int:
+    now = datetime.datetime.utcnow()
+    timestamp_in_seconds = calendar.timegm(now.timetuple()) + (now.microsecond / 1e6)
+    timestamp_in_milliseconds = int(timestamp_in_seconds * 1000)
+    return timestamp_in_milliseconds
