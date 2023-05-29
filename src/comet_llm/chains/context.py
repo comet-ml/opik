962a4e99f61c6fd6149ad5113ec4e5d2b61da3ed
Alexander Kuzmik <alexander.kuzmik99@gmail.com>
2023-05-29 10:03:42 +0200

    Rename group_stack -> context

---
+++ src/comet_llm/chains/context.py
@@ -0,0 +1,18 @@
+from . import state
+
+class ChainGroupStack:
+    def __init__(self,):
+        self._stack = []
+        self._id = state.get_new_id()
+
+    def add(self, group):
+        pass
+
+    def pop(self):
+        pass
+
+    def current_group_ids(self):
+        pass
+
+    def all_historical_groups(self):
+        pass
---
+++ tests/unit/chains/test_context.py
@@ -0,0 +1,29 @@
+import box
+import pytest
+from testix import *
+
+from comet_llm.chains import group_stack
+
+
+@pytest.fixture(autouse=True)
+def mock_imports(patch_module):
+    patch_module(group_stack, "state")
+    patch_module(group_stack, "convert")
+
+
+def _construct(id):
+    with Scenario() as s:
+        s.state.get_new_id() >> id
+        tested = group_stack.ChainGroupStack()
+    
+    return tested
+
+
+def test_as_dict__no_groups__empty_dict_returned():
+    tested = _construct("some-id")
+    assert tested.as_dict() == {}
+
+
+def test_as_dict__some_groups_added__dict_with_groups_returned():
+    tested = _construct("some-id")
+    tested.add(Fake("group1", id="the-id-1"))
