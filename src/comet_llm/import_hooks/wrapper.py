40d096875923927e6af0ed8ae0823ce8c1561803
Alexander Kuzmik <alexander.kuzmik99@gmail.com>
2023-08-03 13:21:21 +0200

    Add copyrights

--- src/comet_llm/import_hooks/wrapper.py
+++ src/comet_llm/import_hooks/wrapper.py
@@ -1,3 +1,17 @@
+# -*- coding: utf-8 -*-
+# *******************************************************
+#   ____                     _               _
+#  / ___|___  _ __ ___   ___| |_   _ __ ___ | |
+# | |   / _ \| '_ ` _ \ / _ \ __| | '_ ` _ \| |
+# | |__| (_) | | | | | |  __/ |_ _| | | | | | |
+#  \____\___/|_| |_| |_|\___|\__(_)_| |_| |_|_|
+#
+#  Sign up for free at https://www.comet.com
+#  Copyright (C) 2015-2023 Comet ML INC
+#  This file can not be copied and/or distributed without the express
+#  permission of Comet ML Inc.
+# *******************************************************
+
 import functools
 from typing import Callable
 
@@ -6,7 +20,9 @@ from . import callable_extenders, callback_runners
 
 def wrap(original: Callable, callbacks: callable_extenders.CallableExtenders):
     def wrapped(*args, **kwargs):
-        args, kwargs = callback_runners.run_before(callbacks.before, original, *args, **kwargs)
+        args, kwargs = callback_runners.run_before(
+            callbacks.before, original, *args, **kwargs
+        )
         try:
             result = original(*args, **kwargs)
         except Exception as exception:
