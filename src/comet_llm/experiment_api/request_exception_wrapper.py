40c0ed7d85671175ce45d801cd5be50bbec129a6
Aliaksandr Kuzmik <98702584+alexkuzmik@users.noreply.github.com>
2023-08-08 19:12:50 +0200

    [CM-7772] llm add a proper asset upload error message for on prem backend users (#34)
    * Implement check for on-prem in request exceptions handler
    
    * Fix lint errors
    
    * Refactor
    
    * Refactor request exception wrapper

--- src/comet_llm/experiment_api/request_exception_wrapper.py
+++ src/comet_llm/experiment_api/request_exception_wrapper.py
@@ -14,19 +14,40 @@
 
 
 import functools
-from typing import Any, Callable
+import urllib.parse
+from typing import Any, Callable, List
 
 import requests  # type: ignore
 
-from .. import exceptions
+from .. import config, exceptions
 
 
-def wrap(func: Callable) -> Callable:
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs) -> Any:  # type: ignore
-        try:
-            return func(*args, **kwargs)
-        except requests.RequestException as exception:
-            raise exceptions.CometLLMException() from exception
+def wrap(check_on_prem: bool = False) -> Callable:
+    def inner_wrap(func: Callable) -> Callable:
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs) -> Any:  # type: ignore
+            try:
+                return func(*args, **kwargs)
+            except requests.RequestException as exception:
+                exception_args: List[Any] = []
 
-    return wrapper
+                if check_on_prem:
+                    comet_url = config.comet_url()
+                    if _is_on_prem(comet_url):
+                        exception_args.append(
+                            f"Failed to send prompt to your Comet installation at "
+                            f"{comet_url}. Check that your Comet "
+                            f"installation is up-to-date and check the traceback for more details."
+                        )
+
+                raise exceptions.CometLLMException(*exception_args) from exception
+
+        return wrapper
+
+    return inner_wrap
+
+
+def _is_on_prem(url: str) -> bool:
+    parsed = urllib.parse.urlparse(url)
+    root = f"{parsed.scheme}://{parsed.hostname}/"
+    return root != "https://www.comet.com/"
