a4421f017adb08279ccb2d8b2abf82052794fb6c
Guy Avnon <guyavnon900@gmail.com>
2023-09-04 14:33:27 +0300

    added log_user_feedback and unit tests

--- src/comet_llm/logging_messages.py
+++ src/comet_llm/logging_messages.py
@@ -12,14 +12,11 @@
 #  permission of Comet ML Inc.
 # *******************************************************
 
-from . import app, logging
-from .chains.api import end_chain, start_chain
-from .chains.span import Span
-from .config import init
-from .prompts.api import log_prompt
-from .api import log_user_feedback
 
-__all__ = ["log_prompt", "start_chain", "end_chain", "Span", "init", "log_user_feedback"]
+API_KEY_NOT_FOUND_MESSAGE = """
+    CometLLM requires an API key. Please provide it as the
+    api_key argument to %s or as an environment
+    variable named COMET_API_KEY
+    """
 
-logging.setup()
-app.register_summary_print()
+NON_ALLOWED_SCORE = "Score can only be 0 or 1 when calling 'log_user_feedback'"
