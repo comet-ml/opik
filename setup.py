fdb27e5d52953dff03edbfe6dae3536ad1193c99
Aliaksandr Kuzmik <98702584+alexkuzmik@users.noreply.github.com>
2023-10-06 15:17:25 +0200

    [CM-8539] Add missing handlers init (#81)
    * Bump version 1.4.0
    
    * fix lint errors
    
    * Fix name
    
    * Bump version 1.4.1

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ setup(
     package_dir={"": "src"},
     url="https://www.comet.com",
     project_urls=project_urls,
-    version="1.4.0",
+    version="1.4.1",
     zip_safe=False,
     license="MIT",
 )
