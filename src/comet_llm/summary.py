696bf570c941456632da6b6bdf2d81db6310e68f
Aliaksandr Kuzmik <98702584+alexkuzmik@users.noreply.github.com>
2023-10-31 16:37:30 +0100

    [NA] API key error handling (#78)
    * [CM-8405] Comet LLM log prompt behavior when the api key is missing (#77)
    
    * Make Summary thread safe
    
    * Refactor summary.py, delete logs_registry
    
    * Add incremenet_failed method to Summary
    
    * Refactor failed response handler and introduce exceptions.filter
    
    * Wrap api with errors filter
    
    * Fix lint errors
    
    * Reorganize exception related modules
    
    * Fix lint error
    
    * Add log_message_once=True to api key related exception
    
    * Fix lint errors

--- src/comet_llm/summary.py
+++ src/comet_llm/summary.py
@@ -12,25 +12,36 @@
 #  LICENSE file in the root directory of this package.
 # *******************************************************
 
+import collections
 import logging
-
-from . import logs_registry
+import threading
+from typing import DefaultDict
 
 LOGGER = logging.getLogger(__name__)
 
 
 class Summary:
     def __init__(self) -> None:
-        self._registry = logs_registry.LogsRegistry()
+        self._logs_registry: DefaultDict[str, int] = collections.defaultdict(lambda: 0)
+        self._failed = 0
+        self._lock = threading.Lock()
 
     def add_log(self, project_url: str, name: str) -> None:
-        if self._registry.empty():
-            LOGGER.info("%s logged to %s", name.capitalize(), project_url)
+        with self._lock:
+            if len(self._logs_registry) == 0:
+                LOGGER.info("%s logged to %s", name.capitalize(), project_url)
 
-        self._registry.register_log(project_url)
+            self._logs_registry[project_url] += 1
+
+    def increment_failed(self) -> None:
+        with self._lock:
+            self._failed += 1
 
     def print(self) -> None:
-        registry_items = self._registry.as_dict().items()
-
-        for project, logs_amount in registry_items:
+        for project, logs_amount in self._logs_registry.items():
             LOGGER.info("%d prompts and chains logged to %s", logs_amount, project)
+
+        if self._failed > 0:
+            LOGGER.info(
+                "%d prompts and chains were not logged because of errors", self._failed
+            )
