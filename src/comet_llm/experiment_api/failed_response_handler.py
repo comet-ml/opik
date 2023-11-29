2438f547e02a9bb5f443a5647ea2a6f5a614b996
Alexander Kuzmik <alexander.kuzmik99@gmail.com>
2023-11-29 15:47:46 +0100

    Reorganize request exception handling logic

--- src/comet_llm/experiment_api/failed_response_handler.py
+++ src/comet_llm/experiment_api/failed_response_handler.py
@@ -11,18 +11,30 @@
 #  This source code is licensed under the MIT license found in the
 #  LICENSE file in the root directory of this package.
 # *******************************************************
-import json
-from typing import Optional
 
+import json
+from typing import NoReturn
+
+import collections
 import requests  # type: ignore
 
-from .. import backend_error_codes, logging_messages
+from .. import backend_error_codes, logging_messages, exceptions
 
-SDK_ERROR_CODES_LOGGING_MESSAGE = {
-    backend_error_codes.UNABLE_TO_LOG_TO_NON_LLM_PROJECT: logging_messages.UNABLE_TO_LOG_TO_NON_LLM_PROJECT
-}
+_SDK_ERROR_CODES_LOGGING_MESSAGE = collections.defaultdict(
+    lambda: logging_messages.FAILED_TO_SEND_DATA_TO_SERVER,
+    {
+        backend_error_codes.UNABLE_TO_LOG_TO_NON_LLM_PROJECT: logging_messages.UNABLE_TO_LOG_TO_NON_LLM_PROJECT
+    }
+)
 
+# def handle(response: requests.Response) -> Optional[str]:
+#     sdk_error_code = json.loads(response.text)["sdk_error_code"]
+#     return SDK_ERROR_CODES_LOGGING_MESSAGE.get(sdk_error_code)
 
-def handle(response: requests.Response) -> Optional[str]:
+def handle(exception: requests.RequestException) -> NoReturn:
+    response = exception.response
     sdk_error_code = json.loads(response.text)["sdk_error_code"]
-    return SDK_ERROR_CODES_LOGGING_MESSAGE.get(sdk_error_code)
+    error_message = _SDK_ERROR_CODES_LOGGING_MESSAGE[sdk_error_code]
+
+    raise exceptions.CometLLMException(error_message) from exception
+
---
+++ tests/unit/experiment_api/test_failed_response_handler.py
@@ -0,0 +1,21 @@
+import json
+
+import box
+import pytest
+
+from testix import *
+
+from comet_llm.exceptions import exceptions
+from comet_llm.experiment_api import failed_response_handler
+
+def test_wrap__request_exception_non_llm_project_sdk_code__log_specifc_message_in_exception():
+    exception = Exception()
+    exception.response = box.Box(text=json.dumps({"sdk_error_code": 34323}))
+
+    expected_log_message = "Failed to send prompt to the specified project as it is not an LLM project, please specify a different project name."
+
+
+    with pytest.raises(exceptions.CometLLMException) as excinfo:
+        failed_response_handler.handle(exception)
+
+    assert excinfo.value.args == (expected_log_message, )
