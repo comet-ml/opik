00ab213aa9079f15b4b7b5d9e3560061d1a42bda
Alexander Kuzmik <alexander.kuzmik99@gmail.com>
2023-12-04 12:04:21 +0100

    Fix lint errors

--- src/comet_llm/import_hooks/module_loader.py
+++ src/comet_llm/import_hooks/module_loader.py
@@ -14,7 +14,6 @@
 
 import importlib.abc
 import logging
-
 from types import ModuleType
 from typing import TYPE_CHECKING, Optional
 
@@ -24,6 +23,7 @@ if TYPE_CHECKING:  # pragma: no cover
     from importlib import machinery
 
 LOGGER = logging.getLogger(__name__)
+
 
 class CometModuleLoader(importlib.abc.Loader):
     def __init__(
@@ -51,6 +51,8 @@ class CometModuleLoader(importlib.abc.Loader):
 
         try:
             patcher.patch(module, self._module_extension)
-        except Exception as exception:
-            LOGGER.debug(f"Failed to patch {self._module_name} with extension {self._module_extension.items()}", exc_info=True)
- 
+        except Exception:
+            LOGGER.debug(
+                f"Failed to patch {self._module_name} with extension {self._module_extension.items()}",
+                exc_info=True,
+            )
