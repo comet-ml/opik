a0a6350c1bed083dc06e5704207532e1c4e90dc8
Alexander Kuzmik <alexander.kuzmik99@gmail.com>
2023-12-04 11:08:41 +0100

    Bypass cache decorator in tests

--- tests/unit/experiment_api/test_comet_api_client.py
+++ tests/unit/experiment_api/test_comet_api_client.py
@@ -19,6 +19,7 @@ def mock_rest_api_class(patch_module):
 
 def test_get__happyflow(mock_rest_api_class):
     client_instance = Fake("client")
+    tested_function_undecorated = comet_api_client.get.__wrapped__
 
     with Scenario() as s:
         s.config.comet_url() >> "comet-url"
@@ -26,12 +27,14 @@ def test_get__happyflow(mock_rest_api_class):
         s.config.tls_verification_enabled() >> True
         s.CometAPIClient("api-key", "comet-url", "the-session") >> client_instance
 
-        assert comet_api_client.get("api-key") is client_instance
+        assert tested_function_undecorated("api-key") is client_instance
 
 
 def test_get__tls_verification_disabled__set_session_property_verify_to_False(mock_rest_api_class):
     client_instance = Fake("client")
     session = box.Box()
+
+    tested_function_undecorated = comet_api_client.get.__wrapped__
 
     with Scenario() as s:
         s.config.comet_url() >> "comet-url"
@@ -39,5 +42,5 @@ def test_get__tls_verification_disabled__set_session_property_verify_to_False(mo
         s.config.tls_verification_enabled() >> False
         s.CometAPIClient("api-key", "comet-url", session) >> client_instance
 
-        assert comet_api_client.get("api-key") is client_instance
+        assert tested_function_undecorated("api-key") is client_instance
         assert session.verify is False
