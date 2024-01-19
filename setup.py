4d0830aa246780ac16bad6d7bea3e8c8317b7b02
Alexander Kuzmik <alexander.kuzmik99@gmail.com>
2024-01-19 16:58:22 +0100

    Add comet_ml version restriction

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from pathlib import Path
 from setuptools import find_packages, setup
 
 requirements = [
-    "comet_ml",
+    "comet_ml>=3.35.5",
     "dataclasses; python_version<'3.7.0'",
     "flatten-dict",
     "requests",
