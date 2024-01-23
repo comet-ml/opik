93dbd9125211d57f7da4c45dfd241a0a34bb0908
Alexander Kuzmik <alexander.kuzmik99@gmail.com>
2024-01-23 12:56:00 +0100

    Bump version 2.1.1

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ setup(
     package_dir={"": "src"},
     url="https://www.comet.com",
     project_urls=project_urls,
-    version="2.1.0",
+    version="2.1.1",
     zip_safe=False,
     license="MIT",
 )
