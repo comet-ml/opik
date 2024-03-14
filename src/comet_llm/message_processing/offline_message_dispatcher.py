17f31cfbfa12f64a8da23809ecf334d27397edbb
Aliaksandr Kuzmik <alexander.kuzmik99@gmail.com>
2024-03-14 16:02:04 +0100

    Fix lint errors, change some names

---
+++ src/comet_llm/message_processing/offline_message_dispatcher.py
@@ -0,0 +1,9 @@
+from .messages import BaseMessage
+
+class OfflineMessageProcessor:
+    def __init__(self, offline_directory: str, batch_duration_seconds: int) -> None:
+        self._offline_directory = offline_directory
+        self._batch_duration_seconds = batch_duration_seconds
+
+    def process(message: BaseMessage) -> None:
+        pass
