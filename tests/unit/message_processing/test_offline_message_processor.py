55146a70b20150bf88ba073a2d953a0687ddfad4
Aliaksandr Kuzmik <alexander.kuzmik99@gmail.com>
2024-03-22 16:07:16 +0100

    Add new test for offline message processor

--- tests/unit/message_processing/test_offline_message_processor.py
+++ tests/unit/message_processing/test_offline_message_processor.py
@@ -1,14 +1,18 @@
-import time
+import glob
+import json
+import os
+import shutil
 
 import pytest
 from testix import *
 
+from comet_llm import experiment_info
 from comet_llm.message_processing import messages, offline_message_processor
 
 NOT_USED = None
 
 
-@pytest.fixture(autouse=True)
+@pytest.fixture()
 def mock_imports(patch_module):
     patch_module(offline_message_processor, "prompt")
     patch_module(offline_message_processor, "chain")
@@ -16,7 +20,7 @@ def mock_imports(patch_module):
     patch_module(offline_message_processor, "os")
 
 
-def test_offline_message_processor__new_filename_created_because_of_time_passed():
+def test_offline_message_processor__new_filename_created_because_of_time_passed(mock_imports):
     message = messages.PromptMessage(
         experiment_info_=NOT_USED,
         prompt_asset_data=NOT_USED,
@@ -49,7 +53,7 @@ def test_offline_message_processor__new_filename_created_because_of_time_passed(
         tested.process(message)
 
 
-def test_offline_message_processor__messages_dispatched_to_correct_senders():
+def test_offline_message_processor__messages_dispatched_to_correct_senders(mock_imports):
     prompt_message = messages.PromptMessage(
         experiment_info_=NOT_USED,
         prompt_asset_data=NOT_USED,
@@ -84,3 +88,56 @@ def test_offline_message_processor__messages_dispatched_to_correct_senders():
         s.time.time() >> 0
         tested.process("some-unhandled-message-type")
 
+
+@pytest.fixture
+def offline_directory():
+    dirpath = "./test-offline-dir"
+    os.makedirs(dirpath)
+    yield dirpath
+    shutil.rmtree(dirpath)
+
+
+def test_offline_logging__prompt_and_chain_messages__happyflow(offline_directory):
+    tested = offline_message_processor.OfflineMessageProcessor(
+        offline_directory=offline_directory,
+        file_usage_duration=5,
+    )
+
+    experiment_info_ = experiment_info.get(
+        workspace="the-workspace",
+        project_name="the-project-name",
+        api_key="api-key"
+    )
+
+    prompt_message = messages.PromptMessage(
+        experiment_info_=experiment_info_,
+        prompt_asset_data={"prompt-asset-key": "prompt-asset-value"},
+        duration="prompt-duration",
+        metadata={"prompt-metadata-key": "prompt-metadata-value"},
+        tags=["prompt-tag1", "prompt-tag2"]
+    )
+
+    chain_message = messages.ChainMessage(
+        experiment_info_=experiment_info_,
+        chain_data={"chain-key": "chain-value"},
+        duration="chain-duration",
+        metadata={"chain-metadata-key": "chain-metadata-value"},
+        tags=["chain-tag1", "chain-tag2"],
+        others={"other-key": "other-value"},
+    )
+
+    tested.process(prompt_message)
+    tested.process(chain_message)
+
+    # Read messages from the disk, compare with the original ones
+    filenames = glob.glob(f"{offline_directory}/*")
+
+    for filename in filenames:
+        with open(file=filename, mode="r") as in_stream:
+            lines = in_stream.readlines()
+
+        assert len(lines) == 2
+        prompt_line, chain_line = lines[0], lines[1]
+
+        assert messages.PromptMessage.from_dict(json.loads(prompt_line)["message"], api_key="api-key") == prompt_message
+        assert messages.ChainMessage.from_dict(json.loads(chain_line)["message"], api_key="api-key") == chain_message
