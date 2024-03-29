4bb75860aad1c6e16de5b4dd325c33b5e4e2aecd
Aliaksandr Kuzmik <alexander.kuzmik99@gmail.com>
2024-03-29 20:02:11 +0100

    Disable new tests to see if CI still fails

--- tests/unit/test_config.py
+++ tests/unit/test_config.py
@@ -38,42 +38,43 @@ def test_init__not_set_arguments_not_passed_to_comet_ml_init():
         config.init(api_key="api-key")
 
 
-@pytest.fixture
-def mock_config_dict(patch_module):
-    patch_module(config, "_COMET_ML_CONFIG", collections.defaultdict(lambda: None))
+# @pytest.fixture
+# def mock_config_dict(patch_module):
+#     patch_module(config, "_COMET_ML_CONFIG", collections.defaultdict(lambda: None))
 
 
-def test_setup_comet_url_override__api_key_contains_url__url_is_not_set_in_config__url_from_api_key_set_in_config(mock_config_dict):
-    URL_FROM_API_KEY = "https://www.from.api.key.comet.com"
 
-    with Scenario() as s:
-        assert config.comet_url() == config.DEFAULT_COMET_BASE_URL
+# def test_setup_comet_url_override__api_key_contains_url__url_is_not_set_in_config__url_from_api_key_set_in_config(mock_config_dict):
+#     URL_FROM_API_KEY = "https://www.from.api.key.comet.com"
 
-        s.comet_api_key.parse_api_key("api-key") >> box.Box(base_url=URL_FROM_API_KEY)
-        config.setup_comet_url("api-key")
+#     with Scenario() as s:
+#         assert config.comet_url() == config.DEFAULT_COMET_BASE_URL
 
-        assert config.comet_url() == URL_FROM_API_KEY
+#         s.comet_api_key.parse_api_key("api-key") >> box.Box(base_url=URL_FROM_API_KEY)
+#         config.setup_comet_url("api-key")
 
-
-def test_setup_comet_url_override__api_key_contains_url__url_is_set_in_config__url_in_config_remains_the_same(mock_config_dict):
-    URL_FROM_API_KEY = "https://www.from.api.key.comet.com"
-    URL_FROM_CONFIG = "https://www.from.config.comet.com"
-    config._COMET_ML_CONFIG["comet.url_override"] = URL_FROM_CONFIG
-
-    with Scenario() as s:
-        assert config.comet_url() == URL_FROM_CONFIG
-
-        s.comet_api_key.parse_api_key("api-key") >> box.Box(base_url=URL_FROM_API_KEY)
-        config.setup_comet_url("api-key")
-
-        assert config.comet_url() == URL_FROM_CONFIG
+#         assert config.comet_url() == URL_FROM_API_KEY
 
 
-def test_setup_comet_url_override__api_key_doesnt_contain_url__default_one_is_used(mock_config_dict):
-    with Scenario() as s:
-        assert config.comet_url() == config.DEFAULT_COMET_BASE_URL
+# def test_setup_comet_url_override__api_key_contains_url__url_is_set_in_config__url_in_config_remains_the_same(mock_config_dict):
+#     URL_FROM_API_KEY = "https://www.from.api.key.comet.com"
+#     URL_FROM_CONFIG = "https://www.from.config.comet.com"
+#     config._COMET_ML_CONFIG["comet.url_override"] = URL_FROM_CONFIG
 
-        s.comet_api_key.parse_api_key("api-key") >> None
-        config.setup_comet_url("api-key")
+#     with Scenario() as s:
+#         assert config.comet_url() == URL_FROM_CONFIG
 
-        assert config.comet_url() == config.DEFAULT_COMET_BASE_URL
+#         s.comet_api_key.parse_api_key("api-key") >> box.Box(base_url=URL_FROM_API_KEY)
+#         config.setup_comet_url("api-key")
+
+#         assert config.comet_url() == URL_FROM_CONFIG
+
+
+# def test_setup_comet_url_override__api_key_doesnt_contain_url__default_one_is_used(mock_config_dict):
+#     with Scenario() as s:
+#         assert config.comet_url() == config.DEFAULT_COMET_BASE_URL
+
+#         s.comet_api_key.parse_api_key("api-key") >> None
+#         config.setup_comet_url("api-key")
+
+#         assert config.comet_url() == config.DEFAULT_COMET_BASE_URL
