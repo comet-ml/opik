c34dba9e2c8cf628909eeda487e14078367b419d
Aliaksandr Kuzmik <98702584+alexkuzmik@users.noreply.github.com>
2024-04-09 18:51:13 +0200

    [CM-10055] decouple config from comet ml one (#119)
    * Change the way comet-ml config object is created, rework imports in config.py
    
    * Fix lint errors
    
    * Fix config, move some logic to config helper
    
    * Fix imports
    
    * Fix accidentally broken test

--- tests/unit/test_config.py
+++ tests/unit/test_config.py
@@ -11,15 +11,16 @@ from comet_llm import config
 def mock_imports(patch_module):
     patch_module(config, "comet_ml")
     patch_module(config, "comet_api_key")
+    patch_module(config, "config_helper")
 
 
 
 @pytest.fixture(autouse=True)
 def mock_config_dict(patch_module):
-    patch_module(config, "_COMET_ML_CONFIG", collections.defaultdict(lambda: None))
+    patch_module(config, "_COMET_LLM_CONFIG", collections.defaultdict(lambda: None))
 
 
-def test_init__happyflow():
+def test_init__happyflow(mock_config_dict):
     with Scenario() as s:
         s.comet_ml.init(
             api_key="api-key",
@@ -27,23 +28,23 @@ def test_init__happyflow():
             project_name="the-project"
         )
         # Config object is recreated to re-read the config files
-        s.comet_ml.get_config() >> "new-config"
+        s.config_helper.create_config_instance() >> "new-config"
 
         config.init(
             api_key="api-key",
             workspace="the-workspace",
             project="the-project"
         )
-        assert config._COMET_ML_CONFIG == "new-config"
+        assert config._COMET_LLM_CONFIG == "new-config"
 
-def test_init__not_set_arguments_not_passed_to_comet_ml_init():
+def test_init__not_set_arguments_not_passed_to_comet_ml_init(mock_config_dict):
     with Scenario() as s:
         s.comet_ml.init(api_key="api-key")
         # Config object is recreated to re-read the config files
-        s.comet_ml.get_config() >> "new-config"
+        s.config_helper.create_config_instance() >> "new-config"
 
         config.init(api_key="api-key")
-        assert config._COMET_ML_CONFIG == "new-config"
+        assert config._COMET_LLM_CONFIG == "new-config"
 
 
 def test_setup_comet_url_override__api_key_contains_url__url_is_not_set_in_config__url_from_api_key_set_in_config(mock_config_dict):
@@ -61,7 +62,7 @@ def test_setup_comet_url_override__api_key_contains_url__url_is_not_set_in_confi
 def test_setup_comet_url_override__api_key_contains_url__url_is_set_in_config__url_in_config_remains_the_same(mock_config_dict):
     URL_FROM_API_KEY = "https://www.from.api.key.comet.com"
     URL_FROM_CONFIG = "https://www.from.config.comet.com"
-    config._COMET_ML_CONFIG["comet.url_override"] = URL_FROM_CONFIG
+    config._COMET_LLM_CONFIG["comet.url_override"] = URL_FROM_CONFIG
 
     with Scenario() as s:
         assert config.comet_url() == URL_FROM_CONFIG
