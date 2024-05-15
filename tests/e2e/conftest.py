71751a5131cfc9171536508c16b4fab118687e8c
Aliaksandr Kuzmik <alexander.kuzmik99@gmail.com>
2024-05-15 15:33:21 +0200

    Delete project setup fixture since it's moved to workflow file

--- tests/e2e/conftest.py
+++ tests/e2e/conftest.py
@@ -1,16 +1,9 @@
-import os
-import sys
 import comet_ml
 
 import pytest
 
 
-@pytest.fixture(autouse=True, scope="session")
-def project():
-    os.environ["COMET_PROJECT_NAME"] = f"llm-e2e-tests-setup-py{sys.version_info[2:]}"
-
-
 @pytest.fixture(scope="session")
-def comet_api(project):
+def comet_api():
     api = comet_ml.API(cache=False)
     return api
