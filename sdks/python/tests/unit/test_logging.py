79b35782ef15db99fd942d1fb698612e4044d718
Aliaksandr Kuzmik <alexander.kuzmik99@gmail.com>
2024-09-03 15:15:14 +0200

    Fix lint errors

--- sdks/python/tests/unit/test_logging.py
+++ sdks/python/tests/unit/test_logging.py
@@ -2,12 +2,18 @@ import mock
 import logging
 from opik import _logging
 
+
 def test_convert_exception_to_log_message__happy_scenario_with_original_returned_value():
     function = mock.Mock(return_value="return value")
     logger = mock.Mock(spec=["log"])
 
-    convert_exception_to_log_message_decorator = _logging.convert_exception_to_log_message(
-        "Error message", logger=logger, exception_info=True, logging_level="some-level"
+    convert_exception_to_log_message_decorator = (
+        _logging.convert_exception_to_log_message(
+            "Error message",
+            logger=logger,
+            exception_info=True,
+            logging_level="some-level",
+        )
     )
     decorated_function = convert_exception_to_log_message_decorator(function)
 
@@ -21,35 +27,44 @@ def test_convert_exception_to_log_message__exception_raised__exception_converted
     function = mock.Mock(side_effect=Exception())
     logger = mock.Mock(spec=["log"])
 
-    convert_exception_to_log_message_decorator = _logging.convert_exception_to_log_message(
-        "Error message",
-        logger=logger,
-        some_log_kwarg="some-log-kwarg",
-        return_on_exception="return_value",
-        logging_level="some-level"
+    convert_exception_to_log_message_decorator = (
+        _logging.convert_exception_to_log_message(
+            "Error message",
+            logger=logger,
+            some_log_kwarg="some-log-kwarg",
+            return_on_exception="return_value",
+            logging_level="some-level",
+        )
     )
     decorated_function = convert_exception_to_log_message_decorator(function)
 
     assert decorated_function() == "return_value"
 
     function.assert_called_once()
-    logger.log.assert_called_once_with("some-level" ,"Error message", some_log_kwarg="some-log-kwarg")
+    logger.log.assert_called_once_with(
+        "some-level", "Error message", some_log_kwarg="some-log-kwarg"
+    )
 
 
 def test_convert_exception_to_log_message__logging_level_not_set__error_level_used():
     function = mock.Mock(side_effect=Exception())
     logger = mock.Mock(spec=["log"])
 
-    convert_exception_to_log_message_decorator = _logging.convert_exception_to_log_message(
-        "Error message",
-        logger=logger,
-        some_log_kwarg="some-log-kwarg",
-        return_on_exception="return_value",
+    convert_exception_to_log_message_decorator = (
+        _logging.convert_exception_to_log_message(
+            "Error message",
+            logger=logger,
+            some_log_kwarg="some-log-kwarg",
+            return_on_exception="return_value",
+        )
     )
     decorated_function = convert_exception_to_log_message_decorator(function)
 
     assert decorated_function() == "return_value"
 
     function.assert_called_once()
-    logger.log.assert_called_once_with(logging.ERROR, "Error message", some_log_kwarg="some-log-kwarg",
-)
+    logger.log.assert_called_once_with(
+        logging.ERROR,
+        "Error message",
+        some_log_kwarg="some-log-kwarg",
+    )
