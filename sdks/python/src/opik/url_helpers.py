7ca67cd767f04ffc1a92ff7b67ffbaabf7ac41ba
Aliaksandr Kuzmik <alexander.kuzmik99@gmail.com>
2024-09-03 16:35:23 +0200

    Fix the way UI url is built

--- sdks/python/src/opik/url_helpers.py
+++ sdks/python/src/opik/url_helpers.py
@@ -1,10 +1,8 @@
 import opik.config
-import urllib.parse
 
 
 def get_ui_url() -> str:
     config = opik.config.OpikConfig()
     opik_url_override = config.url_override
 
-    parsed_url = urllib.parse.urlparse(opik_url_override)
-    return f"{parsed_url.scheme}://{parsed_url.netloc}"
+    return opik_url_override.rstrip("/api")
