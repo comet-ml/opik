01738c2d80df1fa121ab72b82c20a4e8361ed09c
liyaka <liyak@comet.com>
2024-09-03 13:27:04 +0300

    add newrelic agent

--- apps/opik-backend/entrypoint.sh
+++ apps/opik-backend/entrypoint.sh
@@ -5,7 +5,13 @@ echo $(pwd)
 jwebserver -d /opt/opik/redoc -b 0.0.0.0 -p 3003 &
 
 echo "OPIK_VERSION=$OPIK_VERSION"
+echo "NEW_RELIC_ENABLED=$NEW_RELIC_ENABLED"
+echo "NEW_RELIC_VERSION=$NEW_RELIC_VERSION"
 
+if [[ "${NEW_RELIC_ENABLED}" == "true" && "${NEW_RELIC_LICENSE_KEY}" != "" ]];then 
+    curl -o /tmp/newrelic-agent.jar https://download.newrelic.com/newrelic/java-agent/newrelic-agent/${NEW_RELIC_VERSION}/newrelic-agent-${NEW_RELIC_VERSION}.jar
+    JAVA_OPTS="$JAVA_OPTS -javaagent:/tmp/newrelic-agent.jar"
+fi
 
 # Check if ENABLE_VIRTUAL_THREADS is set to true
 if [ "$ENABLE_VIRTUAL_THREADS" = "true" ]; then
