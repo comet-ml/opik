9ee23dbcd71ead1e1ae1f640907b2650939eaf41
Aliaksandr Kuzmik <alexander.kuzmik99@gmail.com>
2024-09-10 11:43:51 +0200

    Implement new functions for updating current traces and spans

--- sdks/python/src/opik/opik_context.py
+++ sdks/python/src/opik/opik_context.py
@@ -1,6 +1,7 @@
-from typing import TYPE_CHECKING, Optional
+from typing import TYPE_CHECKING, Optional, Dict, List, Any
+from opik.types import UsageDict
 
-from . import context_storage
+from . import context_storage, dict_utils, exceptions
 
 if TYPE_CHECKING:
     from .api_objects import trace, span
@@ -25,7 +26,65 @@ def get_current_trace() -> Optional["trace.Trace"]:
     return context_storage.get_trace()
 
 
+def update_current_span(
+    input: Optional[Dict[str, Any]] = None,
+    output: Optional[Dict[str, Any]] = None,
+    metadata: Optional[Dict[str, Any]] = None,
+    tags: Optional[List[str]] = None,
+    usage: Optional[UsageDict] = None,
+) -> None:
+    """
+    Updates current span created by track() decorator or raises exception if no span was found.
+    Context-wise.
+    """
+    new_params = dict_utils.remove_none_from_dict(
+        {
+            "input": input,
+            "output": output,
+            "metadata": metadata,
+            "tags": tags,
+            "usage": usage,
+        }
+    )
+    current_span = context_storage.top_span()
+    if current_span is None:
+        raise exceptions.OpikException(
+            "There is no span in the context."
+        )  # TODO: add proper exception
+
+    current_span.update(**new_params)
+
+
+def update_current_trace(
+    input: Optional[Dict[str, Any]] = None,
+    output: Optional[Dict[str, Any]] = None,
+    metadata: Optional[Dict[str, Any]] = None,
+    tags: Optional[List[str]] = None,
+) -> None:
+    """
+    Updates current trace created by track() decorator or raises exception if no trace was found.
+    Context-wise.
+    """
+
+    new_params = dict_utils.remove_none_from_dict(
+        {
+            "input": input,
+            "output": output,
+            "metadata": metadata,
+            "tags": tags,
+        }
+    )
+    current_trace = context_storage.get_trace()
+
+    if current_trace is None:
+        raise exceptions.OpikException(
+            "There is no trace in the context."
+        )  # TODO: add proper exception
+
+    current_trace.update(**new_params)
+
+
 __all__ = [
-    "get_current_span",
-    "get_current_trace",
+    "update_current_span",
+    "update_current_trace",
 ]
