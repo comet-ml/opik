6d8d0de4df7b1bebcd67aeaf3abd899cc1dd9059
andrii.dudar <fanat.web@gmail.com>
2024-09-12 13:59:28 +0200

    [OPIK-91] [UX improvements] Implement the Experiment configuration section in the compare page

--- apps/opik-frontend/src/constants/shared.ts
+++ apps/opik-frontend/src/constants/shared.ts
@@ -1,7 +1,7 @@
 import { CELL_VERTICAL_ALIGNMENT, ROW_HEIGHT } from "@/types/shared";
 
 export const ROW_HEIGHT_MAP = {
-  [ROW_HEIGHT.small]: "h-12",
+  [ROW_HEIGHT.small]: "h-14",
   [ROW_HEIGHT.medium]: "h-[120px]",
   [ROW_HEIGHT.large]: "h-[296px]",
 };
