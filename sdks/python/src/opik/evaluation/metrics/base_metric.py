27ddaea8254358d8390ae7f2fe4c82a80cf75cbb
Jacques Verre <jverre@gmail.com>
2024-09-12 23:27:44 +0100

    Fix linter issues

--- sdks/python/src/opik/evaluation/metrics/base_metric.py
+++ sdks/python/src/opik/evaluation/metrics/base_metric.py
@@ -8,26 +8,27 @@ class BaseMetric(abc.ABC):
     """
     Abstract base class for all metrics. When creating a new metric, you should inherit
     from this class and implement the abstract methods.
-    
+
     Args:
         name: The name of the metric.
 
     Example:
         >>> from opik.evaluation.metrics import base_metric, score_result
-        >>> 
+        >>>
         >>> class MyCustomMetric(base_metric.BaseMetric):
         >>>     def __init__(self, name: str):
         >>>         self.name = name
-        >>> 
+        >>>
         >>>     def score(self, input: str, output: str, **ignored_kwargs: Any):
         >>>         # Add you logic here
-        >>> 
+        >>>
         >>>         return score_result.ScoreResult(
         >>>             value=0,
         >>>             name=self.name,
         >>>             reason="Optional reason for the score"
         >>>         )
     """
+
     def __init__(self, name: str) -> None:
         self.name = name
 
