7feeb71f5ddbd8662c584023466f737da1ff529c
Andrei Căutișanu <andreicautisanu@ip-192-168-1-141.eu-west-1.compute.internal>
2024-09-19 12:48:16 +0300

    increase timeout on ui test

--- tests_end_to_end/installer/test_app_status.py
+++ tests_end_to_end/installer/test_app_status.py
@@ -2,5 +2,5 @@ from playwright.sync_api import Page, expect
 
 def test_app_loads(page: Page):
     page.goto("http://localhost:5173/default/projects")
-    expect(page.get_by_role("link", name="comet logo")).to_be_visible()
+    expect(page.get_by_role("link", name="comet logo")).to_be_visible(timeout=10000)
 
