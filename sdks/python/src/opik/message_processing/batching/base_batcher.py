bba34184b2c30365fdbbae7c2dec6e8c3e0d7165
Aliaksandr Kuzmik <alexander.kuzmik99@gmail.com>
2024-09-20 11:24:40 +0200

    Add base batcher unit test

--- sdks/python/src/opik/message_processing/batching/base_batcher.py
+++ sdks/python/src/opik/message_processing/batching/base_batcher.py
@@ -13,18 +13,19 @@ class BaseBatcher(abc.ABC):
         max_batch_size: int,
         flush_interval: float,
     ):
-        self._flush_interval = flush_interval
-        self._flush_callback = flush_callback
+        self._flush_interval: float = flush_interval
+        self._flush_callback: Callable[[messages.BaseMessage], None] = flush_callback
         self._accumulated_messages: List[messages.BaseMessage] = []
-        self._max_batch_size = max_batch_size
+        self._max_batch_size: int = max_batch_size
 
+        self._last_time_flush_callback_called: float = 0
         self._lock = threading.RLock()
-        self._last_time_flushed: float = 0
+
 
     def add(self, message: messages.BaseMessage) -> None:
         with self._lock:
             self._accumulated_messages.append(message)
-            if self._accumulated_messages == self._max_batch_size:
+            if len(self._accumulated_messages) == self._max_batch_size:
                 self.flush()
 
     def flush(self) -> None:
@@ -34,10 +35,10 @@ class BaseBatcher(abc.ABC):
                 self._accumulated_messages = []
 
                 self._flush_callback(batch_message)
-                self._last_time_flushed = time.time()
+                self._last_time_flush_callback_called = time.time()
 
     def is_ready_to_flush(self) -> bool:
-        return (time.time() - self._last_time_flushed) >= self._flush_interval
+        return (time.time() - self._last_time_flush_callback_called) >= self._flush_interval
 
     def is_empty(self) -> bool:
         with self._lock:
