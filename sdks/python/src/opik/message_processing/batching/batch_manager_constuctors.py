ed297447bba5d1952265e07256bbb1666a5c75cd
Aliaksandr Kuzmik <alexander.kuzmik99@gmail.com>
2024-09-25 19:35:17 +0200

    Rename constant

--- sdks/python/src/opik/message_processing/batching/batch_manager_constuctors.py
+++ sdks/python/src/opik/message_processing/batching/batch_manager_constuctors.py
@@ -7,13 +7,13 @@ from . import base_batcher
 from . import create_span_message_batcher
 from . import batch_manager
 
-CREATE_SPANS_MESSAGE_BATCHER_FLUSH_INTERVAL = 1.0
+CREATE_SPANS_MESSAGE_BATCHER_FLUSH_INTERVAL_SECONDS = 1.0
 CREATE_SPANS_MESSAGE_BATCHER_MAX_BATCH_SIZE = 1000
 
 
 def create_batch_manager(message_queue: queue.Queue) -> batch_manager.BatchManager:
     create_span_message_batcher_ = create_span_message_batcher.CreateSpanMessageBatcher(
-        flush_interval_seconds=CREATE_SPANS_MESSAGE_BATCHER_FLUSH_INTERVAL,
+        flush_interval_seconds=CREATE_SPANS_MESSAGE_BATCHER_FLUSH_INTERVAL_SECONDS,
         max_batch_size=CREATE_SPANS_MESSAGE_BATCHER_MAX_BATCH_SIZE,
         flush_callback=message_queue.put,
     )
