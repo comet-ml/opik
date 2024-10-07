d41fb843229d6b0a86cb657765739e0ed8d46724
Jacques Verre <jverre@gmail.com>
2024-10-07 16:58:28 +0100

    Fix linter issues

--- sdks/python/src/opik/url_helpers.py
+++ sdks/python/src/opik/url_helpers.py
@@ -23,6 +23,7 @@ def get_experiment_url(dataset_name: str, experiment_id: str) -> str:
 
     return f'{ui_url}/{config.workspace}/experiments/{dataset_id}/compare?experiments=%5B"{experiment_id}"%5D'
 
+
 def get_projects_url() -> str:
     config = opik.config.OpikConfig()
     ui_url = get_ui_url()
