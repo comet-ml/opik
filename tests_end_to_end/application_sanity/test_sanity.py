2b5846b825eddc1810ca3e5a576ea673a4b4148b
AndreiCautisanu <30831438+AndreiCautisanu@users.noreply.github.com>
2024-10-10 14:21:47 +0300

    [OPIK-194] rest of e2e sanity tests, docstrings and architecture cleanup (#364)
    * trace details test and basic dataset test
    
    * cleanup and finishing up
    
    * changed name of job
    
    * removed on:push
    
    ---------
    
    Co-authored-by: Andrei Căutișanu <andreicautisanu@Andreis-MacBook-Pro.local>

--- tests_end_to_end/application_sanity/test_sanity.py
+++ tests_end_to_end/application_sanity/test_sanity.py
@@ -1,28 +1,31 @@
 import pytest
+import json
+
 from playwright.sync_api import Page, expect
-from page_objects.ProjectsPage import ProjectsPage
-from page_objects.TracesPage import TracesPage
+
 from page_objects.TracesPageSpansMenu import TracesPageSpansMenu
+from page_objects.IndividualDatasetPage import IndividualDatasetPage
 
 
-def test_project_name(page: Page, log_traces_and_spans_decorator, log_traces_and_spans_low_level):
-    projects_page = ProjectsPage(page)
-    projects_page.go_to_page()
-    projects_page.check_project_exists('test-project')
+def test_project_name(projects_page_timeout, log_traces_and_spans_decorator, log_traces_and_spans_low_level):
+    '''
+    Checks that the project created via the fixtures exists
+    Does a timeout of 5 seconds to wait for the traces to show up in UI for later tests (TODO: figure out a better way to do this)
+
+    1. Open projects page
+    2. Check the created project exists
+    '''
+    projects_page_timeout.check_project_exists('test-project')
 
 
-def test_traces_created(page, config, log_traces_and_spans_low_level, log_traces_and_spans_decorator):
-    #navigate to project
-    projects_page = ProjectsPage(page)
-    projects_page.go_to_page()
-    
-    #wait for data to actually arrive to the frontend
-    #TODO: replace this with a smarter waiting mechanism
-    page.wait_for_timeout(5000)
-    projects_page.click_project(config['project']['name'])
+def test_traces_created(traces_page, config, log_traces_and_spans_low_level, log_traces_and_spans_decorator):
+    '''
+    Checks that every trace defined in the sanity_config file is present in the project
 
-    #grab all traces of project
-    traces_page = TracesPage(page)
+    1. Open the traces page of the project
+    2. Grab all the names of the traces (should never set more than 15 in config so 1 page is safe)
+    3. Check that every possible name of the traces as defined in sanity_config.yaml is present in the names list
+    '''
     trace_names = traces_page.get_all_trace_names()
 
     client_prefix = config['traces']['client']['prefix']
@@ -33,16 +36,13 @@ def test_traces_created(page, config, log_traces_and_spans_low_level, log_traces
             assert prefix+str(count) in trace_names
 
 
-def test_spans_of_traces(page, config, log_traces_and_spans_low_level, log_traces_and_spans_decorator):
-    projects_page = ProjectsPage(page)
-    projects_page.go_to_page()
-    
-    #wait for data to actually arrive to the frontend
-    #TODO: replace this with a smarter waiting mechanism
-    projects_page.click_project(config['project']['name'])
-
-    #grab all traces of project
-    traces_page = TracesPage(page)
+def test_spans_of_traces(page, traces_page, config, log_traces_and_spans_low_level, log_traces_and_spans_decorator):
+    '''
+    Checks that every trace has the correct number and names of spans defined in the sanity_config.yaml file
+    1. Open the traces page of the project
+    2. Go through each trace and click it
+    3. Check that the spans are present in each trace
+    '''
     trace_names = traces_page.get_all_trace_names()
 
     for trace in trace_names:
@@ -54,3 +54,75 @@ def test_spans_of_traces(page, config, log_traces_and_spans_low_level, log_trace
             spans_menu.check_span_exists_by_name(f'{prefix}{count}')
 
 
+def test_trace_and_span_details(page, traces_page, config, log_traces_and_spans_low_level, log_traces_and_spans_decorator):
+    '''
+    Checks that for each trace and spans, the attributes defined in sanity_config.yaml are present
+    1. Go through each trace of the project
+    2. Check the created tags are present
+    3. Check the created feedback scores are present
+    4. Check the defined metadata is present
+    5. Go through each span of the traces and repeat 2-4
+    '''
+    trace_names = traces_page.get_all_trace_names()
+
+    for trace in trace_names:
+        page.get_by_text(trace).click()
+        spans_menu = TracesPageSpansMenu(page)
+        trace_type = trace.split('-')[0]
+        tag_names = config['traces'][trace_type]['tags']
+
+        for tag in tag_names:
+            spans_menu.check_tag_exists_by_name(tag)
+
+        spans_menu.get_feedback_scores_tab().click()
+
+        for score in config['traces'][trace_type]['feedback-scores']:
+            expect(page.get_by_role('cell', name=score, exact=True)).to_be_visible()
+            expect(page.get_by_role('cell', name=str(config['traces'][trace_type]['feedback-scores'][score]), exact=True)).to_be_visible()
+        
+        spans_menu.get_metadata_tab().click()
+        for md_key in config['traces'][trace_type]['metadata']:
+            expect(page.get_by_text(f'{md_key}: {config['traces'][trace_type]['metadata'][md_key]}')).to_be_visible()
+        
+        for count in range(config['spans']['count']):
+            prefix = config['spans'][trace_type]['prefix']
+            spans_menu.get_first_span_by_name(f'{prefix}{count}').click()
+
+            spans_menu.get_feedback_scores_tab().click()
+            for score in config['spans'][trace_type]['feedback-scores']:
+                expect(page.get_by_role('cell', name=score, exact=True)).to_be_visible()
+                expect(page.get_by_role('cell', name=str(config['spans'][trace_type]['feedback-scores'][score]), exact=True)).to_be_visible()
+        
+            spans_menu.get_metadata_tab().click()
+            for md_key in config['spans'][trace_type]['metadata']:
+                expect(page.get_by_text(f'{md_key}: {config['spans'][trace_type]['metadata'][md_key]}')).to_be_visible()
+
+
+def test_dataset_name(datasets_page, config, dataset):
+    '''
+    Checks that the dataset created via the fixture as defined in sanity_config.yaml is present on the datasets page
+    '''
+    datasets_page.check_dataset_exists_by_name(config['dataset']['name'])
+
+
+def test_dataset_items(page: Page, datasets_page, config, dataset_content):
+    '''
+    Checks that the traces created via the fixture and defined in sanity_dataset.jsonl are present within the dataset
+    '''
+    datasets_page.select_database_by_name(config['dataset']['name'])
+
+    individual_dataset_page = IndividualDatasetPage(page)
+    for item in dataset_content:
+        individual_dataset_page.check_cell_exists_by_text(json.dumps(item['input']).replace('{', '{ ').replace('}', ' }'))
+        individual_dataset_page.check_cell_exists_by_text(json.dumps(item['expected_output']).replace('{', '{ ').replace('}', ' }'))
+        
+
+def test_experiments_exist(experiments_page, config, create_experiments):
+    '''
+    Checks that the experiments created via the fixture are present and have the correct values for the metrics (experiments defined in a way to always return the same results)
+    '''
+    experiments_page.check_experiment_exists_by_name('test-experiment-Equals')
+    expect(experiments_page.page.get_by_text('Equals0').first).to_be_visible()
+
+    experiments_page.check_experiment_exists_by_name('test-experiment-Contains')
+    expect(experiments_page.page.get_by_text('Contains1').first).to_be_visible()
