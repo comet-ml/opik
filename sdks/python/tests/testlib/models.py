434f4eaa9e9441bca76cf2a80603fc7be316300e
Aliaksandr Kuzmik <alexander.kuzmik99@gmail.com>
2024-10-16 16:49:56 +0200

    Update default value with default factory

--- sdks/python/tests/testlib/models.py
+++ sdks/python/tests/testlib/models.py
@@ -18,7 +18,7 @@ class SpanModel:
     type: str = "general"
     usage: Optional[Dict[str, Any]] = None
     end_time: Optional[datetime.datetime] = None
-    project_name: str = ANY  # we don't want to check the project name unless it's specified explicitly in the test
+    project_name: str = dataclasses.field(default_factory=lambda: ANY)  # we don't want to check the project name unless it's specified explicitly in the test
     spans: List["SpanModel"] = dataclasses.field(default_factory=list)
     feedback_scores: List["FeedbackScoreModel"] = dataclasses.field(
         default_factory=list
@@ -36,7 +36,7 @@ class TraceModel:
     tags: Optional[List[str]] = None
     metadata: Optional[Dict[str, Any]] = None
     end_time: Optional[datetime.datetime] = None
-    project_name: str = ANY  # we don't want to check the project name unless it's specified explicitly in the test
+    project_name: str = dataclasses.field(default_factory=lambda: ANY)  # we don't want to check the project name unless it's specified explicitly in the test
     spans: List["SpanModel"] = dataclasses.field(default_factory=list)
     feedback_scores: List["FeedbackScoreModel"] = dataclasses.field(
         default_factory=list
