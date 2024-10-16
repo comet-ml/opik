a8a0a0522d332dcc1b146bf182f4317c261eadfe
Aliaksandr Kuzmik <alexander.kuzmik99@gmail.com>
2024-10-16 16:40:11 +0200

    Add e2e test for dataset item update flow

--- sdks/python/tests/e2e/test_dataset.py
+++ sdks/python/tests/e2e/test_dataset.py
@@ -1,7 +1,7 @@
 import opik
 from . import verifiers
 from opik.api_objects.dataset import dataset_item
-
+from opik.api_objects import helpers
 
 def test_create_and_populate_dataset__happyflow(
     opik_client: opik.Opik, dataset_name: str
@@ -48,6 +48,45 @@ def test_create_and_populate_dataset__happyflow(
         description=DESCRIPTION,
         dataset_items=EXPECTED_DATASET_ITEMS,
     )
+
+
+def test_insert_and_update_item__dataset_size_should_be_the_same__an_item_with_the_same_id_should_have_new_content(
+    opik_client: opik.Opik, dataset_name: str
+):
+    DESCRIPTION = "E2E test dataset"
+
+    dataset = opik_client.create_dataset(dataset_name, description=DESCRIPTION)
+
+    ITEM_ID = helpers.generate_id()
+    dataset.insert(
+        [
+            {
+                "id": ITEM_ID,
+                "input": {"question": "What is the of capital of France?"},
+            },
+        ]
+    )
+    dataset.update(
+        [
+            {
+                "id": ITEM_ID,
+                "input": {"question": "What is the of capital of Belarus?"},
+            },
+        ]
+    )
+    EXPECTED_DATASET_ITEMS = [
+        dataset_item.DatasetItem(
+            input={"question": "What is the of capital of Belarus?"},
+        ),
+    ]
+
+    verifiers.verify_dataset(
+        opik_client=opik_client,
+        name=dataset_name,
+        description=DESCRIPTION,
+        dataset_items=EXPECTED_DATASET_ITEMS,
+    )
+
 
 
 def test_deduplication(opik_client: opik.Opik, dataset_name: str):
