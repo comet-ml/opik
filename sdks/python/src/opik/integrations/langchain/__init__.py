163d41c3262ea96ceee111af2dbe7913f281a14b
Aliaksandr Kuzmik <alexander.kuzmik99@gmail.com>
2024-10-17 18:11:57 +0200

    Add missing file, move encoder extension registering to opik_tracer file

--- sdks/python/src/opik/integrations/langchain/__init__.py
+++ sdks/python/src/opik/integrations/langchain/__init__.py
@@ -1,6 +1,3 @@
 from .opik_tracer import OpikTracer
-from . import opik_encoder_extension
-
-opik_encoder_extension.register()
 
 __all__ = ["OpikTracer"]
--- sdks/python/src/opik/integrations/langchain/opik_tracer.py
+++ sdks/python/src/opik/integrations/langchain/opik_tracer.py
@@ -7,7 +7,7 @@ from opik.api_objects import opik_client, span, trace
 from opik import dict_utils
 from opik import opik_context
 
-from . import openai_run_helpers
+from . import openai_run_helpers, opik_encoder_extension
 from ...logging_messages import NESTED_SPAN_PROJECT_NAME_MISMATCH_WARNING_MESSAGE
 
 if TYPE_CHECKING:
@@ -17,6 +17,7 @@ if TYPE_CHECKING:
 
 LOGGER = logging.getLogger(__name__)
 
+opik_encoder_extension.register()
 
 def _get_span_type(run: "Run") -> Literal["llm", "tool", "general"]:
     if run.run_type in ["llm", "tool"]:
