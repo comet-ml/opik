67b66474372978202a458cf714b4bf2124f03fb7
Aliaksandr Kuzmik <alexander.kuzmik99@gmail.com>
2024-10-18 12:00:16 +0200

    Add e2e test for feedback scores specified via update_current_[span,trace]

--- sdks/python/tests/e2e/test_feedback_scores.py
+++ sdks/python/tests/e2e/test_feedback_scores.py
@@ -1,5 +1,6 @@
 from typing import List
 import opik
+from opik import opik_context
 from opik.types import FeedbackScoreDict
 from . import verifiers
 
@@ -133,4 +134,80 @@ def test_feedbacks_are_logged_via_client__happyflow(opik_client: opik.Opik):
         parent_span_id=None,
         name="span-name-1",
         feedback_scores=EXPECTED_SPAN_FEEDBACK_SCORES,
+    )
+
+
+def test_feedback_scores_added_via_update_current_span_and_trace__project_specified_manually__feedback_scores_correctly_attached_to_span_and_trace(
+    opik_client: opik.Opik,
+):
+    ID_STORAGE = {}
+
+    @opik.track
+    def f_inner():
+        ID_STORAGE["f_inner-span-id"] = opik_context.get_current_span_data().id
+        opik_context.update_current_span(
+            feedback_scores=[
+                {
+                    "name": "inner-span-feedback-score",
+                    "value": 0.25,
+                    "category_name": "span-score-category",
+                    "reason": "span-score-reason",
+                }
+            ]
+        )
+        return {"inner-output": 42}
+
+    @opik.track(project_name="manually-specified-project")
+    def f_outer():
+        ID_STORAGE["f_outer-trace-id"] = opik_context.get_current_trace_data().id
+        ID_STORAGE["f_outer-span-id"] = opik_context.get_current_span_data().id
+        f_inner()
+        opik_context.update_current_trace(
+            feedback_scores=[
+                {
+                    "name": "trace-feedback-score",
+                    "value": 0.75,
+                    "category_name": "trace-score-category",
+                    "reason": "trace-score-reason",
+                }
+            ]
+        )
+        return {"outer-output": 100}
+
+    f_outer()
+    opik.flush_tracker()
+
+    EXPECTED_INNER_SPAN_FEEDBACK_SCORES: List[FeedbackScoreDict] = [
+        {
+            "id": ID_STORAGE["f_inner-span-id"],
+            "name": "inner-span-feedback-score",
+            "value": 0.25,
+            "category_name": "span-score-category",
+            "reason": "span-score-reason",
+        },
+    ]
+
+    EXPECTED_TRACE_FEEDBACK_SCORES: List[FeedbackScoreDict] = [
+        {
+            "id": ID_STORAGE["f_outer-trace-id"],
+            "name": "trace-feedback-score",
+            "value": 0.75,
+            "category_name": "trace-score-category",
+            "reason": "trace-score-reason",
+        },
+    ]
+
+    verifiers.verify_trace(
+        opik_client=opik_client,
+        trace_id=ID_STORAGE["f_outer-trace-id"],
+        name="f_outer",
+        feedback_scores=EXPECTED_TRACE_FEEDBACK_SCORES,
+    )
+    verifiers.verify_span(
+        opik_client=opik_client,
+        span_id=ID_STORAGE["f_inner-span-id"],
+        trace_id=ID_STORAGE["f_outer-trace-id"],
+        parent_span_id=ID_STORAGE["f_outer-span-id"],
+        name="f_inner",
+        feedback_scores=EXPECTED_INNER_SPAN_FEEDBACK_SCORES,
     )
