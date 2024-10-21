68a5596f67a38806ea8bd061e0b7bd6ffa0aa5a1
Aliaksandr Kuzmik <alexander.kuzmik99@gmail.com>
2024-10-21 13:23:46 +0200

    Add unit tests for opik api key

---
+++ sdks/python/tests/unit/conftest.py
@@ -0,0 +1,23 @@
+import pytest
+import logging
+
+@pytest.fixture
+def capture_log(caplog):
+    logger = logging.getLogger("opik")
+    logger.setLevel("INFO")
+    logger.propagate = True  # Propagate so pytest logging capture works
+
+    yield caplog
+
+    logger.propagate = False
+
+
+@pytest.fixture
+def capture_log_debug(caplog):
+    logger = logging.getLogger("opik")
+    logger.setLevel("DEBUG")
+    logger.propagate = True  # Propagate so pytest logging capture works
+
+    yield caplog
+
+    logger.propagate = False
