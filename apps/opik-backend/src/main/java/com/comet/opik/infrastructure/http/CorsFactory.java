7cc5c6abecef47828ef7cdc47aa5c72198d8aeb4
Ido Berkovich <ido@comet.com>
2024-10-31 11:49:40 +0200

    [OPIK-136] fix cors request header comet workspace (#520)
    * OPIK-136 failing test
    
    * OPIK-136 refactored to constants
    
    * OPIK-136 failing test green
    
    * OPIK-136 refactored to constants
    
    * OPIK-136 changed header to "Comet-Workspace"
    
    * OPIK-136 reuse existing constant
    
    * OPIK-136 fixed compilation error
    
    * OPIK-136 let exception to be thrown

--- apps/opik-backend/src/main/java/com/comet/opik/infrastructure/http/CorsFactory.java
+++ apps/opik-backend/src/main/java/com/comet/opik/infrastructure/http/CorsFactory.java
@@ -1,6 +1,7 @@
 package com.comet.opik.infrastructure.http;
 
 import com.comet.opik.infrastructure.OpikConfiguration;
+import com.comet.opik.infrastructure.auth.RequestContext;
 import com.google.common.net.HttpHeaders;
 import io.dropwizard.core.setup.Environment;
 import jakarta.servlet.DispatcherType;
@@ -13,6 +14,9 @@ import java.util.EnumSet;
 
 @Slf4j
 public class CorsFactory {
+    public static final String COMET_SDK_API_HEADER = "Comet-Sdk-Api";
+    public static final String COMET_USERNAME_HEADER = "comet-username";
+    public static final String COMET_REACT_VER_HEADER = "comet-react-ver";
     private static final String CORS_PATH_FILTER = "/*";
 
     private static final String[] ALLOWED_HEADERS = new String[]{
@@ -21,9 +25,10 @@ public class CorsFactory {
             HttpHeaders.ACCEPT,
             HttpHeaders.X_REQUESTED_WITH,
             HttpHeaders.ORIGIN,
-            "Comet-Sdk-Api",
-            "comet-username",
-            "comet-react-ver",
+            COMET_SDK_API_HEADER,
+            COMET_USERNAME_HEADER,
+            COMET_REACT_VER_HEADER,
+            RequestContext.WORKSPACE_HEADER,
     };
 
     private static final String[] ALLOWED_METHODS = new String[]{
@@ -47,9 +52,9 @@ public class CorsFactory {
         final FilterRegistration.Dynamic cors = environment.servlets().addFilter("CORS", CrossOriginFilter.class);
 
         // CORS parameters
-        cors.setInitParameter("allowedOrigins", "*");
-        cors.setInitParameter("allowedHeaders", String.join(",", ALLOWED_HEADERS));
-        cors.setInitParameter("allowedMethods", String.join(",", ALLOWED_METHODS));
+        cors.setInitParameter(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "*");
+        cors.setInitParameter(CrossOriginFilter.ALLOWED_HEADERS_PARAM, String.join(",", ALLOWED_HEADERS));
+        cors.setInitParameter(CrossOriginFilter.ALLOWED_METHODS_PARAM, String.join(",", ALLOWED_METHODS));
         cors.setInitParameter(CrossOriginFilter.CHAIN_PREFLIGHT_PARAM, Boolean.FALSE.toString());
 
         // URL mappings
