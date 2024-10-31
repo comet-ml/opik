8548095c38dab7ec8c7477bd287b2242c2c9937c
Ido Berkovich <ido@comet.com>
2024-10-31 08:53:15 +0200

    OPIK-136 failing test

--- apps/opik-backend/src/test/java/com/comet/opik/infrastructure/http/cors/CorsEnabledE2ETest.java
+++ apps/opik-backend/src/test/java/com/comet/opik/infrastructure/http/cors/CorsEnabledE2ETest.java
@@ -5,7 +5,11 @@ import com.comet.opik.api.resources.utils.ClientSupportUtils;
 import com.comet.opik.api.resources.utils.MySQLContainerUtils;
 import com.comet.opik.api.resources.utils.RedisContainerUtils;
 import com.comet.opik.api.resources.utils.TestDropwizardAppExtensionUtils;
+import com.comet.opik.infrastructure.http.CorsFactory;
+import com.google.common.net.HttpHeaders;
 import com.redis.testcontainers.RedisContainer;
+import org.eclipse.jetty.http.HttpMethod;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -63,13 +67,29 @@ class CorsEnabledE2ETest {
     }
 
     @Test
-    @DisplayName("Should contain CORS headers")
-    void testCorsEnabled() {
+    @DisplayName("Should contain CORS headers for GET request")
+    void testCorsHeadersForGet() {
         var response = client.target("%s/is-alive/ping".formatted(baseURI))
                 .request().header("Origin", "localhost")
                 .get();
 
         assertThat(response.getStatus()).isEqualTo(200);
         assertThat(response.getHeaders()).containsKey("Access-Control-Allow-Origin");
+    }
+
+    @Test
+    @DisplayName("Should contain CORS headers for OPTIONS request containing the comet workspace header")
+    void testCorsHeadersForRequestHeader() {
+        try (var response = client.target("%s/v1/private/projects".formatted(baseURI))
+                .request()
+                .header("Origin", "localhost")
+                .header(HttpHeaders.ACCESS_CONTROL_REQUEST_HEADERS, CorsFactory.COMET_WORKSPACE_REQUEST_HEADER)
+                .header(HttpHeaders.ACCESS_CONTROL_REQUEST_METHOD, HttpMethod.GET.toString())
+                .options()) {
+            assertThat(response.getStatus()).isEqualTo(200);
+            assertThat(response.getHeaders()).containsKey("Access-Control-Allow-Origin");
+        } catch (Exception exception) {
+            Assertions.fail(exception);
+        }
     }
 }
