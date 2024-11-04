fddbf7eb50d421835dc93ff0c4972acf5ef8434b
Sasha <aliaksandr@comet.com>
2024-11-04 11:50:24 +0100

    [OPIK-347]: update the prompt page name;

--- apps/opik-frontend/src/components/layout/SideBar/SideBar.tsx
+++ apps/opik-frontend/src/components/layout/SideBar/SideBar.tsx
@@ -60,7 +60,7 @@ const MAIN_MENU_ITEMS: MenuItem[] = [
     path: "/$workspaceName/prompts",
     type: MENU_ITEM_TYPE.router,
     icon: FileTerminal,
-    label: "Prompts",
+    label: "Prompts library",
     count: "prompts",
   },
   {
