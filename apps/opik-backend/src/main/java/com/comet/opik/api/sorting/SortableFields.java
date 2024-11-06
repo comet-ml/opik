7a274d4d3273734006ae4737304593714fece634
Ido Berkovich <ido@comet.com>
2024-11-06 12:28:24 +0200

    OPIK-265 minor fix

--- apps/opik-backend/src/main/java/com/comet/opik/api/sorting/SortableFields.java
+++ apps/opik-backend/src/main/java/com/comet/opik/api/sorting/SortableFields.java
@@ -1,7 +1,7 @@
 package com.comet.opik.api.sorting;
 
 public class SortableFields {
-    public static final String ID = "ID";
+    public static final String ID = "id";
     public static final String NAME = "name";
     public static final String LAST_UPDATED_AT = "last_updated_at";
 }
