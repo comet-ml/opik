90f5a900705ec3e1fd8aa0d5fc12ff76905b6d88
Aliaksandr Kuzmik <alexander.kuzmik99@gmail.com>
2024-11-06 13:41:29 +0100

    Initial implementation for [Anthropic/AsyncAnthropic].messages.create

---
+++ sdks/python/src/opik/integrations/anthropic/messages_create_decorator.py
@@ -0,0 +1,83 @@
+import logging
+from typing import List, Any, Dict, Optional, Callable, Tuple, Union
+from opik.types import SpanType
+from opik.decorator import base_track_decorator, arguments_helpers
+from opik import dict_utils
+
+from anthropic.types import Message as AnthropicMessage
+
+
+LOGGER = logging.getLogger(__name__)
+
+KWARGS_KEYS_TO_LOG_AS_INPUTS = ["messages", "system", "tools"]
+RESPONSE_KEYS_TO_LOG_AS_OUTPUT = ["content"]
+
+
+class AnthropicMessagesCreateDecorator(base_track_decorator.BaseTrackDecorator):
+    """
+    An implementation of BaseTrackDecorator designed specifically for tracking
+    calls of `[Anthropic.AsyncAnthropic].messages.create` method.
+    """
+
+    def _start_span_inputs_preprocessor(
+        self,
+        func: Callable,
+        name: Optional[str],
+        type: SpanType,
+        tags: Optional[List[str]],
+        metadata: Optional[Dict[str, Any]],
+        capture_input: bool,
+        args: Optional[Tuple],
+        kwargs: Optional[Dict[str, Any]],
+        project_name: Optional[str],
+    ) -> arguments_helpers.StartSpanParameters:
+        assert (
+            kwargs is not None
+        ), "Expected kwargs to be not None in Antropic.messages.create(**kwargs)"
+
+        input, metadata = dict_utils.split_dict_by_keys(
+            kwargs, KWARGS_KEYS_TO_LOG_AS_INPUTS
+        )
+
+        name = name if name is not None else func.__name__
+
+        result = arguments_helpers.StartSpanParameters(
+            name=name,
+            input=input,
+            type=type,
+            tags=tags,
+            metadata=metadata,
+            project_name=project_name,
+        )
+
+        return result
+
+    def _end_span_inputs_preprocessor(
+        self, output: AnthropicMessage, capture_output: bool
+    ) -> arguments_helpers.EndSpanParameters:
+        usage = {
+            "prompt_tokens": output.usage.input_tokens,
+            "completion_tokens": output.usage.output_tokens,
+            "total_tokens": output.usage.input_tokens + output.usage.output_tokens,
+        }
+
+        output_dict = output.model_dump()
+        span_output, metadata = dict_utils.split_dict_by_keys(
+            output_dict, RESPONSE_KEYS_TO_LOG_AS_OUTPUT
+        )
+
+        result = arguments_helpers.EndSpanParameters(
+            output=span_output, usage=usage, metadata=metadata
+        )
+
+        return result
+
+    def _generators_handler(
+        self,
+        output: Any,
+        capture_output: bool,
+        generations_aggregator: Optional[Callable[[List[Any]], Any]],
+    ) -> Union[None,]:
+        NOT_A_STREAM = None
+
+        return NOT_A_STREAM
---
+++ sdks/python/src/opik/integrations/anthropic/opik_tracker.py
@@ -0,0 +1,39 @@
+from typing import Optional
+
+
+import anthropic
+from . import messages_create_decorator
+from typing import TypeVar
+
+AnthropicClient = TypeVar(
+    "AnthropicClient", anthropic.AsyncAnthropic, anthropic.Anthropic
+)
+
+
+def track_anthropic(
+    anthropic_client: AnthropicClient,
+    project_name: Optional[str] = None,
+) -> AnthropicClient:
+    """Adds Opik tracking to an OpenAI client.
+
+    Tracks calls to `openai_client.chat.completions.create()`, it includes support for streaming model.
+    Can be used within other Opik-tracked functions.
+
+    Args:
+        anthropic_client: An instance of Anthropic client.
+        project_name: The name of the project to log data.
+
+    Returns:
+        The modified OpenAI client with Opik tracking enabled.
+    """
+    decorator = messages_create_decorator.AnthropicMessagesCreateDecorator()
+    if not hasattr(anthropic_client.messages.create, "opik_tracked"):
+        wrapper = decorator.track(
+            type="llm",
+            name="anthropic_messages_create",
+            # generations_aggregator=chunks_aggregator.aggregate,
+            project_name=project_name,
+        )
+        anthropic_client.messages.create = wrapper(anthropic_client.messages.create)
+
+    return anthropic_client
