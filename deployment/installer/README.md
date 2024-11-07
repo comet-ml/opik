5126c644c4a3a15099b5eff1620acb35846f13e8
Liya Katz <liyak@comet.com>
2024-11-07 12:52:33 +0200

    OPIK-386: remove installer (#575)

--- deployment/installer/README.md
+++
@@ -1,3 +0,0 @@
-# Opik installer
-
-If you would like to contribute to the Opik installer, please refer to the [Contribution guide](./CONTRIBUTING.md).
--- deployment/installer/opik_installer/version.py
+++
@@ -1,7 +0,0 @@
-"""Version information for the package."""
-
-from importlib import metadata
-
-__version__: str = "0.0.0+dev"
-if __package__:
-    __version__ = metadata.version(__package__)
