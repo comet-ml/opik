9dec58d07074af6ca77d0a6cf59cb7b9a9cdc997
Ido Berkovich <ido@comet.com>
2024-11-07 13:15:49 +0200

    OPIK-370 improve regex

--- apps/opik-backend/src/main/java/com/comet/opik/domain/ImageUtils.java
+++ apps/opik-backend/src/main/java/com/comet/opik/domain/ImageUtils.java
@@ -3,7 +3,7 @@ package com.comet.opik.domain;
 import org.stringtemplate.v4.ST;
 
 public class ImageUtils {
-    private static final String IMAGE_TRUNCATION_REGEX = "data:image/.+;base64[^\"]+";
+    private static final String IMAGE_TRUNCATION_REGEX = "data:image/.+;base64,[A-Za-z0-9+/=]+";
 
     public static ST addTruncateToTemplate(ST template, boolean truncate) {
         return template.add("truncate", truncate ? ImageUtils.IMAGE_TRUNCATION_REGEX : null);
