d2ad84311e970b12e4f3be3f000197521134d93f
sasha <aadereiko@gmail.com>
2024-11-08 10:27:46 +0100

    [OPIK-325]: add link to project page (#585)
    * [OPIK-325]: add redirect to project page;
    
    * [OPIK-325]: change the order in a file;
    
    * [OPIK-325]: update dependencies;
    
    * [OPIK-325]: fix the default redirect;
    
    * [OPIK-325]: rename a var redirectProjectsRoute;
    
    * [OPIK-325]: remove a navigate redirect;
    
    * [OPIK-325]: fix the bug with `/redirect` path;
    
    * [OPIK-325]: update the number of projects for getting a project by name;
    
    * [OPIK-325]: remove console log;
    
    * [OPIK-325]: simplify redirect logic;
    
    * [OPIK-325]: eslint issue;
    
    * [OPIK-325]: fix the condition and add the traces to projects redirect;
    
    ---------
    
    Co-authored-by: Sasha <aliaksandr@comet.com>

---
+++ apps/opik-frontend/src/components/redirect/RedirectProjects.tsx
@@ -0,0 +1,51 @@
+import { useEffect } from "react";
+import Loader from "@/components/shared/Loader/Loader";
+import { StringParam, useQueryParams } from "use-query-params";
+import useAppStore from "@/store/AppStore";
+import { Navigate, useNavigate } from "@tanstack/react-router";
+import useProjectByName from "@/api/projects/useProjectByName";
+import NoData from "@/components/shared/NoData/NoData";
+
+const RedirectProjects = () => {
+  const [query] = useQueryParams({
+    id: StringParam,
+    name: StringParam,
+  });
+
+  const navigate = useNavigate();
+  const workspaceName = useAppStore((state) => state.activeWorkspaceName);
+
+  const { data: projectByName, isPending: isPendingProjectByName } =
+    useProjectByName(
+      { projectName: query.name || "" },
+      { enabled: !!query.name && !query.id },
+    );
+
+  useEffect(() => {
+    if (projectByName?.id) {
+      navigate({
+        to: "/$workspaceName/projects/$projectId/traces",
+        params: {
+          projectId: projectByName.id,
+          workspaceName,
+        },
+      });
+    }
+  }, [projectByName?.id, workspaceName, navigate]);
+
+  if (query.id) {
+    return <Navigate to={`/${workspaceName}/projects/${query.id}/traces`} />;
+  }
+
+  if (!isPendingProjectByName && !projectByName) {
+    return <NoData message="No project with this name" />;
+  }
+
+  if (!query.id && !query.name) {
+    return <NoData message="No project params set" />;
+  }
+
+  return <Loader />;
+};
+
+export default RedirectProjects;
