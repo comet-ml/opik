0c71615b5cf9ca40f1ef38418d9558e0169b15c7
Ido Berkovich <ido@comet.com>
2024-11-10 11:45:28 +0200

    OPIK-400 multiple images failing test green

--- apps/opik-backend/src/main/java/com/comet/opik/domain/ImageUtils.java
+++ apps/opik-backend/src/main/java/com/comet/opik/domain/ImageUtils.java
@@ -3,7 +3,7 @@ package com.comet.opik.domain;
 import org.stringtemplate.v4.ST;
 
 public class ImageUtils {
-    private static final String IMAGE_TRUNCATION_REGEX = "data:image/.+;base64,[A-Za-z0-9+/=]+";
+    private static final String IMAGE_TRUNCATION_REGEX = "data:image/[^;]{3,4};base64,[^\"]+";
 
     public static ST addTruncateToTemplate(ST template, boolean truncate) {
         return template.add("truncate", truncate ? ImageUtils.IMAGE_TRUNCATION_REGEX : null);
