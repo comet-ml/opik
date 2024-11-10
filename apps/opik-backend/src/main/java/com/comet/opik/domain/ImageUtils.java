d0ab65ba162d0595e07d3649e0feb70e591dfca0
Ido Berkovich <ido@comet.com>
2024-11-10 12:13:40 +0200

    OPIK-400 jpeg no prefix failing test green

--- apps/opik-backend/src/main/java/com/comet/opik/domain/ImageUtils.java
+++ apps/opik-backend/src/main/java/com/comet/opik/domain/ImageUtils.java
@@ -3,7 +3,7 @@ package com.comet.opik.domain;
 import org.stringtemplate.v4.ST;
 
 public class ImageUtils {
-    private static final String IMAGE_TRUNCATION_REGEX = "data:image/[^;]{3,4};base64,[^\"]+";
+    private static final String IMAGE_TRUNCATION_REGEX = "(data:image/[^;]{3,4};base64,)[^\"]+|/9j/[^\"]+";
 
     public static ST addTruncateToTemplate(ST template, boolean truncate) {
         return template.add("truncate", truncate ? ImageUtils.IMAGE_TRUNCATION_REGEX : null);
