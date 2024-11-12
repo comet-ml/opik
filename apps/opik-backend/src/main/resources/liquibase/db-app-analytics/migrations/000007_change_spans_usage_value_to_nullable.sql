369e3dde12789e0f7dae49e2a3f4c985789107e4
Borys Tkachenko <borystkachenko@Boryss-MacBook-Pro.local>
2024-11-12 18:29:57 +0100

    Update

--- apps/opik-backend/src/main/resources/liquibase/db-app-analytics/migrations/000007_change_spans_usage_value_to_nullable.sql
+++
@@ -1,7 +0,0 @@
---liquibase formatted sql
---changeset thiagohora:change_decimal_precision
-
-ALTER TABLE ${ANALYTICS_DB_DATABASE_NAME}.spans
-    MODIFY COLUMN usage Map(String, Nullable(Int32));
-
---rollback ALTER TABLE ${ANALYTICS_DB_DATABASE_NAME}.spans MODIFY COLUMN usage Map(String, Int32);
