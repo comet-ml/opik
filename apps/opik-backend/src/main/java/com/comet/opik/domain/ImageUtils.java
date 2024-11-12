e843670104b6023fa1235bdc0ed8af5a026c4ba1
Ido Berkovich <ido@comet.com>
2024-11-12 17:21:38 +0200

    OPIK-407 failing test reproducing the bug green

--- apps/opik-backend/src/main/java/com/comet/opik/domain/ImageUtils.java
+++ apps/opik-backend/src/main/java/com/comet/opik/domain/ImageUtils.java
@@ -4,24 +4,32 @@ import org.stringtemplate.v4.ST;
 
 public class ImageUtils {
     public static final String PREFIX_JPEG = "/9j/";
-    public static final String PREFIX_PNG = "iVBORw0KGgo=";
+    public static final String PREFIX_PNG = "iVBORw0KGgo";
     public static final String PREFIX_GIF0 = "R0lGODlh";
     public static final String PREFIX_GIF1 = "R0lGODdh";
     public static final String PREFIX_BMP = "Qk";
     public static final String PREFIX_TIFF0 = "SUkqAA";
-    public static final String PREFIX_TIFF1 = "II*";
+    public static final String PREFIX_TIFF1 = "II\\*";
     public static final String PREFIX_WEBP = "UklGR";
 
-    private static final String IMAGE_CHARS = "[^\"]+";
-    private static final String IMAGE_TRUNCATION_REGEX = "data:image/[^;]{3,4};base64," + IMAGE_CHARS + "|"
-            + PREFIX_JPEG + IMAGE_CHARS + "|"
-            + PREFIX_PNG + IMAGE_CHARS + "|"
-            + PREFIX_GIF0 + IMAGE_CHARS + "|"
-            + PREFIX_GIF1 + IMAGE_CHARS + "|"
-            + PREFIX_BMP + IMAGE_CHARS + "|"
-            + PREFIX_TIFF0 + IMAGE_CHARS + "|"
-            + PREFIX_TIFF1 + IMAGE_CHARS + "|"
-            + PREFIX_WEBP + IMAGE_CHARS;
+    private static final String IMAGE_CHARS = "[A-Za-z0-9+/]+={0,2}";
+    private static final String IMAGE_TRUNCATION_REGEX =
+            // capture images with general base64 prefix in case it is present
+            "(data:image/[^;]{3,4};base64,)?"
+                    // capture images with no base64 prefix but with specific image type prefix
+                    + "("
+                    + PREFIX_JPEG + "|"
+                    + PREFIX_PNG + "|"
+                    + PREFIX_GIF0 + "|"
+                    + PREFIX_GIF1 + "|"
+                    + PREFIX_BMP + "|"
+                    + PREFIX_TIFF0 + "|"
+                    + PREFIX_TIFF1 + "|"
+                    + PREFIX_WEBP
+                    + ")"
+                    // optional base64 padding
+                    + "={0,2}"
+                    + IMAGE_CHARS;
 
     public static ST addTruncateToTemplate(ST template, boolean truncate) {
         return template.add("truncate", truncate ? ImageUtils.IMAGE_TRUNCATION_REGEX : null);
