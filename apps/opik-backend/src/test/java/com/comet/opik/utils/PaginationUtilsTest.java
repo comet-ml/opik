acfa6cdf665607bd1bfbc9dae023be671dfc3aea
Ido Berkovich <ido@comet.com>
2024-11-13 11:35:46 +0200

    OPIK-396 pagination failing test

---
+++ apps/opik-backend/src/test/java/com/comet/opik/utils/PaginationUtilsTest.java
@@ -0,0 +1,33 @@
+package com.comet.opik.utils;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Named.named;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+public class PaginationUtilsTest {
+    private static final List<Integer> LIST = IntStream.range(0, 50).boxed().toList();
+    @ParameterizedTest
+    @MethodSource
+    void testPagination(int page, int size, List<Integer> expected) {
+        List<Integer> actual = PaginationUtils.paginate(page, size, LIST);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> testPagination() {
+        return Stream.of(
+                arguments(1, 10, IntStream.range(0, 10).boxed().toList()),
+                arguments(2, 15, IntStream.range(15, 30).boxed().toList()),
+                arguments(4, 15, IntStream.range(45, 50).boxed().toList()),
+                arguments(5, 15, List.of()),
+                arguments(1, 60, named("full list", LIST)));
+    }
+}
