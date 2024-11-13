67b11e329bbfcfc23aa9e8f5e84a218dd6661bc3
Ido Berkovich <ido@comet.com>
2024-11-13 11:37:57 +0200

    OPIK-396 pagination failing test green

--- apps/opik-backend/src/main/java/com/comet/opik/utils/PaginationUtils.java
+++ apps/opik-backend/src/main/java/com/comet/opik/utils/PaginationUtils.java
@@ -6,11 +6,16 @@ import java.util.List;
 
 public class PaginationUtils {
     public static <T> List<T> paginate(int page, int size, @NonNull List<T> elements) {
+        int offset = (page - 1) * size;
+
+        if (offset > elements.size()) {
+            return List.of();
+        }
+
         if (size > elements.size()) {
             return elements;
         }
 
-        int offset = (page - 1) * size;
         return elements.subList(offset, Math.min(offset + size, elements.size()));
     }
 }
