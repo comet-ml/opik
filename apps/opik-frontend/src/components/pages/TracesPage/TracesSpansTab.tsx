2213e2c862a122815d73bfdeba38578fc9c77422
Fernando Carril <facarril@gmail.com>
2024-11-13 11:49:42 +0100

    Enable truncation of base64 images in the traces/spans tables (#624)

--- apps/opik-frontend/src/components/pages/TracesPage/TracesSpansTab.tsx
+++ apps/opik-frontend/src/components/pages/TracesPage/TracesSpansTab.tsx
@@ -96,7 +96,7 @@ export const TracesSpansTab: React.FC<TracesSpansTabProps> = ({
       page: page as number,
       size: size as number,
       search: search as string,
-      truncate: false,
+      truncate: true,
     },
     {
       placeholderData: keepPreviousData,
