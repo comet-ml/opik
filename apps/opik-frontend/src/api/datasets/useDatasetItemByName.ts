734e9fddb708f724d07d35546a6829f0e3b9593e
sasha <aadereiko@gmail.com>
2024-11-15 10:43:54 +0100

    [NA]: [FE] replace getProjectByName function endpoint; (#643)
    * [NA]: replace getProjectByName function endpoint;
    
    * [NA]: add signals to get by name requests;
    
    * [NA]: use signal in getDatasetByName;
    
    ---------
    
    Co-authored-by: Sasha <aliaksandr@comet.com>

--- apps/opik-frontend/src/api/datasets/useDatasetItemByName.ts
+++ apps/opik-frontend/src/api/datasets/useDatasetItemByName.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { QueryFunctionContext, useQuery } from "@tanstack/react-query";
 import api, { DATASETS_REST_ENDPOINT, QueryConfig } from "@/api/api";
 import { Dataset } from "@/types/datasets";
 
@@ -6,11 +6,17 @@ type UseDatasetByNameParams = {
   datasetName: string;
 };
 
-const getDatasetByName = async ({ datasetName }: UseDatasetByNameParams) => {
+const getDatasetByName = async (
+  { signal }: QueryFunctionContext,
+  { datasetName }: UseDatasetByNameParams,
+) => {
   const response = await api.post<Dataset>(
     `${DATASETS_REST_ENDPOINT}/retrieve`,
     {
       dataset_name: datasetName,
+    },
+    {
+      signal,
     },
   );
 
@@ -23,7 +29,7 @@ export default function useDatasetItemByName(
 ) {
   return useQuery({
     queryKey: ["dataset", params],
-    queryFn: () => getDatasetByName(params),
+    queryFn: (context) => getDatasetByName(context, params),
     ...options,
   });
 }
