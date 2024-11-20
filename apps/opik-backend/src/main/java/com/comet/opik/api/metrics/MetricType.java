465b19430f1a73eb3d3cb93d89c9f9ac5011df3f
Ido Berkovich <ido@comet.com>
2024-11-20 15:34:57 +0200

    OPIK-432 code style

--- apps/opik-backend/src/main/java/com/comet/opik/api/metrics/MetricType.java
+++ apps/opik-backend/src/main/java/com/comet/opik/api/metrics/MetricType.java
@@ -1,9 +1,5 @@
 package com.comet.opik.api.metrics;
 
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
-
-@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public enum MetricType {
     FEEDBACK_SCORES,
     NUMBER_OF_TRACES,
