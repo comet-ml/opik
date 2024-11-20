c840ca98798616cce1b7793b87e6198606230303
Aliaksandr Kuzmik <98702584+alexkuzmik@users.noreply.github.com>
2024-11-20 15:59:23 +0100

    [OPIK-285] allow users to specify function parameters to not log when using the track decorator (#677)
    * Add a test for ignore argument in track
    
    * Refactor track implementation, all repeatable track arguments that were passed everywhere in BaseTrackDecorator and its inherited classes are moved to a separate dataclass - TrackOptions.
    
    * Remove odd line
    
    * Implement dropping ignored input arguments
    
    * Handle capture_input=True case
    
    * Temporarily remove test
    
    * Revert test back
    
    * Update tests
    
    * Fix lint errors
    
    * Rename ignore -> ignore_arguments

--- sdks/python/src/opik/integrations/anthropic/messages_create_decorator.py
+++ sdks/python/src/opik/integrations/anthropic/messages_create_decorator.py
@@ -1,6 +1,5 @@
 import logging
 from typing import List, Any, Dict, Optional, Callable, Tuple, Union
-from opik.types import SpanType
 from opik.decorator import base_track_decorator, arguments_helpers
 from opik import dict_utils
 
@@ -24,19 +23,15 @@ class AnthropicMessagesCreateDecorator(base_track_decorator.BaseTrackDecorator):
     def _start_span_inputs_preprocessor(
         self,
         func: Callable,
-        name: Optional[str],
-        type: SpanType,
-        tags: Optional[List[str]],
-        metadata: Optional[Dict[str, Any]],
-        capture_input: bool,
+        track_options: arguments_helpers.TrackOptions,
         args: Optional[Tuple],
         kwargs: Optional[Dict[str, Any]],
-        project_name: Optional[str],
     ) -> arguments_helpers.StartSpanParameters:
         assert (
             kwargs is not None
         ), "Expected kwargs to be not None in Antropic.messages.create(**kwargs)"
-        metadata = metadata if metadata is not None else {}
+        metadata = track_options.metadata if track_options.metadata is not None else {}
+        name = track_options.name if track_options.name is not None else func.__name__
 
         input, metadata_from_kwargs = dict_utils.split_dict_by_keys(
             kwargs, KWARGS_KEYS_TO_LOG_AS_INPUTS
@@ -44,15 +39,14 @@ class AnthropicMessagesCreateDecorator(base_track_decorator.BaseTrackDecorator):
         metadata.update(metadata_from_kwargs)
         metadata["created_from"] = "anthropic"
         tags = ["anthropic"]
-        name = name if name is not None else func.__name__
 
         result = arguments_helpers.StartSpanParameters(
             name=name,
             input=input,
-            type=type,
+            type=track_options.type,
             tags=tags,
             metadata=metadata,
-            project_name=project_name,
+            project_name=track_options.project_name,
         )
 
         return result
