5e7c0ef2a621cbcbfd5e051557fc9155eec8ad09
Aliaksandr Kuzmik <98702584+alexkuzmik@users.noreply.github.com>
2024-11-21 17:41:43 +0100

    [OPIK-445] LLama-index tests fix (#689)
    * Change fail fast to false
    
    * Update test
    
    * Bump minimal python version

--- sdks/python/tests/library_integration/llama_index/test_llama_index.py
+++ sdks/python/tests/library_integration/llama_index/test_llama_index.py
@@ -69,8 +69,7 @@ def test_llama_index__happyflow(
     index = VectorStoreIndex.from_documents(documents)
 
     query_engine = index.as_query_engine()
-    response = query_engine.query("What did the author do growing up?")
-    print(response)
+    _ = query_engine.query("What did the author do growing up?")
 
     opik_callback_handler.flush()
 
