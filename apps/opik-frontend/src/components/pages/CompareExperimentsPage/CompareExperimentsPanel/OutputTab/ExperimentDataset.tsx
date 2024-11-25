3cbebfce63bc138bebb90616ac09121c60ab41f1
Sasha <aliaksandr@comet.com>
2024-11-25 13:39:25 +0100

    [OPIK-438]: eslint fixes;

--- apps/opik-frontend/src/components/pages/CompareExperimentsPage/CompareExperimentsPanel/OutputTab/ExperimentDataset.tsx
+++ apps/opik-frontend/src/components/pages/CompareExperimentsPage/CompareExperimentsPanel/OutputTab/ExperimentDataset.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useEffect, useMemo } from "react";
 
 import { extractImageUrls } from "@/lib/images";
 import { DatasetItem } from "@/types/datasets";
