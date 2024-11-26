c71d0c87d1666b3d81c9124ac47b4a71b865f92d
Ido Berkovich <ido@comet.com>
2024-11-26 11:01:08 +0200

    [OPIK-395] Project Metrics - all but `usage` (#713)
    * OPIK-395 extend datapoint to hold more than one value
    
    * OPIK-395 handler factory
    
    * OPIK-395 handler factory logic
    
    * OPIK-395 generic mapping logic
    
    * OPIK-395 added skeleton e2e test for feedback scores
    
    * OPIK-395 post rebase changes
    
    * OPIK-395 intervals failing test
    
    * OPIK-395 intervals failing test green
    
    * OPIK-395 intervals failing test green [WIP - weekly]
    
    * OPIK-395 refactor
    
    * OPIK-395 feedback scores failing test [WIP]
    
    * OPIK-395 feedback scores failing test [WIP]
    
    * OPIK-395 feedback scores failing test green
    
    * OPIK-395 feedback scores deduplication
    
    * OPIK-395 refactor
    
    * OPIK-395 refactor
    
    * OPIK-395 refactor
    
    * OPIK-395 comments
    
    * OPIK-395 refactor
    
    * OPIK-395 refactor
    
    * OPIK-395 refactor
    
    * OPIK-395 weekly failing tests green
    
    * OPIK-395 weekly failing tests green
    
    * OPIK-395 null responses failing test
    
    * OPIK-395 null responses failing test green
    
    * OPIK-395 all time failing test
    
    * OPIK-395 all time failing test green
    
    * OPIK-395 all time only weekly
    
    * OPIK-395 fix after rebase
    
    * OPIK-395 pr comments
    
    * OPIK-395 pr comments
    
    * OPIK-395 fix compilation error
    
    * OPIK-395 removed support for "All time"
    
    * OPIK-395 pr comments

--- apps/opik-backend/src/main/java/com/comet/opik/domain/ProjectMetricsDAO.java
+++ apps/opik-backend/src/main/java/com/comet/opik/domain/ProjectMetricsDAO.java
@@ -1,22 +1,30 @@
 package com.comet.opik.domain;
 
-import com.comet.opik.api.DataPoint;
+import com.comet.opik.api.TimeInterval;
 import com.comet.opik.api.metrics.ProjectMetricRequest;
+import com.comet.opik.infrastructure.db.TransactionTemplateAsync;
 import com.comet.opik.infrastructure.instrumentation.InstrumentAsyncUtils;
 import com.google.inject.ImplementedBy;
 import io.r2dbc.spi.Connection;
 import io.r2dbc.spi.Result;
+import io.r2dbc.spi.Row;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
+import lombok.Builder;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.reactivestreams.Publisher;
 import org.stringtemplate.v4.ST;
 import reactor.core.publisher.Mono;
 
+import java.math.BigDecimal;
 import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.List;
 import java.util.UUID;
+import java.util.function.Function;
 
 import static com.comet.opik.domain.AsyncContextUtils.bindWorkspaceIdToMono;
 import static com.comet.opik.infrastructure.instrumentation.InstrumentAsyncUtils.endSegment;
@@ -25,55 +33,144 @@ import static com.comet.opik.utils.AsyncUtils.makeMonoContextAware;
 
 @ImplementedBy(ProjectMetricsDAOImpl.class)
 public interface ProjectMetricsDAO {
-    Mono<List<DataPoint>> getTraceCount(UUID projectId, ProjectMetricRequest request, Connection connection);
+    @Builder
+    record Entry(String name, Instant time, Number value) {}
+
+    Mono<List<Entry>> getTraceCount(@NonNull UUID projectId, @NonNull ProjectMetricRequest request);
+    Mono<List<Entry>> getFeedbackScores(@NonNull UUID projectId, @NonNull ProjectMetricRequest request);
 }
 
 @Slf4j
 @Singleton
 @RequiredArgsConstructor(onConstructor_ = @Inject)
 class ProjectMetricsDAOImpl implements ProjectMetricsDAO {
+    private final @NonNull TransactionTemplateAsync template;
+
+    public static final String NAME_TRACES = "traces";
+
     private static final String GET_TRACE_COUNT = """
-            SELECT toStartOfInterval(start_time, toIntervalHour(1)) AS bucket,
-                   count() as count
+            SELECT toStartOfInterval(start_time, <convert_interval>) AS bucket,
+                   nullIf(count(DISTINCT id), 0) as count
             FROM traces
             WHERE project_id = :project_id
                 AND workspace_id = :workspace_id
                 AND start_time >= parseDateTime64BestEffort(:start_time, 9)
-                AND end_time \\<= parseDateTime64BestEffort(:end_time, 9)
+                AND start_time \\<= parseDateTime64BestEffort(:end_time, 9)
             GROUP BY bucket
             ORDER BY bucket
             WITH FILL
+            <if(is_weekly)>
+                FROM toStartOfWeek(parseDateTime64BestEffort(:start_time), 3)
+                TO toDate(formatDateTime(parseDateTime64BestEffort(:end_time), '%F'))
+            <else>
                 FROM parseDateTimeBestEffort(:start_time)
                 TO parseDateTimeBestEffort(:end_time)
-                STEP toIntervalHour(1);
+            <endif>
+                STEP <convert_interval>;
+            """;
+
+    private static final String GET_FEEDBACK_SCORES = """
+            WITH feedback_scores_deduplication AS (
+                SELECT created_at,
+                        name,
+                        value
+                FROM feedback_scores
+                WHERE project_id = :project_id
+                    AND workspace_id = :workspace_id
+                    AND entity_type = 'trace'
+                ORDER BY entity_id DESC, last_updated_at DESC
+                LIMIT 1 BY entity_id, name
+            )
+            SELECT toStartOfInterval(created_at, <convert_interval>) AS bucket,
+                    name,
+                    nullIf(avg(value), 0) AS value
+            FROM feedback_scores_deduplication
+            WHERE created_at >= parseDateTime64BestEffort(:start_time, 9)
+                AND created_at \\<= parseDateTime64BestEffort(:end_time, 9)
+            GROUP BY name, bucket
+            ORDER BY name, bucket
+            WITH FILL
+            <if(is_weekly)>
+                FROM toStartOfWeek(parseDateTime64BestEffort(:start_time), 3)
+                TO toDate(formatDateTime(parseDateTime64BestEffort(:end_time), '%F'))
+            <else>
+                FROM parseDateTimeBestEffort(:start_time)
+                TO parseDateTimeBestEffort(:end_time)
+            <endif>
+                STEP <convert_interval>;
             """;
 
     @Override
-    public Mono<List<DataPoint>> getTraceCount(
-            UUID projectId, ProjectMetricRequest request, Connection connection) {
-        return getTracesCountForProject(projectId, request, connection)
-                .flatMapMany(this::mapToIntDataPoint)
-                .collectList();
+    public Mono<List<Entry>> getTraceCount(@NonNull UUID projectId, @NonNull ProjectMetricRequest request) {
+        return template.nonTransaction(connection -> getMetric(projectId, request, connection,
+                GET_TRACE_COUNT, "traceCount")
+                .flatMapMany(result -> rowToDataPoint(result, request, row -> NAME_TRACES,
+                        row -> row.get("count", Integer.class)))
+                .collectList());
     }
 
-    private Mono<? extends Result> getTracesCountForProject(
-            UUID projectId, ProjectMetricRequest request, Connection connection) {
-        var template = new ST(GET_TRACE_COUNT);
+    @Override
+    public Mono<List<Entry>> getFeedbackScores(@NonNull UUID projectId, @NonNull ProjectMetricRequest request) {
+        return template.nonTransaction(connection -> getMetric(projectId, request, connection,
+                GET_FEEDBACK_SCORES, "feedbackScores")
+                .flatMapMany(result -> rowToDataPoint(
+                        result,
+                        request,
+                        row -> row.get("name", String.class),
+                        row -> row.get("value", BigDecimal.class)))
+                .collectList());
+    }
+
+    private Mono<? extends Result> getMetric(
+            UUID projectId, ProjectMetricRequest request, Connection connection, String query, String segmentName) {
+        var template = new ST(query)
+                .add("convert_interval", intervalToSql(request.interval()))
+                .add("is_weekly", request.interval() == TimeInterval.WEEKLY);
         var statement = connection.createStatement(template.render())
                 .bind("project_id", projectId)
                 .bind("start_time", request.intervalStart().toString())
                 .bind("end_time", request.intervalEnd().toString());
 
-        InstrumentAsyncUtils.Segment segment = startSegment("traceCount", "Clickhouse", "get");
+        InstrumentAsyncUtils.Segment segment = startSegment(segmentName, "Clickhouse", "get");
 
         return makeMonoContextAware(bindWorkspaceIdToMono(statement))
                 .doFinally(signalType -> endSegment(segment));
     }
 
-    private Publisher<DataPoint> mapToIntDataPoint(Result result) {
-        return result.map(((row, rowMetadata) -> DataPoint.builder()
-                .time(row.get("bucket", Instant.class))
-                .value(row.get("count", Integer.class))
+    private Publisher<Entry> rowToDataPoint(
+            Result result, ProjectMetricRequest request, Function<Row, String> nameGetter,
+            Function<Row, ? extends Number> valueGetter) {
+        return result.map(((row, rowMetadata) -> Entry.builder()
+                .name(nameGetter.apply(row))
+                .value(valueGetter.apply(row))
+                .time(extractBucket(request, row))
                 .build()));
+    }
+
+    private Instant extractBucket(ProjectMetricRequest request, Row row) {
+        if (request.interval() == TimeInterval.WEEKLY) {
+            var date = row.get("bucket", LocalDate.class);
+            if (date == null) {
+                return null;
+            }
+
+            return date.atStartOfDay(ZoneId.of("UTC")).toInstant();
+        }
+
+        return row.get("bucket", Instant.class);
+    }
+
+    private String intervalToSql(TimeInterval interval) {
+        if (interval == TimeInterval.WEEKLY) {
+               return "toIntervalWeek(1)";
+        }
+        if (interval == TimeInterval.DAILY) {
+            return "toIntervalDay(1)";
+        }
+        if (interval == TimeInterval.HOURLY) {
+            return "toIntervalHour(1)";
+        }
+
+        throw new IllegalArgumentException("Invalid interval: " + interval);
     }
 }
