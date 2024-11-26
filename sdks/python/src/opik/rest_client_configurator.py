c5c5e304f2ba4887b72ff1af725ffdc28ae86008
Aliaksandr Kuzmik <alexander.kuzmik99@gmail.com>
2024-11-26 14:12:28 +0100

    Fix lint errors

--- sdks/python/src/opik/rest_client_configurator.py
+++ sdks/python/src/opik/rest_client_configurator.py
@@ -5,9 +5,7 @@ from typing import Callable, Any
 
 connection_retry = tenacity.retry(
     stop=tenacity.stop_after_attempt(3),
-    wait=tenacity.wait_exponential(
-        multiplier=1, min=1, max=10
-    ),
+    wait=tenacity.wait_exponential(multiplier=1, min=1, max=10),
     retry=tenacity.retry_if_exception_type(
         (
             httpx.RemoteProtocolError,  # handle retries for expired connections
@@ -16,6 +14,7 @@ connection_retry = tenacity.retry(
         )
     ),
 )
+
 
 def configure(rest_client: rest_api.OpikApi) -> None:
     _configure_retries(rest_client)
@@ -31,12 +30,16 @@ def _configure_retries(rest_client: rest_api.OpikApi) -> None:
         "prompts",
     ]
     for domain_client_name in domain_client_names:
-        _decorate_public_instance_methods(getattr(rest_client, domain_client_name), connection_retry)
+        _decorate_public_instance_methods(
+            getattr(rest_client, domain_client_name), connection_retry
+        )
 
 
-def _decorate_public_instance_methods(instance: Any, decorator: Callable[[Callable], Callable]) -> None:
+def _decorate_public_instance_methods(
+    instance: Any, decorator: Callable[[Callable], Callable]
+) -> None:
+    attr_name: str
     for attr_name, attr_value in instance.__dict__.items():
-        attr_name: str
-        if callable(attr_value) and not attr_name.startswith('_'):
+        if callable(attr_value) and not attr_name.startswith("_"):
             decorated_method = decorator(attr_value)
             setattr(instance, attr_name, decorated_method)
