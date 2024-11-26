9e6e9aa877a8dc4dd608f7374a231e1e6857db46
Aliaksandr Kuzmik <alexander.kuzmik99@gmail.com>
2024-11-26 15:48:22 +0100

    Remove timeouts from httpx_client.py

--- sdks/python/src/opik/httpx_client.py
+++ sdks/python/src/opik/httpx_client.py
@@ -7,14 +7,7 @@ import platform
 
 def get(workspace: str, api_key: Optional[str]) -> httpx.Client:
     limits = httpx.Limits(keepalive_expiry=30)
-    timeout = httpx.Timeout(
-        connect=5.0,  # Time to establish a connection
-        read=5,  # Time to wait for a response after request
-        write=5,  # Time to send data to the server
-        pool=60.0,  # Time a connection can remain idle in the pool
-    )
-
-    client = httpx.Client(limits=limits, timeout=timeout)
+    client = httpx.Client(limits=limits)
 
     headers = _prepare_headers(workspace=workspace, api_key=api_key)
     client.headers.update(headers)
