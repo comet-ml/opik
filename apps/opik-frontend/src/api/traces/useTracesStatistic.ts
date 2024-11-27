e26b20c9c45e37ffb03f295d4db94329c49205c1
andrii.dudar <fanat.web@gmail.com>
2024-11-27 11:02:38 +0100

    [OPIK-398] Project statistics - FE implementation

---
+++ apps/opik-frontend/src/api/traces/useTracesStatistic.ts
@@ -0,0 +1,44 @@
+import { QueryFunctionContext, useQuery } from "@tanstack/react-query";
+import api, { QueryConfig, TRACES_KEY, TRACES_REST_ENDPOINT } from "@/api/api";
+import { ColumnsStatistic } from "@/types/shared";
+import { Filters } from "@/types/filters";
+import { generateSearchByIDFilters, processFilters } from "@/lib/filters";
+
+type UseTracesStatisticParams = {
+  projectId: string;
+  filters?: Filters;
+  search?: string;
+};
+
+export type UseTracesStatisticResponse = {
+  stats: ColumnsStatistic;
+};
+
+const getTracesStatistic = async (
+  { signal }: QueryFunctionContext,
+  { projectId, filters, search }: UseTracesStatisticParams,
+) => {
+  const { data } = await api.get<UseTracesStatisticResponse>(
+    `${TRACES_REST_ENDPOINT}stats`,
+    {
+      signal,
+      params: {
+        project_id: projectId,
+        ...processFilters(filters, generateSearchByIDFilters(search)),
+      },
+    },
+  );
+
+  return data;
+};
+
+export default function useTracesStatistic(
+  params: UseTracesStatisticParams,
+  options?: QueryConfig<UseTracesStatisticResponse>,
+) {
+  return useQuery({
+    queryKey: [TRACES_KEY, params],
+    queryFn: (context) => getTracesStatistic(context, params),
+    ...options,
+  });
+}
