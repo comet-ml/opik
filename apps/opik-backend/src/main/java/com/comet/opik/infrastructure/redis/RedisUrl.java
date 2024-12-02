217cfeeba2d65cde6260a4f8dc954999b22a3c2b
Thiago dos Santos Hora <thiagoh@comet.com>
2024-12-02 12:34:59 +0100

    [OPIK-493] Fix Redis setup to properly config database indexes (#779)
    * [OPIK-493] Fix redis setup to properly config database indexes
    
    * PR review
    
    * Cover credentials case

---
+++ apps/opik-backend/src/main/java/com/comet/opik/infrastructure/redis/RedisUrl.java
@@ -0,0 +1,36 @@
+package com.comet.opik.infrastructure.redis;
+
+import lombok.NonNull;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public record RedisUrl(String scheme, String host, int port, int database) {
+
+    public static RedisUrl parse(@NonNull String redisUrl) {
+        try {
+            URI uri = new URI(redisUrl.trim());
+            String scheme = uri.getScheme();
+            String host = uri.getHost();
+            int port = uri.getPort() == -1 ? 6379 : uri.getPort();
+            String path = uri.getPath();
+            int database = 0;
+
+            if (path != null && path.length() > 1) {
+                database = getDatabase(path);
+            }
+
+            return new RedisUrl(scheme, host, port, database);
+        } catch (URISyntaxException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    private static int getDatabase(String path) {
+        try {
+            return Integer.parseInt(path.substring(1));
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Invalid database index in Redis URL: " + path);
+        }
+    }
+}
---
+++ apps/opik-backend/src/test/java/com/comet/opik/infrastructure/redis/RedisUrlTest.java
@@ -0,0 +1,40 @@
+package com.comet.opik.infrastructure.redis;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("Redis URL parser Unit Test")
+class RedisUrlTest {
+
+    public static Stream<Arguments> testRedisUrlParser() {
+        return Stream.of(
+                Arguments.of("redis://localhost:6379/0", "redis", "localhost", 6379, 0),
+                Arguments.of("redis://localhost:6379/1", "redis", "localhost", 6379, 1),
+                Arguments.of("redis://localhost:6379", "redis", "localhost", 6379, 0),
+                Arguments.of("rediss://localhost:6379", "rediss", "localhost", 6379, 0),
+                Arguments.of("rediss://master.redis.cache.amazonaws.com:7000/3", "rediss",
+                        "master.redis.cache.amazonaws.com", 7000, 3),
+                Arguments.of("rediss://:xxxxxxxxxxx@redis-16379.hosted.com:16379/16", "rediss",
+                        "redis-16379.hosted.com", 16379, 16)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    @DisplayName("Test parse method with different URLs")
+    void testRedisUrlParser(String url, String scheme, String host, int port, int database) {
+        var redisUrl = RedisUrl.parse(url);
+
+        assertThat(scheme).isEqualTo(redisUrl.scheme());
+        assertThat(host).isEqualTo(redisUrl.host());
+        assertThat(port).isEqualTo(redisUrl.port());
+        assertThat(database).isEqualTo(redisUrl.database());
+    }
+
+}
