5a1b0788e3d6b6bf80d394a51ac88e55ae09e9b4
AndreiCautisanu <30831438+AndreiCautisanu@users.noreply.github.com>
2024-12-04 16:01:28 +0200

    OPIK-363 experiments tests no items (#800)
    * experiments tests no items
    
    * comments
    
    * run automatically on fern generated code changes PRs
    
    * fixes
    
    * fix for broken dataset items tests
    
    ---------
    
    Co-authored-by: Andrei Căutișanu <andreicautisanu@ip-192-168-1-150.eu-west-1.compute.internal>
    Co-authored-by: Andrei Căutișanu <andreicautisanu@Andreis-MacBook-Pro.local>

--- tests_end_to_end/page_objects/ExperimentsPage.py
+++ tests_end_to_end/page_objects/ExperimentsPage.py
@@ -4,9 +4,25 @@ class ExperimentsPage:
     def __init__(self, page: Page):
         self.page = page
         self.url = '/default/experiments'
+        self.search_bar = self.page.get_by_test_id('search-input')
 
     def go_to_page(self):
         self.page.goto(self.url)
+
+    def search_experiment_by_name(self, exp_name: str):
+        self.search_bar.click()
+        self.search_bar.fill(exp_name)
     
-    def check_experiment_exists_by_name(self, name):
-        expect(self.page.get_by_text(name).first).to_be_visible()
+    def check_experiment_exists_by_name(self, exp_name: str):
+        self.search_experiment_by_name(exp_name)
+        expect(self.page.get_by_text(exp_name).first).to_be_visible()
+    
+    def check_experiment_not_exists_by_name(self, exp_name: str):
+        self.search_experiment_by_name(exp_name)
+        expect(self.page.get_by_text(exp_name)).not_to_be_visible()
+
+    def delete_experiment_by_name(self, exp_name: str):
+        self.search_experiment_by_name(exp_name)
+        self.page.get_by_role('row', name=exp_name).first.get_by_role('button', name='Actions menu').click()
+        self.page.get_by_role('menuitem', name='Delete').click()
+        self.page.get_by_role('button', name='Delete experiment').click()
