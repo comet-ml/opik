f7e4d405d0eb8ade37548cab401e2e4dfb742b1b
Jacques Verré <jverre@gmail.com>
2024-12-05 06:53:26 -0500

    Jacques/doc updates (#821)
    * Removed cookbooks in md format
    
    * Removed autogenerated files and updated build scripts
    
    * Updated gitignore file for autogenerated files
    
    * Update build script
    
    * Updated build script

--- apps/opik-documentation/documentation/build_script.sh
+++ apps/opik-documentation/documentation/build_script.sh
@@ -5,7 +5,8 @@ npm run docusaurus clean-api-docs all
 npm run docusaurus gen-api-docs all
 
 # Rebuild the Cookbooks
-jupyter nbconvert docs/cookbook/*.ipynb --clear-output --to markdown 
+jupyter nbconvert --clear-output --inplace docs/cookbook/*.ipynb
+jupyter nbconvert docs/cookbook/*.ipynb --to markdown
 
 # Build the docs
 docusaurus build
