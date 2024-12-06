8aa7f4d5a1a5aee38c5803e5e8382fa8cc73f59a
Alexander Barannikov <japdubengsub@gmail.com>
2024-12-06 12:39:45 +0000

    use functools.wraps()

--- sdks/python/src/opik/integrations/langchain/base_llm_patcher.py
+++ sdks/python/src/opik/integrations/langchain/base_llm_patcher.py
@@ -1,5 +1,5 @@
+from functools import wraps
 from typing import Any, Dict
-
 from langchain_core import language_models
 
 __BaseLLM_original_dict = language_models.BaseLLM.dict
@@ -8,11 +8,15 @@ __BaseLLM_original_dict = language_models.BaseLLM.dict
 def base_llm_dict_patch(
     llm_instance: language_models.BaseLLM, **kwargs: Any
 ) -> Dict[str, Any]:
-    result = __BaseLLM_original_dict(llm_instance, **kwargs)
-    if (
-        hasattr(llm_instance, "client")
-        and hasattr(llm_instance.client, "_client")
-        and hasattr(llm_instance.client._client, "base_url")
-    ):
-        result["base_url"] = llm_instance.client._client.base_url
-    return result
+    @wraps(__BaseLLM_original_dict)
+    def wrapper(*args: Any, **kwargs: Any) -> Dict[str, Any]:
+        result = __BaseLLM_original_dict(*args, **kwargs)
+        if (
+            hasattr(llm_instance, "client")
+            and hasattr(llm_instance.client, "_client")
+            and hasattr(llm_instance.client._client, "base_url")
+        ):
+            result["base_url"] = llm_instance.client._client.base_url
+        return result
+
+    return wrapper(llm_instance, **kwargs)
