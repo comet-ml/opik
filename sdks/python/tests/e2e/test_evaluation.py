e171dea04ee8890138782627a3ecef8bbc163c02
Alexander Barannikov <japdubengsub@gmail.com>
2024-12-09 18:08:34 +0000

    g-eval

--- sdks/python/tests/e2e/test_evaluation.py
+++ sdks/python/tests/e2e/test_evaluation.py
@@ -1,6 +1,6 @@
 import pytest
 
-from opik.evaluation.metrics import AnswerRelevance, ContextPrecision, ContextRecall, Hallucination, Moderation
+from opik.evaluation import metrics
 
 
 def assert_score_result(result):
@@ -18,7 +18,7 @@ def assert_score_result(result):
     ],
 )
 def test__answer_relevance(context):
-    answer_relevance_metric = AnswerRelevance()
+    answer_relevance_metric = metrics.AnswerRelevance()
 
     result = answer_relevance_metric.score(
         input="What's the capital of France?",
@@ -37,7 +37,7 @@ def test__answer_relevance(context):
     ],
 )
 def test__context_precision(context):
-    context_precision_metric = ContextPrecision()
+    context_precision_metric = metrics.ContextPrecision()
 
     result = context_precision_metric.score(
         input="What's the capital of France?",
@@ -57,7 +57,7 @@ def test__context_precision(context):
     ],
 )
 def test__context_recall(context):
-    context_precision_metric = ContextRecall()
+    context_precision_metric = metrics.ContextRecall()
 
     result = context_precision_metric.score(
         input="What's the capital of France?",
@@ -77,7 +77,7 @@ def test__context_recall(context):
     ],
 )
 def test__hallucination(context):
-    hallucination_metric = Hallucination()
+    hallucination_metric = metrics.Hallucination()
 
     result = hallucination_metric.score(
         input="What is the capital of France?",
@@ -89,10 +89,29 @@ def test__hallucination(context):
 
 
 def test__moderation():
-    moderation_metric = Moderation()
+    moderation_metric = metrics.Moderation()
 
     result = moderation_metric.score(
         output="The capital of France is Paris. It is famous for its iconic Eiffel Tower and rich cultural heritage."
     )
+
+    assert_score_result(result)
+
+
+def test__g_eval():
+    g_eval_metric = metrics.GEval(
+        task_introduction="You are an expert judge tasked with evaluating the faithfulness of an AI-generated answer to the given context.",
+        evaluation_criteria="The OUTPUT must not introduce new information beyond what's provided in the CONTEXT.",
+    )
+
+    result = g_eval_metric.score(
+        output=str({
+            "OUTPUT": "What is the capital of France?",
+            "CONTEXT": [
+                "France is a country in Western Europe. Its capital is Paris, which is known for landmarks like the Eiffel Tower."
+            ],
+        })
+    )
+    print("g_eval_score:", result)
 
     assert_score_result(result)
