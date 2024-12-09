720fdc0f4ff4413d10224b6b3e45edfb1dbb4ebf
Aliaksandr Kuzmik <alexander.kuzmik99@gmail.com>
2024-12-09 12:20:06 +0100

    Update connection_retry decorator parameters

--- sdks/python/src/opik/rest_client_configurator/retry_decorators.py
+++ sdks/python/src/opik/rest_client_configurator/retry_decorators.py
@@ -3,7 +3,7 @@ import httpx
 
 connection_retry = tenacity.retry(
     stop=tenacity.stop_after_attempt(3),
-    wait=tenacity.wait_exponential(multiplier=1, min=1, max=10),
+    wait=tenacity.wait_exponential(multiplier=2, min=3, max=10),
     retry=tenacity.retry_if_exception_type(
         (
             httpx.RemoteProtocolError,  # handle retries for expired connections
