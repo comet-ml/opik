978e507975b5e37af479e1bbace9fae3d4256421
Alexander Barannikov <32936723+japdubengsub@users.noreply.github.com>
2024-12-09 10:01:41 +0000

    OPIK-519 [SDK] Langchain integration: cost tracking for openai calls (#831)
    * OPIK-519 [SDK] Langchain integration: cost tracking for openai calls
    
    * additional check for base_url
    
    * move monkey patch to separate file
    
    * import module, not class
    
    * use functools.wraps()
    
    * do not use project name in test
    
    * refactor llm usage info
    
    * change wrapper behavior

--- sdks/python/src/opik/integrations/langchain/openai_run_helpers.py
+++ sdks/python/src/opik/integrations/langchain/openai_run_helpers.py
@@ -1,10 +1,10 @@
-from typing import Dict, Any, Optional, TYPE_CHECKING, cast
-from opik.types import UsageDict
-
+import dataclasses
 import logging
+from typing import Any, Dict, Optional, TYPE_CHECKING, Tuple, cast
 
-from opik.validation import usage as usage_validator
 from opik import logging_messages
+from opik.types import UsageDict
+from opik.validation import usage as usage_validator
 
 if TYPE_CHECKING:
     from langchain_core.tracers.schemas import Run
@@ -13,7 +13,24 @@ if TYPE_CHECKING:
 LOGGER = logging.getLogger(__name__)
 
 
-def try_get_token_usage(run_dict: Dict[str, Any]) -> Optional[UsageDict]:
+@dataclasses.dataclass
+class LLMUsageInfo:
+    provider: Optional[str] = None
+    model: Optional[str] = None
+    token_usage: Optional[UsageDict] = None
+
+
+def get_llm_usage_info(run_dict: Optional[Dict[str, Any]] = None) -> LLMUsageInfo:
+    if run_dict is None:
+        return LLMUsageInfo()
+
+    usage_dict = _try_get_token_usage(run_dict)
+    provider, model = _get_provider_and_model(run_dict)
+
+    return LLMUsageInfo(provider=provider, model=model, token_usage=usage_dict)
+
+
+def _try_get_token_usage(run_dict: Dict[str, Any]) -> Optional[UsageDict]:
     try:
         token_usage = run_dict["outputs"]["llm_output"]["token_usage"]
         if usage_validator.UsageValidator(token_usage).validate().ok():
@@ -44,3 +61,30 @@ def is_openai_run(run: "Run") -> bool:
             exc_info=True,
         )
         return False
+
+
+def _get_provider_and_model(
+    run_dict: Dict[str, Any],
+) -> Tuple[Optional[str], Optional[str]]:
+    """
+    Fetches the provider and model information from a given run dictionary.
+
+    By default, the provider is assumed to be OpenAI (will be available in extra/metadata field).
+    If LLM output is available, the model version is included.
+    If the Client is available, the BaseURL is also checked.
+    """
+    provider = None
+    model = None
+
+    if metadata := run_dict["extra"].get("metadata"):
+        provider = metadata.get("ls_provider")
+        model = metadata.get("ls_model_name")
+
+    if llm_output := run_dict["outputs"].get("llm_output"):
+        model = llm_output.get("model_name", model)
+
+    if base_url := run_dict["extra"].get("invocation_params", {}).get("base_url"):
+        if base_url.host != "api.openai.com":
+            provider = base_url.host
+
+    return provider, model
