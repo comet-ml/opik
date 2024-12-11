53675a5e5f2073b1d479c2e2c84e67491ed95cef
Ido Berkovich <ido@comet.com>
2024-12-11 14:44:06 +0200

    OPIK-419 default null

--- apps/opik-backend/src/main/resources/liquibase/db-app-state/migrations/000006_add_projects_last_updated_trace_at.sql
+++ apps/opik-backend/src/main/resources/liquibase/db-app-state/migrations/000006_add_projects_last_updated_trace_at.sql
@@ -1,6 +1,6 @@
 --liquibase formatted sql
 --changeset idoberko2:add_projects_last_updated_trace_at
 
-ALTER TABLE projects ADD COLUMN last_updated_trace_at TIMESTAMP(6);
+ALTER TABLE projects ADD COLUMN last_updated_trace_at TIMESTAMP(6) DEFAULT NULL;
 
 --rollback ALTER TABLE projects DROP COLUMN last_updated_trace_at;
