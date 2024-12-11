51f5d2942f054b384813b9eabf71aa3bd99ec3b8
Andres Cruz <andresc@comet.com>
2024-12-11 11:43:28 +0100

    OPIK-545: Add chat completion spec and SDKs code (#854)

---
+++ sdks/typescript/src/opik/rest_api/serialization/resources/chatCompletions/client/requests/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ChatCompletionRequest = void 0;
+var ChatCompletionRequest_1 = require("./ChatCompletionRequest");
+Object.defineProperty(exports, "ChatCompletionRequest", { enumerable: true, get: function () { return ChatCompletionRequest_1.ChatCompletionRequest; } });
