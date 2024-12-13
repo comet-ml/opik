017ceab77b5c1e6da070cb8eb0f6736ca87d659b
Sasha <aliaksandr@comet.com>
2024-12-13 15:10:46 +0100

    [OPIK-536]: remove use memo;

--- apps/opik-frontend/src/components/pages/PlaygroundPage/PlaygroundPrompt/PlaygroundPrompt.tsx
+++ apps/opik-frontend/src/components/pages/PlaygroundPage/PlaygroundPrompt/PlaygroundPrompt.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useMemo, useRef } from "react";
+import React, { useCallback, useEffect, useRef } from "react";
 import { CopyPlus, Trash } from "lucide-react";
 import last from "lodash/last";
 
