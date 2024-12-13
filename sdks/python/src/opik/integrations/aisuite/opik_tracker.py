bb98261dab8a8bf7479b69a54e5786a0e7f9e5f9
Alexander Barannikov <32936723+japdubengsub@users.noreply.github.com>
2024-12-13 13:41:18 +0000

    [OPIK-569] [SDK] Implement integration with aisuite (#870)
    * OPIK-569 [SDK] Implement integration with aisuite [wip]
    
    * RC 1
    
    * RC 2
    
    * RC 3
    
    * RC 4
    
    * add integration tests to CI
    
    * raise minimum python version to 3.10 for integration tests

---
+++ sdks/python/src/opik/integrations/aisuite/opik_tracker.py
@@ -0,0 +1,43 @@
+from typing import Optional
+
+import aisuite
+
+from . import aisuite_decorator
+
+
+def track_aisuite(
+    aisuite_client: aisuite.Client,
+    project_name: Optional[str] = None,
+) -> aisuite.Client:
+    """Adds Opik tracking to an AISuite client.
+
+    Tracks calls to:
+    * `aisuite_client.chat.completions.create()`,
+
+    Can be used within other Opik-tracked functions.
+
+    Args:
+        aisuite_client: An instance of AISuite client.
+        project_name: The name of the project to log data.
+
+    Returns:
+        The modified AISuite client with Opik tracking enabled.
+    """
+    if hasattr(aisuite_client, "opik_tracked"):
+        return aisuite_client
+
+    aisuite_client.opik_tracked = True
+
+    decorator_factory = aisuite_decorator.AISuiteTrackDecorator()
+
+    completions_create_decorator = decorator_factory.track(
+        type="llm",
+        name="chat_completion_create",
+        project_name=project_name,
+    )
+
+    aisuite_client.chat.completions.create = completions_create_decorator(
+        aisuite_client.chat.completions.create
+    )
+
+    return aisuite_client
