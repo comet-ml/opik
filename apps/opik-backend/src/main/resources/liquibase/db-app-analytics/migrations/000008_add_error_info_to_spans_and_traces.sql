0d0d9149d1e2b5dbfcae85b3155ab671e3c10095
Borys Tkachenko <borystkachenko@ip-192-168-0-122.eu-west-1.compute.internal>
2024-12-13 10:41:48 +0100

    Fix comment

--- apps/opik-backend/src/main/resources/liquibase/db-app-analytics/migrations/000008_add_error_info_to_spans_and_traces.sql
+++ apps/opik-backend/src/main/resources/liquibase/db-app-analytics/migrations/000008_add_error_info_to_spans_and_traces.sql
@@ -1,5 +1,5 @@
 --liquibase formatted sql
---changeset BorisTkachenko:add_model_to_spans
+--changeset BorisTkachenko:000008_add_error_info_to_spans_and_traces
 
 ALTER TABLE ${ANALYTICS_DB_DATABASE_NAME}.spans
     ADD COLUMN IF NOT EXISTS error_info String DEFAULT '';
