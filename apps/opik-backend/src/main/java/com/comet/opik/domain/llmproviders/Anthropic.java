43296c936f739217f4e1c2b1faa8e089094a4544
Ido Berkovich <ido@comet.com>
2024-12-25 14:41:11 +0200

    OPIK-610 add anthropic [WIP]

---
+++ apps/opik-backend/src/main/java/com/comet/opik/domain/llmproviders/Anthropic.java
@@ -0,0 +1,205 @@
+package com.comet.opik.domain.llmproviders;
+
+import com.comet.opik.domain.ChatCompletionService;
+import com.comet.opik.infrastructure.LlmProviderClientConfig;
+import dev.ai4j.openai4j.chat.AssistantMessage;
+import dev.ai4j.openai4j.chat.ChatCompletionChoice;
+import dev.ai4j.openai4j.chat.ChatCompletionRequest;
+import dev.ai4j.openai4j.chat.ChatCompletionResponse;
+import dev.ai4j.openai4j.chat.Message;
+import dev.ai4j.openai4j.chat.Role;
+import dev.ai4j.openai4j.shared.Usage;
+import dev.langchain4j.data.message.AiMessage;
+import dev.langchain4j.data.message.Content;
+import dev.langchain4j.data.message.TextContent;
+import dev.langchain4j.data.message.UserMessage;
+import dev.langchain4j.model.StreamingResponseHandler;
+import dev.langchain4j.model.anthropic.internal.api.AnthropicContent;
+import dev.langchain4j.model.anthropic.internal.api.AnthropicCreateMessageRequest;
+import dev.langchain4j.model.anthropic.internal.api.AnthropicCreateMessageResponse;
+import dev.langchain4j.model.anthropic.internal.api.AnthropicMessage;
+import dev.langchain4j.model.anthropic.internal.api.AnthropicMessageContent;
+import dev.langchain4j.model.anthropic.internal.api.AnthropicRole;
+import dev.langchain4j.model.anthropic.internal.api.AnthropicTextContent;
+import dev.langchain4j.model.anthropic.internal.api.AnthropicToolChoice;
+import dev.langchain4j.model.anthropic.internal.client.AnthropicClient;
+import dev.langchain4j.model.anthropic.internal.client.AnthropicHttpException;
+import dev.langchain4j.model.output.Response;
+import io.dropwizard.jersey.errors.ErrorMessage;
+import jakarta.ws.rs.BadRequestException;
+import jakarta.ws.rs.ClientErrorException;
+import jakarta.ws.rs.InternalServerErrorException;
+import jakarta.ws.rs.ServerErrorException;
+import jakarta.ws.rs.WebApplicationException;
+import lombok.NonNull;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Consumer;
+
+public class Anthropic implements LlmProviderService {
+    private final LlmProviderClientConfig llmProviderClientConfig;
+    private final AnthropicClient anthropicClient;
+
+    public Anthropic(LlmProviderClientConfig llmProviderClientConfig, String apiKey) {
+        this.llmProviderClientConfig = llmProviderClientConfig;
+        this.anthropicClient = newClient(apiKey);
+    }
+
+    @Override
+    public ChatCompletionResponse generate(@NonNull ChatCompletionRequest request, @NonNull String workspaceId) {
+        var response = anthropicClient.createMessage(AnthropicCreateMessageRequest.builder()
+                .model(request.model())
+                .messages(request.messages().stream().map(this::mapMessage).toList())
+                .temperature(request.temperature())
+                .topP(request.topP())
+                .stopSequences(request.stop())
+                .maxTokens(request.maxTokens())
+                .toolChoice(AnthropicToolChoice.from(request.toolChoice().toString()))
+                .build());
+
+        return ChatCompletionResponse.builder()
+                .id(response.id)
+                .model(response.model)
+                .choices(response.content.stream().map(content -> mapContentToChoice(response, content))
+                        .toList())
+                .usage(Usage.builder()
+                        .promptTokens(response.usage.inputTokens)
+                        .completionTokens(response.usage.outputTokens)
+                        .totalTokens(response.usage.inputTokens + response.usage.outputTokens)
+                        .build())
+                .build();
+    }
+
+    @Override
+    public void generateStream(
+            @NonNull ChatCompletionRequest request,
+            @NonNull String workspaceId,
+            @NonNull Consumer<ChatCompletionResponse> handleMessage,
+            @NonNull Runnable handleClose, @NonNull Consumer<Throwable> handleError) {
+        return anthropicClient.createMessage(AnthropicCreateMessageRequest.builder()
+                .model(request.model())
+                .messages(request.messages().stream().map(this::mapMessage).toList())
+                .temperature(request.temperature())
+                .topP(request.topP())
+                .stopSequences(request.stop())
+                .maxTokens(request.maxTokens())
+                .toolChoice(AnthropicToolChoice.from(request.toolChoice().toString()))
+                .build(), new ChunkedResponseHandler(handleMessage, handleClose, handleError));
+
+        return ChatCompletionResponse.builder()
+                .id(response.id)
+                .model(response.model)
+                .choices(response.content.stream().map(content -> mapContentToChoice(response, content))
+                        .toList())
+                .usage(Usage.builder()
+                        .promptTokens(response.usage.inputTokens)
+                        .completionTokens(response.usage.outputTokens)
+                        .totalTokens(response.usage.inputTokens + response.usage.outputTokens)
+                        .build())
+                .build();
+    }
+
+    @Override
+    public WebApplicationException mapRuntimeException(RuntimeException runtimeException) {
+        if (runtimeException.getCause() instanceof AnthropicHttpException anthropicHttpException) {
+            if (anthropicHttpException.statusCode() >= 400 && anthropicHttpException.statusCode() <= 499) {
+                return new ClientErrorException(anthropicHttpException.getMessage(),
+                        anthropicHttpException.statusCode());
+            }
+
+            return new ServerErrorException(anthropicHttpException.getMessage(), anthropicHttpException.statusCode());
+        }
+
+        return new InternalServerErrorException(ChatCompletionService.UNEXPECTED_ERROR_CALLING_LLM_PROVIDER);
+    }
+
+    @Override
+    public ErrorMessage mapThrowableToError(Throwable throwable) {
+        if (throwable instanceof AnthropicHttpException anthropicHttpException) {
+            return new ErrorMessage(anthropicHttpException.statusCode(), anthropicHttpException.getMessage());
+        }
+
+        return new ErrorMessage(ChatCompletionService.UNEXPECTED_ERROR_CALLING_LLM_PROVIDER);
+    }
+
+    private AnthropicMessage mapMessage(Message message) {
+        if (message.role() == Role.ASSISTANT) {
+            return AnthropicMessage.builder()
+                    .role(AnthropicRole.ASSISTANT)
+                    .content(List.of(new AnthropicTextContent(((AssistantMessage) message).content())))
+                    .build();
+        } else if (message.role() == Role.USER) {
+            return AnthropicMessage.builder()
+                    .role(AnthropicRole.USER)
+                    .content(((UserMessage) message).contents().stream().map(this::mapMessageContent).toList())
+                    .build();
+        }
+
+        // Anthropic only supports assistant and user roles
+        throw new BadRequestException("not supported message role: " + message.role());
+    }
+
+    private AnthropicMessageContent mapMessageContent(Content content) {
+        if (content instanceof TextContent) {
+            return new AnthropicTextContent(((TextContent) content).text());
+        }
+
+        throw new BadRequestException("only text content is supported");
+    }
+
+    private ChatCompletionChoice mapContentToChoice(AnthropicCreateMessageResponse response, AnthropicContent content) {
+        return ChatCompletionChoice.builder()
+                .message(AssistantMessage.builder()
+                        .name(content.name)
+                        .content(content.text)
+                        .build())
+                .finishReason(response.stopReason)
+                .build();
+    }
+
+    private AnthropicClient newClient(String apiKey) {
+        var anthropicClientBuilder = AnthropicClient.builder();
+        Optional.ofNullable(llmProviderClientConfig.getOpenAiClient())
+                .map(LlmProviderClientConfig.OpenAiClientConfig::url)
+                .ifPresent(baseUrl -> {
+                    if (StringUtils.isNotBlank(baseUrl)) {
+                        anthropicClientBuilder.baseUrl(baseUrl);
+                    }
+                });
+        Optional.ofNullable(llmProviderClientConfig.getCallTimeout())
+                .ifPresent(callTimeout -> anthropicClientBuilder.timeout(callTimeout.toJavaDuration()));
+        return anthropicClientBuilder
+                .apiKey(apiKey)
+                .build();
+    }
+
+    private static class ChunkedResponseHandler implements StreamingResponseHandler<AiMessage> {
+        private final Consumer<ChatCompletionResponse> handleMessage;
+        private final Runnable handleClose;
+        private final Consumer<Throwable> handleError;
+
+        public ChunkedResponseHandler(
+                Consumer<ChatCompletionResponse> handleMessage, Runnable handleClose, Consumer<Throwable> handleError) {
+            this.handleMessage = handleMessage;
+            this.handleClose = handleClose;
+            this.handleError = handleError;
+        }
+
+        @Override
+        public void onNext(String s) {
+
+        }
+
+        @Override
+        public void onComplete(Response<AiMessage> response) {
+            StreamingResponseHandler.super.onComplete(response);
+        }
+
+        @Override
+        public void onError(Throwable throwable) {
+            handleError.accept(throwable);
+        }
+    }
+}
