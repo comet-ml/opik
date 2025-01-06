cba577328fd388c13e40469a18b7f12727ae87f1
Ido Berkovich <ido@comet.com>
2025-01-06 18:51:36 +0200

    OPIK-611 gemini e2e failing test green [WIP - only create]

--- apps/opik-backend/src/main/java/com/comet/opik/domain/llmproviders/Gemini.java
+++ apps/opik-backend/src/main/java/com/comet/opik/domain/llmproviders/Gemini.java
@@ -1,24 +1,86 @@
 package com.comet.opik.domain.llmproviders;
 
 import com.comet.opik.infrastructure.LlmProviderClientConfig;
+import dev.ai4j.openai4j.chat.AssistantMessage;
+import dev.ai4j.openai4j.chat.ChatCompletionChoice;
 import dev.ai4j.openai4j.chat.ChatCompletionRequest;
 import dev.ai4j.openai4j.chat.ChatCompletionResponse;
+import dev.ai4j.openai4j.chat.Message;
+import dev.ai4j.openai4j.shared.Usage;
+import dev.langchain4j.data.message.AiMessage;
+import dev.langchain4j.data.message.ChatMessage;
+import dev.langchain4j.data.message.SystemMessage;
+import dev.langchain4j.data.message.UserMessage;
+import dev.langchain4j.model.googleai.GoogleAiGeminiChatModel;
+import jakarta.ws.rs.BadRequestException;
 import lombok.NonNull;
 import org.apache.commons.lang3.NotImplementedException;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.function.Consumer;
 
 public class Gemini implements LlmProviderService {
     private final LlmProviderClientConfig llmProviderClientConfig;
+    private final String apiKey;
+
+    private static final String ERR_UNEXPECTED_ROLE = "unexpected role '%s'";
 
     public Gemini(LlmProviderClientConfig llmProviderClientConfig, String apiKey) {
         this.llmProviderClientConfig = llmProviderClientConfig;
+        this.apiKey = apiKey;
     }
 
     @Override
     public ChatCompletionResponse generate(@NonNull ChatCompletionRequest request, @NonNull String workspaceId) {
-        throw new NotImplementedException("Gemini not implemented yet");
+        GoogleAiGeminiChatModel client = GoogleAiGeminiChatModel.builder()
+                .apiKey(apiKey)
+                .modelName(request.model())
+                .maxOutputTokens(request.maxCompletionTokens())
+                .maxRetries(1)
+                .stopSequences(request.stop())
+                .temperature(request.temperature())
+                .topP(request.topP())
+                .timeout(llmProviderClientConfig.getCallTimeout().toJavaDuration())
+                .build();
+        var response = client.generate(request.messages().stream().map(this::toChatMessage).toList());
+
+        return ChatCompletionResponse.builder()
+                .model(request.model())
+                .choices(List.of(ChatCompletionChoice.builder()
+                        .message(AssistantMessage.builder().content(response.content().text()).build())
+                        .build()))
+                .usage(Usage.builder()
+                        .promptTokens(response.tokenUsage().inputTokenCount())
+                        .completionTokens(response.tokenUsage().outputTokenCount())
+                        .totalTokens(response.tokenUsage().totalTokenCount())
+                        .build())
+                .build();
+    }
+
+    private ChatMessage toChatMessage(Message message) {
+        switch (message.role()) {
+            case ASSISTANT -> {
+                return AiMessage.from(((AiMessage) message).text());
+            }
+            case USER -> {
+                return UserMessage
+                        .from(toStringMessageContent(((dev.ai4j.openai4j.chat.UserMessage) message).content()));
+            }
+            case SYSTEM -> {
+                return SystemMessage.from(((dev.ai4j.openai4j.chat.SystemMessage) message).content());
+            }
+        }
+
+        throw new BadRequestException(ERR_UNEXPECTED_ROLE.formatted(message.role()));
+    }
+
+    private String toStringMessageContent(Object rawContent) {
+        if (rawContent instanceof String content) {
+            return content;
+        }
+
+        throw new BadRequestException("only text content is supported");
     }
 
     @Override
@@ -30,7 +92,6 @@ public class Gemini implements LlmProviderService {
 
     @Override
     public void validateRequest(ChatCompletionRequest request) {
-        throw new NotImplementedException("Gemini not implemented yet");
     }
 
     @Override
