c2abc64d4d3962de07b6c17b800accc34f284680
Andres Cruz <andresc@comet.com>
2025-01-08 13:30:19 +0100

    Rev1: add documentation

--- apps/opik-backend/src/main/java/com/comet/opik/api/error/CannotDeleteProjectException.java
+++ apps/opik-backend/src/main/java/com/comet/opik/api/error/CannotDeleteProjectException.java
@@ -5,8 +5,7 @@ import jakarta.ws.rs.core.Response;
 
 public class CannotDeleteProjectException extends ClientErrorException {
 
-    public CannotDeleteProjectException(
-            ErrorMessage response) {
+    public CannotDeleteProjectException(ErrorMessage response) {
         super(Response.status(Response.Status.CONFLICT).entity(response).build());
     }
 }
