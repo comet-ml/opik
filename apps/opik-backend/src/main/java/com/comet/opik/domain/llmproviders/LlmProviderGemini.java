5f364f2cf76a162a3433bf912251279a39adbdde
Ido Berkovich <ido@comet.com>
2025-01-12 09:51:54 +0200

    OPIK-611 gemini mappers

--- apps/opik-backend/src/main/java/com/comet/opik/domain/llmproviders/LlmProviderGemini.java
+++ apps/opik-backend/src/main/java/com/comet/opik/domain/llmproviders/LlmProviderGemini.java
@@ -1,21 +1,11 @@
 package com.comet.opik.domain.llmproviders;
 
-import dev.ai4j.openai4j.chat.AssistantMessage;
-import dev.ai4j.openai4j.chat.ChatCompletionChoice;
 import dev.ai4j.openai4j.chat.ChatCompletionRequest;
 import dev.ai4j.openai4j.chat.ChatCompletionResponse;
-import dev.ai4j.openai4j.chat.Message;
-import dev.ai4j.openai4j.shared.Usage;
-import dev.langchain4j.data.message.AiMessage;
-import dev.langchain4j.data.message.ChatMessage;
-import dev.langchain4j.data.message.SystemMessage;
-import dev.langchain4j.data.message.UserMessage;
 import io.dropwizard.jersey.errors.ErrorMessage;
-import jakarta.ws.rs.BadRequestException;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.function.Consumer;
 
@@ -24,24 +14,13 @@ public class LlmProviderGemini implements LlmProviderService {
     private final @NonNull LlmProviderClientGenerator llmProviderClientGenerator;
     private final @NonNull String apiKey;
 
-    private static final String ERR_UNEXPECTED_ROLE = "unexpected role '%s'";
-
     @Override
     public ChatCompletionResponse generate(@NonNull ChatCompletionRequest request, @NonNull String workspaceId) {
+        var mapper = LlmProviderGeminiMapper.INSTANCE;
         var response = llmProviderClientGenerator.newGeminiClient(apiKey, request)
-                .generate(request.messages().stream().map(this::toChatMessage).toList());
+                .generate(request.messages().stream().map(mapper::toChatMessage).toList());
 
-        return ChatCompletionResponse.builder()
-                .model(request.model())
-                .choices(List.of(ChatCompletionChoice.builder()
-                        .message(AssistantMessage.builder().content(response.content().text()).build())
-                        .build()))
-                .usage(Usage.builder()
-                        .promptTokens(response.tokenUsage().inputTokenCount())
-                        .completionTokens(response.tokenUsage().outputTokenCount())
-                        .totalTokens(response.tokenUsage().totalTokenCount())
-                        .build())
-                .build();
+        return mapper.toChatCompletionResponse(request, response);
     }
 
     @Override
@@ -49,7 +28,7 @@ public class LlmProviderGemini implements LlmProviderService {
             @NonNull Consumer<ChatCompletionResponse> handleMessage, @NonNull Runnable handleClose,
             @NonNull Consumer<Throwable> handleError) {
         llmProviderClientGenerator.newGeminiStreamingClient(apiKey, request)
-                .generate(request.messages().stream().map(this::toChatMessage).toList(),
+                .generate(request.messages().stream().map(LlmProviderGeminiMapper.INSTANCE::toChatMessage).toList(),
                         new ChunkedResponseHandler(handleMessage, handleClose, handleError, request.model()));
     }
 
@@ -62,28 +41,4 @@ public class LlmProviderGemini implements LlmProviderService {
         return Optional.empty();
     }
 
-    private ChatMessage toChatMessage(Message message) {
-        switch (message.role()) {
-            case ASSISTANT -> {
-                return AiMessage.from(((AiMessage) message).text());
-            }
-            case USER -> {
-                return UserMessage
-                        .from(toStringMessageContent(((dev.ai4j.openai4j.chat.UserMessage) message).content()));
-            }
-            case SYSTEM -> {
-                return SystemMessage.from(((dev.ai4j.openai4j.chat.SystemMessage) message).content());
-            }
-        }
-
-        throw new BadRequestException(ERR_UNEXPECTED_ROLE.formatted(message.role()));
-    }
-
-    private String toStringMessageContent(Object rawContent) {
-        if (rawContent instanceof String content) {
-            return content;
-        }
-
-        throw new BadRequestException("only text content is supported");
-    }
 }
--- apps/opik-backend/src/test/java/com/comet/opik/domain/llmproviders/LlmProviderClientsMappersTest.java
+++ apps/opik-backend/src/test/java/com/comet/opik/domain/llmproviders/LlmProviderClientsMappersTest.java
@@ -3,18 +3,33 @@ package com.comet.opik.domain.llmproviders;
 import com.comet.opik.podam.PodamFactoryUtils;
 import dev.ai4j.openai4j.chat.AssistantMessage;
 import dev.ai4j.openai4j.chat.ChatCompletionChoice;
+import dev.ai4j.openai4j.chat.ChatCompletionRequest;
+import dev.ai4j.openai4j.chat.Message;
 import dev.ai4j.openai4j.shared.Usage;
+import dev.langchain4j.data.message.AiMessage;
+import dev.langchain4j.data.message.ChatMessage;
+import dev.langchain4j.data.message.SystemMessage;
+import dev.langchain4j.data.message.UserMessage;
 import dev.langchain4j.model.anthropic.internal.api.AnthropicContent;
 import dev.langchain4j.model.anthropic.internal.api.AnthropicCreateMessageResponse;
 import dev.langchain4j.model.anthropic.internal.api.AnthropicUsage;
+import dev.langchain4j.model.output.FinishReason;
+import dev.langchain4j.model.output.Response;
+import dev.langchain4j.model.output.TokenUsage;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import uk.co.jemos.podam.api.PodamFactory;
 
 import java.util.List;
+import java.util.stream.Stream;
 
+import static dev.langchain4j.data.message.AiMessage.aiMessage;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class LlmProviderClientsMappersTest {
     private final PodamFactory podamFactory = PodamFactoryUtils.newPodamFactory();
@@ -57,6 +72,47 @@ public class LlmProviderClientsMappersTest {
                     .completionTokens(usage.outputTokens)
                     .totalTokens(usage.inputTokens + usage.outputTokens)
                     .build());
+        }
+    }
+
+    @Nested
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    class GeminiMappers {
+        @Test
+        void testToResponse() {
+            var request = ChatCompletionRequest.builder().model(podamFactory.manufacturePojo(String.class)).build();
+            var response = new Response<>(aiMessage(podamFactory.manufacturePojo(String.class)),
+                    new TokenUsage(podamFactory.manufacturePojo(Integer.class),
+                            podamFactory.manufacturePojo(Integer.class)),
+                    FinishReason.STOP);
+            var actual = LlmProviderGeminiMapper.INSTANCE.toChatCompletionResponse(request, response);
+            assertThat(actual).isNotNull();
+            assertThat(actual.model()).isEqualTo(request.model());
+            assertThat(actual.choices()).isEqualTo(List.of(ChatCompletionChoice.builder()
+                    .message(AssistantMessage.builder().content(response.content().text()).build())
+                    .build()));
+            assertThat(actual.usage()).isEqualTo(Usage.builder()
+                    .promptTokens(response.tokenUsage().inputTokenCount())
+                    .completionTokens(response.tokenUsage().outputTokenCount())
+                    .totalTokens(response.tokenUsage().totalTokenCount())
+                    .build());
+        }
+
+        @ParameterizedTest
+        @MethodSource
+        void testToChatMessage(Message message, ChatMessage expected) {
+            ChatMessage actual = LlmProviderGeminiMapper.INSTANCE.toChatMessage(message);
+            assertThat(actual).isEqualTo(expected);
+        }
+
+        private Stream<Arguments> testToChatMessage() {
+            var content = podamFactory.manufacturePojo(String.class);
+            return Stream.of(
+                    arguments(AssistantMessage.builder().content(content).build(), AiMessage.from(content)),
+                    arguments(dev.ai4j.openai4j.chat.UserMessage.builder().content(content).build(),
+                            UserMessage.from(content)),
+                    arguments(dev.ai4j.openai4j.chat.SystemMessage.builder().content(content).build(),
+                            SystemMessage.from(content)));
         }
     }
 }
