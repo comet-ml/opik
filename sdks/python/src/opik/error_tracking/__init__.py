5e48f83b5a11cdc0bf3c40ca4738d1d858e323d8
Aliaksandr Kuzmik <alexander.kuzmik99@gmail.com>
2025-01-14 16:40:44 +0100

    Remove comet comment

--- sdks/python/src/opik/error_tracking/__init__.py
+++ sdks/python/src/opik/error_tracking/__init__.py
@@ -1,16 +1,3 @@
-# -*- coding: utf-8 -*-
-# *******************************************************
-#   ____                     _               _
-#  / ___|___  _ __ ___   ___| |_   _ __ ___ | |
-# | |   / _ \| '_ ` _ \ / _ \ __| | '_ ` _ \| |
-# | |__| (_) | | | | | |  __/ |_ _| | | | | | |
-#  \____\___/|_| |_| |_|\___|\__(_)_| |_| |_|_|
-#
-#  Sign up for free at https://www.comet.com
-#  Copyright (C) 2015-2021 Comet ML INC
-#  This source code is licensed under the MIT license.
-# *******************************************************
-
 from .api import (
     enabled_in_config,
     randomized_should_enable_reporting,
