b40c6fe2ef347eb3e81c87ade624d53de8b3a112
Borys Tkachenko <borystkachenko@ip-192-168-0-122.eu-west-1.compute.internal>
2025-01-15 10:52:30 +0100

    address comments

--- apps/opik-backend/src/main/java/com/comet/opik/api/error/JsonInvalidFormatExceptionMapper.java
+++ apps/opik-backend/src/main/java/com/comet/opik/api/error/JsonInvalidFormatExceptionMapper.java
@@ -1,11 +1,10 @@
 package com.comet.opik.api.error;
 
 import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import io.dropwizard.jersey.errors.ErrorMessage;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.ExceptionMapper;
 import lombok.extern.slf4j.Slf4j;
-
-import java.util.List;
 
 @Slf4j
 public class JsonInvalidFormatExceptionMapper implements ExceptionMapper<InvalidFormatException> {
@@ -15,8 +14,8 @@ public class JsonInvalidFormatExceptionMapper implements ExceptionMapper<Invalid
         log.info("Deserialization exception: {}", exception.getMessage());
 
         return Response.status(Response.Status.BAD_REQUEST)
-                .entity(new ErrorMessage(
-                        List.of("Unable to process JSON. " + exception.getMessage())))
+                .entity(new ErrorMessage(Response.Status.BAD_REQUEST.getStatusCode(),
+                        "Unable to process JSON. " + exception.getMessage()))
                 .build();
     }
 }
