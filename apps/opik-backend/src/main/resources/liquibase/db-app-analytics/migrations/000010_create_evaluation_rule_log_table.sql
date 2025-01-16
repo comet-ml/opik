755d147387cad5246820382bcf1900a502ce2d16
Thiago Hora <thiagoh@comet.com>
2025-01-16 12:57:11 +0100

    OPIK-795: Create rule logs table

---
+++ apps/opik-backend/src/main/resources/liquibase/db-app-analytics/migrations/000010_create_evaluation_rule_log_table.sql
@@ -0,0 +1,17 @@
+--liquibase formatted sql
+--changeset thiagohora:000010_create_automation_rule_log_table
+
+CREATE TABLE IF NOT EXISTS ${ANALYTICS_DB_DATABASE_NAME}.automation_rule_evaluator_logs (
+    timestamp DateTime64(9, 'UTC') DEFAULT now64(9),
+    workspace_id String,
+    rule_id  FixedString(36),
+    level Enum8('INFO'=0, 'ERROR'=1),
+    message String,
+    extra Map(String, String),
+    INDEX idx_workspace_rule_id (workspace_id, rule_id) TYPE bloom_filter(0.01)
+)
+ENGINE = MergeTree()
+ORDER BY (timestamp, workspace_id, rule_id, level)
+TTL toDateTime(timestamp + INTERVAL 6 MONTH);
+
+--rollback DROP TABLE IF EXISTS ${ANALYTICS_DB_DATABASE_NAME}.automation_rule_evaluator_logs;
