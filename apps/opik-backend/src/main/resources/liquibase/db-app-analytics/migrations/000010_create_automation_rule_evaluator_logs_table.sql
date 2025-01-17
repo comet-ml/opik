6ab6a1488b31a610f0259c3586aef2fac56eca53
Thiago Hora <thiagoh@comet.com>
2025-01-17 12:14:50 +0100

    Fix file name and column name

--- apps/opik-backend/src/main/resources/liquibase/db-app-analytics/migrations/000010_create_automation_rule_evaluator_logs_table.sql
+++ apps/opik-backend/src/main/resources/liquibase/db-app-analytics/migrations/000010_create_automation_rule_evaluator_logs_table.sql
@@ -1,13 +1,13 @@
 --liquibase formatted sql
---changeset thiagohora:000010_create_automation_rule_log_table
+--changeset thiagohora:create_automation_rule_evaluator_logs_table
 
 CREATE TABLE IF NOT EXISTS ${ANALYTICS_DB_DATABASE_NAME}.automation_rule_evaluator_logs (
     timestamp DateTime64(9, 'UTC') DEFAULT now64(9),
     workspace_id String,
     rule_id  FixedString(36),
-    level Enum8('INFO'=0, 'ERROR'=1, 'WARN'=2, 'DEBUG'=3, 'TRACE'=4),
+    level Enum8('TRACE'=0, 'DEBUG'=1, 'INFO'=2, 'WARM'=3, 'ERROR'=4),
     message String,
-    extra Map(String, String),
+    markers Map(String, String),
     INDEX idx_workspace_rule_id (workspace_id, rule_id) TYPE bloom_filter(0.01)
 )
 ENGINE = MergeTree()
