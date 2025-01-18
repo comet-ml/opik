fcabf1a11ff3c24d8614fa7a110966862d6616c8
Ido Berkovich <ido@comet.com>
2025-01-18 15:33:34 +0200

    OPIK-744 replace usages of existing ModelPrice

--- apps/opik-backend/src/main/java/com/comet/opik/domain/cost/CostService.java
+++ apps/opik-backend/src/main/java/com/comet/opik/domain/cost/CostService.java
@@ -14,20 +14,20 @@ import static com.comet.opik.domain.llmproviders.OpenaiModelName.GPT_4O_2024_08_
 import static com.comet.opik.domain.llmproviders.OpenaiModelName.GPT_4O_2024_11_20;
 
 public class CostService {
-    private static final Map<OpenaiModelName, ModelPriceNew> modelPrices = Map.of(
-            CHATGPT_4O_LATEST, new ModelPriceNew(new BigDecimal("0.000005"), new BigDecimal("0.000015"),
-                    SpanCostCalculatorNew::textGenerationCost),
-            GPT_4O, new ModelPriceNew(new BigDecimal("0.0000025"), new BigDecimal("0.000010"),
-                    SpanCostCalculatorNew::textGenerationCost),
-            GPT_4O_2024_05_13, new ModelPriceNew(new BigDecimal("0.000005"), new BigDecimal("0.000015"),
-                    SpanCostCalculatorNew::textGenerationCost),
-            GPT_4O_2024_08_06, new ModelPriceNew(new BigDecimal("0.0000025"), new BigDecimal("0.000010"),
-                    SpanCostCalculatorNew::textGenerationCost),
-            GPT_4O_2024_11_20, new ModelPriceNew(new BigDecimal("0.0000025"), new BigDecimal("0.000010"),
-                    SpanCostCalculatorNew::textGenerationCost));
+    private static final Map<OpenaiModelName, ModelPrice> modelPrices = Map.of(
+            CHATGPT_4O_LATEST, new ModelPrice(new BigDecimal("0.000005"), new BigDecimal("0.000015"),
+                    SpanCostCalculator::textGenerationCost),
+            GPT_4O, new ModelPrice(new BigDecimal("0.0000025"), new BigDecimal("0.000010"),
+                    SpanCostCalculator::textGenerationCost),
+            GPT_4O_2024_05_13, new ModelPrice(new BigDecimal("0.000005"), new BigDecimal("0.000015"),
+                    SpanCostCalculator::textGenerationCost),
+            GPT_4O_2024_08_06, new ModelPrice(new BigDecimal("0.0000025"), new BigDecimal("0.000010"),
+                    SpanCostCalculator::textGenerationCost),
+            GPT_4O_2024_11_20, new ModelPrice(new BigDecimal("0.0000025"), new BigDecimal("0.000010"),
+                    SpanCostCalculator::textGenerationCost));
 
-    private static final ModelPriceNew DEFAULT_COST = new ModelPriceNew(new BigDecimal("0"),
-            new BigDecimal("0"), SpanCostCalculatorNew::defaultCost);
+    private static final ModelPrice DEFAULT_COST = new ModelPrice(new BigDecimal("0"),
+            new BigDecimal("0"), SpanCostCalculator::defaultCost);
 
     public static BigDecimal calculateCost(@NonNull OpenaiModelName modelName, @NonNull Map<String, Integer> usage) {
         var modelPriceNew = Optional.ofNullable(modelPrices.get(modelName))
