b003ab17bcadfc45e2a14b59e05c588ed2c40f4a
Thiago dos Santos Hora <thiagoh@comet.com>
2025-01-20 17:34:45 +0100

    OPIK-796: Implement ClickHouse user facing logs (#1066)
    * OPIK-795: Create rule logs table
    
    * OPIK-796: Implement clickhouse user facing logs
    
    * Address PR review
    
    * OPIK-795: Create rule logs table
    
    * Fix file name and column name
    
    * Fix tests
    
    * Change table sortable key
    
    * Fix marker field ingestion
    
    * Address PR comments
    
    * Fix tests
    
    * Extract DAO from log appender
    
    ---------
    
    Co-authored-by: Andres Cruz <andresc@comet.com>

--- apps/opik-backend/src/main/java/com/comet/opik/api/resources/v1/events/OnlineScoringEventListener.java
+++ apps/opik-backend/src/main/java/com/comet/opik/api/resources/v1/events/OnlineScoringEventListener.java
@@ -2,25 +2,35 @@ package com.comet.opik.api.resources.v1.events;
 
 import com.comet.opik.api.AutomationRuleEvaluatorLlmAsJudge;
 import com.comet.opik.api.AutomationRuleEvaluatorType;
+import com.comet.opik.api.FeedbackScoreBatchItem;
 import com.comet.opik.api.Trace;
 import com.comet.opik.api.events.TracesCreated;
 import com.comet.opik.domain.AutomationRuleEvaluatorService;
 import com.comet.opik.domain.ChatCompletionService;
 import com.comet.opik.domain.FeedbackScoreService;
+import com.comet.opik.domain.UserLog;
 import com.comet.opik.infrastructure.auth.RequestContext;
+import com.comet.opik.infrastructure.log.UserFacingLoggingFactory;
 import com.google.common.eventbus.EventBus;
 import com.google.common.eventbus.Subscribe;
+import dev.langchain4j.model.chat.response.ChatResponse;
 import jakarta.inject.Inject;
+import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.MDC;
 import ru.vyarus.dropwizard.guice.module.installer.feature.eager.EagerSingleton;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
-import static com.comet.opik.api.AutomationRuleEvaluatorLlmAsJudge.LlmAsJudgeCode;
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.mapping;
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toMap;
 
 @EagerSingleton
 @Slf4j
@@ -29,16 +39,18 @@ public class OnlineScoringEventListener {
     private final AutomationRuleEvaluatorService ruleEvaluatorService;
     private final ChatCompletionService aiProxyService;
     private final FeedbackScoreService feedbackScoreService;
+    private final @NonNull Logger userFacingLogger;
 
     @Inject
-    public OnlineScoringEventListener(EventBus eventBus,
-            AutomationRuleEvaluatorService ruleEvaluatorService,
-            ChatCompletionService aiProxyService,
-            FeedbackScoreService feedbackScoreService) {
+    public OnlineScoringEventListener(@NonNull EventBus eventBus,
+            @NonNull AutomationRuleEvaluatorService ruleEvaluatorService,
+            @NonNull ChatCompletionService aiProxyService,
+            @NonNull FeedbackScoreService feedbackScoreService) {
         this.ruleEvaluatorService = ruleEvaluatorService;
         this.aiProxyService = aiProxyService;
         this.feedbackScoreService = feedbackScoreService;
         eventBus.register(this);
+        userFacingLogger = UserFacingLoggingFactory.getLogger(OnlineScoringEventListener.class);
     }
 
     /**
@@ -53,10 +65,10 @@ public class OnlineScoringEventListener {
         log.debug(tracesBatch.traces().toString());
 
         Map<UUID, List<Trace>> tracesByProject = tracesBatch.traces().stream()
-                .collect(Collectors.groupingBy(Trace::projectId));
+                .collect(groupingBy(Trace::projectId));
 
         Map<String, Integer> countMap = tracesByProject.entrySet().stream()
-                .collect(Collectors.toMap(entry -> "projectId: " + entry.getKey(),
+                .collect(toMap(entry -> "projectId: " + entry.getKey(),
                         entry -> entry.getValue().size()));
 
         log.debug("Received traces for workspace '{}': {}", tracesBatch.workspaceId(), countMap);
@@ -72,11 +84,30 @@ public class OnlineScoringEventListener {
             log.info("Found {} evaluators for project '{}' on workspace '{}'", evaluators.size(),
                     projectId, tracesBatch.workspaceId());
 
-            // for each rule, sample traces and score them
-            evaluators.forEach(evaluator -> traces.stream()
-                    .filter(e -> random.nextFloat() < evaluator.getSamplingRate())
-                    .forEach(trace -> score(trace, evaluator.getCode(), tracesBatch.workspaceId(),
-                            tracesBatch.userName())));
+            // Important to set the workspaceId for logging purposes
+            try (MDC.MDCCloseable logScope = MDC.putCloseable(UserLog.MARKER, UserLog.AUTOMATION_RULE_EVALUATOR.name());
+                    MDC.MDCCloseable scope = MDC.putCloseable("workspace_id", tracesBatch.workspaceId())) {
+
+                // for each rule, sample traces and score them
+                evaluators.forEach(evaluator -> traces.stream()
+                        .filter(trace -> {
+                            boolean sampled = random.nextFloat() < evaluator.getSamplingRate();
+
+                            if (!sampled) {
+                                MDC.put("rule_id", evaluator.getId().toString());
+                                MDC.put("trace_id", trace.id().toString());
+
+                                userFacingLogger.info(
+                                        "The traceId '{}' was skipped for rule: '{}' and per the sampling rate '{}'",
+                                        trace.id(), evaluator.getName(), evaluator.getSamplingRate());
+                            }
+
+                            return sampled;
+                        })
+                        .forEach(trace -> score(trace, evaluator, tracesBatch.workspaceId(),
+                                tracesBatch.userName())));
+            }
+
         });
     }
 
@@ -85,31 +116,66 @@ public class OnlineScoringEventListener {
      * If the evaluator has multiple score definitions, it calls the LLM once per score definition.
      *
      * @param trace         the trace to score
-     * @param evaluatorCode the automation rule to score the trace
+     * @param evaluator     the automation rule to score the trace
      * @param workspaceId   the workspace the trace belongs
      */
-    private void score(Trace trace, LlmAsJudgeCode evaluatorCode, String workspaceId,
+    private void score(Trace trace, AutomationRuleEvaluatorLlmAsJudge evaluator, String workspaceId,
             String userName) {
 
-        var scoreRequest = OnlineScoringEngine.prepareLlmRequest(evaluatorCode, trace);
+        //This is crucial for logging purposes to identify the rule and trace
+        try (var ruleScope = MDC.putCloseable("rule_id", evaluator.getId().toString());
+                var traceScope = MDC.putCloseable("trace_id", trace.id().toString())) {
 
-        var chatResponse = aiProxyService.scoreTrace(scoreRequest, evaluatorCode.model(), workspaceId);
+            processScores(trace, evaluator, workspaceId, userName);
+        }
+    }
 
-        var scores = OnlineScoringEngine.toFeedbackScores(chatResponse).stream()
-                .map(item -> item.toBuilder()
-                        .id(trace.id())
-                        .projectId(trace.projectId())
-                        .projectName(trace.projectName())
-                        .build())
-                .toList();
+    private void processScores(Trace trace, AutomationRuleEvaluatorLlmAsJudge evaluator, String workspaceId,
+            String userName) {
+        userFacingLogger.info("Evaluating traceId '{}' sampled by rule '{}'", trace.id(), evaluator.getName());
 
-        log.info("Received {} scores for traceId '{}' in workspace '{}'. Storing them.", scores.size(), trace.id(),
-                workspaceId);
+        var scoreRequest = OnlineScoringEngine.prepareLlmRequest(evaluator.getCode(), trace);
 
-        feedbackScoreService.scoreBatchOfTraces(scores)
-                .contextWrite(ctx -> ctx.put(RequestContext.USER_NAME, userName)
-                        .put(RequestContext.WORKSPACE_ID, workspaceId))
-                .block();
+        userFacingLogger.info("Sending traceId '{}' to LLM using the following input:\n\n{}", trace.id(), scoreRequest);
+
+        final ChatResponse chatResponse;
+
+        try {
+            chatResponse = aiProxyService.scoreTrace(scoreRequest, evaluator.getCode().model(), workspaceId);
+            userFacingLogger.info("Received response for traceId '{}':\n\n{}", trace.id(), chatResponse);
+        } catch (Exception e) {
+            userFacingLogger.error("Unexpected error while scoring traceId '{}' with rule '{}'", trace.id(),
+                    evaluator.getName());
+            throw e;
+        }
+
+        try {
+            var scores = OnlineScoringEngine.toFeedbackScores(chatResponse).stream()
+                    .map(item -> item.toBuilder()
+                            .id(trace.id())
+                            .projectId(trace.projectId())
+                            .projectName(trace.projectName())
+                            .build())
+                    .toList();
+
+            log.info("Received {} scores for traceId '{}' in workspace '{}'. Storing them.", scores.size(), trace.id(),
+                    workspaceId);
+
+            feedbackScoreService.scoreBatchOfTraces(scores)
+                    .contextWrite(ctx -> ctx.put(RequestContext.USER_NAME, userName)
+                            .put(RequestContext.WORKSPACE_ID, workspaceId))
+                    .block();
+
+            Map<String, List<BigDecimal>> loggedScores = scores
+                    .stream()
+                    .collect(
+                            groupingBy(FeedbackScoreBatchItem::name, mapping(FeedbackScoreBatchItem::value, toList())));
+
+            userFacingLogger.info("Scores for traceId '{}' stored successfully:\n\n{}", trace.id(), loggedScores);
+        } catch (Exception e) {
+            userFacingLogger.error("Unexpected error while storing scores for traceId '{}'", trace.id());
+            throw e;
+        }
     }
 
 }
