2af70c52ec0f22ebe1e6e6147662ac4c8287b8e7
Borys Tkachenko <borystkachenko@ip-192-168-0-122.eu-west-1.compute.internal>
2025-01-21 16:31:09 +0100

    address comments

--- apps/opik-backend/src/main/java/com/comet/opik/utils/ErrorUtils.java
+++ apps/opik-backend/src/main/java/com/comet/opik/utils/ErrorUtils.java
@@ -3,6 +3,7 @@ package com.comet.opik.utils;
 import io.dropwizard.jersey.errors.ErrorMessage;
 import jakarta.ws.rs.NotFoundException;
 import jakarta.ws.rs.core.Response;
+import lombok.NonNull;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
 
@@ -12,12 +13,12 @@ import java.util.UUID;
 @Slf4j
 public class ErrorUtils {
 
-    public static NotFoundException failWithNotFound(String entity, UUID id) {
+    public static NotFoundException failWithNotFound(@NonNull String entity, @NonNull UUID id) {
         String message = "%s id: %s not found".formatted(entity, id);
         return failWithNotFound(message);
     }
 
-    public static NotFoundException failWithNotFound(String message) {
+    public static NotFoundException failWithNotFound(@NonNull String message) {
         log.info(message);
         return new NotFoundException(Response.status(404)
                 .entity(new ErrorMessage(message)).build());
