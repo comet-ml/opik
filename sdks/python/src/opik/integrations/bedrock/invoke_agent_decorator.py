64add5fb1b6fa9e1821811b39b6ca7c0881610be
Alexander Barannikov <japdubengsub@gmail.com>
2025-01-29 12:14:50 +0000

    remove unused code, extract helper class

--- sdks/python/src/opik/integrations/bedrock/invoke_agent_decorator.py
+++ sdks/python/src/opik/integrations/bedrock/invoke_agent_decorator.py
@@ -1,23 +1,14 @@
 import logging
-from typing import List, Any, Dict, Optional, Callable, Tuple, Union, TypedDict, cast
+from typing import Any, Callable, Dict, List, Optional, Tuple, Union, cast
+
 from opik import dict_utils
-from opik.decorator import base_track_decorator, arguments_helpers
-
-from . import stream_wrappers
-
-from botocore import eventstream
+from opik.decorator import arguments_helpers, base_track_decorator
+from . import helpers, stream_wrappers
 
 LOGGER = logging.getLogger(__name__)
 
 KWARGS_KEYS_TO_LOG_AS_INPUTS = ["inputText"]
 RESPONSE_KEYS_TO_LOG_AS_OUTPUTS = ["output"]
-
-BedrockResponseWithStream = Dict[str, Any]
-
-
-class ConverseStreamOutput(TypedDict):
-    stream: eventstream.EventStream
-    ResponseMetadata: Dict[str, Any]
 
 
 class BedrockInvokeAgentDecorator(base_track_decorator.BaseTrackDecorator):
@@ -77,7 +68,7 @@ class BedrockInvokeAgentDecorator(base_track_decorator.BaseTrackDecorator):
         capture_output: bool,
         generations_aggregator: Optional[Callable[[List[Any]], Any]],
     ) -> Union[
-        ConverseStreamOutput,
+        helpers.ConverseStreamOutput,
         None,
     ]:
         DECORATED_FUNCTION_IS_NOT_EXPECTED_TO_RETURN_GENERATOR = (
@@ -103,7 +94,7 @@ class BedrockInvokeAgentDecorator(base_track_decorator.BaseTrackDecorator):
             )
 
             output["completion"] = wrapped_stream
-            return cast(ConverseStreamOutput, output)
+            return cast(helpers.ConverseStreamOutput, output)
 
         STREAM_NOT_FOUND = None
 
