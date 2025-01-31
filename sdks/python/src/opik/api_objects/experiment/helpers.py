7349aa32946537846d97459f8842555725e2d6c0
Alexander Barannikov <japdubengsub@gmail.com>
2025-01-31 12:30:23 +0000

    update build_metadata_and_prompt_versions helper func

--- sdks/python/src/opik/api_objects/experiment/helpers.py
+++ sdks/python/src/opik/api_objects/experiment/helpers.py
@@ -1,19 +1,21 @@
-from typing import Optional, Dict, Mapping, Tuple, Any
-from .. import prompt
 import logging
-from opik import jsonable_encoder
+from typing import Any, Dict, List, Mapping, Optional, Tuple
+
+from opik import exceptions, jsonable_encoder
 from opik.rest_api import OpikApi
 from opik.rest_api.types import experiment_public
-from opik import exceptions
+from .. import prompt
 
 LOGGER = logging.getLogger(__name__)
 
+PromptVersion = Dict[str, str]
 
-def build_metadata_and_prompt_version(
-    experiment_config: Optional[Dict[str, Any]], prompt: Optional[prompt.Prompt]
-) -> Tuple[Optional[Dict[str, Any]], Optional[Dict[str, str]]]:
-    metadata = None
-    prompt_version: Optional[Dict[str, str]] = None
+
+def build_metadata_and_prompt_versions(
+    experiment_config: Optional[Dict[str, Any]],
+    prompts: Optional[List[prompt.Prompt]],
+) -> Tuple[Optional[Dict[str, Any]], Optional[List[PromptVersion]]]:
+    prompt_versions: Optional[List[PromptVersion]] = None
 
     if experiment_config is None:
         experiment_config = {}
@@ -25,22 +27,26 @@ def build_metadata_and_prompt_version(
         )
         experiment_config = {}
 
-    if prompt is not None and "prompt" in experiment_config:
+    if prompts is not None and len(prompts) > 0 and "prompts" in experiment_config:
         LOGGER.warning(
-            "The prompt parameter will not be added to experiment since there is already `prompt` specified in experiment_config"
+            "The `prompts` parameter will not be added to experiment since there is already `prompts` specified in experiment_config"
         )
-        return (experiment_config, None)
+        return experiment_config, None
 
-    if prompt is not None:
-        prompt_version = {"id": prompt.__internal_api__version_id__}
-        experiment_config["prompt"] = prompt.prompt
+    if prompts is not None and len(prompts) > 0:
+        prompt_versions = []
+        experiment_config["prompts"] = []
+
+        for prompt in prompts:
+            prompt_versions.append({"id": prompt.__internal_api__version_id__})
+            experiment_config["prompts"].append(prompt.prompt)
 
     if experiment_config == {}:
         return None, None
 
     metadata = jsonable_encoder.jsonable_encoder(experiment_config)
 
-    return metadata, prompt_version
+    return metadata, prompt_versions
 
 
 def get_experiment_data_by_name(
@@ -65,7 +71,6 @@ def check_prompt_args(
     prompt: Optional[prompt.Prompt] = None,
     prompts: Optional[List[prompt.Prompt]] = None,
 ) -> Optional[List[prompt.Prompt]]:
-
     if prompts is not None and len(prompts) > 0 and prompt is not None:
         LOGGER.warning("Arguments `prompt` and `prompts` are mutually exclusive, `prompts` will be used`.")
     elif prompt is not None:
--- sdks/python/tests/unit/api_objects/experiment/test_helpers.py
+++ sdks/python/tests/unit/api_objects/experiment/test_helpers.py
@@ -1,71 +1,74 @@
 import types
-from typing import cast
 
 import pytest
 
-from opik import Prompt
 from opik.api_objects import experiment
 from tests.testlib.util_helpers import random_string
 
 
-def fake_prompt():
+def fake_prompt(with_postfix: bool = False):
     postfix = random_string()
+
     fake_prompt_obj = types.SimpleNamespace(
-        __internal_api__version_id__=f"some-prompt-version-id-{postfix}",
-        prompt=f"some-prompt-value-{postfix}",
+        __internal_api__version_id__="some-prompt-version-id",
+        prompt=f"some-prompt-value",
     )
 
-    return cast(fake_prompt_obj, Prompt)
+    if with_postfix:
+        fake_prompt_obj.prompt += postfix
+        fake_prompt_obj.__internal_api__version_id__ += postfix
+
+    return fake_prompt_obj
 
 
 @pytest.mark.parametrize(
     argnames="input_kwargs,expected",
     argvalues=[
         (
-            {"experiment_config": None, "prompt": None},
-            {"metadata": None, "prompt_version": None},
+            {"experiment_config": None, "prompts": None},
+            {"metadata": None, "prompt_versions": None},
         ),
         (
-            {"experiment_config": {}, "prompt": None},
-            {"metadata": None, "prompt_version": None},
+            {"experiment_config": {}, "prompts": None},
+            {"metadata": None, "prompt_versions": None},
         ),
         (
-            {"experiment_config": None, "prompt": fake_prompt()},
+            {"experiment_config": None, "prompts": [fake_prompt()]},
             {
-                "metadata": {"prompt": "some-prompt-value"},
-                "prompt_version": {"id": "some-prompt-version-id"},
+                "metadata": {"prompts": ["some-prompt-value"]},
+                "prompt_versions": [{"id": "some-prompt-version-id"}],
             },
         ),
         (
-            {"experiment_config": {}, "prompt": fake_prompt()},
+            {"experiment_config": {}, "prompts": [fake_prompt()]},
             {
-                "metadata": {"prompt": "some-prompt-value"},
-                "prompt_version": {"id": "some-prompt-version-id"},
+                "metadata": {"prompts": ["some-prompt-value"]},
+                "prompt_versions": [{"id": "some-prompt-version-id"}],
             },
         ),
         (
-            {"experiment_config": {"some-key": "some-value"}, "prompt": None},
-            {"metadata": {"some-key": "some-value"}, "prompt_version": None},
+            {"experiment_config": {"some-key": "some-value"}, "prompts": None},
+            {"metadata": {"some-key": "some-value"}, "prompt_versions": None},
         ),
         (
             {
                 "experiment_config": "NOT-DICT-VALUE-THAT-WILL-BE-IGNORED-AND-REPLACED-WITH-DICT-WITH-PROMPT",
-                "prompt": fake_prompt(),
+                "prompts": [fake_prompt()],
             },
             {
-                "metadata": {"prompt": "some-prompt-value"},
-                "prompt_version": {"id": "some-prompt-version-id"},
+                "metadata": {"prompts": ["some-prompt-value"]},
+                "prompt_versions": [{"id": "some-prompt-version-id"}],
             },
         ),
     ],
 )
-def test_experiment_build_metadata_from_prompt_version(input_kwargs, expected):
-    metadata, prompt_version = experiment.build_metadata_and_prompt_version(
+def test_experiment_build_metadata_from_prompt_versions(input_kwargs, expected):
+    metadata, prompt_versions = experiment.build_metadata_and_prompt_versions(
         **input_kwargs
     )
 
     assert metadata == expected["metadata"]
-    assert prompt_version == expected["prompt_version"]
+    assert prompt_versions == expected["prompt_versions"]
 
 
 def test_check_prompt_args_with_none_arguments():
@@ -79,7 +82,7 @@ def test_check_prompt_args_with_none_and_empty_list():
 
 
 def test_check_prompt_args_with_single_prompt():
-    mock_prompt = fake_prompt()
+    mock_prompt = fake_prompt(with_postfix=True)
     result = experiment.check_prompt_args(prompt=mock_prompt, prompts=None)
     assert isinstance(result, list)
     assert len(result) == 1
@@ -87,18 +90,18 @@ def test_check_prompt_args_with_single_prompt():
 
 
 def test_check_prompt_args_with_prompts_list():
-    mock_prompt_1 = fake_prompt()
-    mock_prompt_2 = fake_prompt()
+    mock_prompt_1 = fake_prompt(with_postfix=True)
+    mock_prompt_2 = fake_prompt(with_postfix=True)
     prompts = [mock_prompt_1, mock_prompt_2]
     result = experiment.check_prompt_args(prompt=None, prompts=prompts)
     assert result == prompts
 
 
 def test_check_prompt_args_with_both_prompt_and_prompts():
-    mock_prompt = fake_prompt()
+    mock_prompt = fake_prompt(with_postfix=True)
     mock_prompt_list = [
-        fake_prompt(),
-        fake_prompt(),
+        fake_prompt(with_postfix=True),
+        fake_prompt(with_postfix=True),
     ]
     result = experiment.check_prompt_args(prompt=mock_prompt, prompts=mock_prompt_list)
     assert isinstance(result, list)
