4075dd5c3eaecc2ea6ed14dbeb9e52b0834951a8
Jacques Verre <jverre@gmail.com>
2025-01-31 15:32:44 +0000

    Update docs

--- apps/opik-documentation/documentation/dev_script.sh
+++ apps/opik-documentation/documentation/dev_script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Convert the files to markdown if that has not already been done
-jupyter nbconvert cookbook/*.ipynb --to markdown
+jupyter nbconvert -ClearOutputPreprocessor.enabled=True cookbook/*.ipynb --to markdown && for file in cookbook/*.md; do mv "$file" "${file%.md}.mdx"; done
 
 # Start the docs server and rebuild the cookbooks on change
 npx concurrently \
   "mintlify dev" \
-  "nodemon --watch docs/cookbook/ -e ipynb --exec 'jupyter nbconvert docs/cookbook/*.ipynb --to markdown'"
+  "nodemon --watch docs/cookbook/ -e ipynb --exec 'jupyter nbconvert --ClearOutputPreprocessor.enabled=True docs/cookbook/*.ipynb --to markdown && for file in docs/cookbook/*.md; do mv "$file" "${file%.md}.mdx"; done'"
