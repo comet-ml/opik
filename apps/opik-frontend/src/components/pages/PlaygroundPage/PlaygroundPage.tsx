0acb8e2ec59b3accda7971d5ae88fd39fbfe7f7c
Sasha <aliaksandr@comet.com>
2025-02-06 15:35:34 +0100

    [OPIK-657]: lint issues;

--- apps/opik-frontend/src/components/pages/PlaygroundPage/PlaygroundPage.tsx
+++ apps/opik-frontend/src/components/pages/PlaygroundPage/PlaygroundPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useEffect, useMemo } from "react";
 import { Loader } from "lucide-react";
 import useLocalStorageState from "use-local-storage-state";
 
