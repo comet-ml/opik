0c3fe5f84922974de8a5dd117513898e7d1f8bc9
Jacques Verré <jverre@gmail.com>
2025-02-06 09:57:50 +0000

    Jacques/method list datasets (#1189)
    * Add new endpoints for getting datasets and experiments
    
    * Fix linter
    
    * Address code review feedback
    
    * Fix tests
    
    ---------
    
    Co-authored-by: Aliaksandr Kuzmik <98702584+alexkuzmik@users.noreply.github.com>

--- sdks/python/src/opik/api_objects/experiment/helpers.py
+++ sdks/python/src/opik/api_objects/experiment/helpers.py
@@ -1,9 +1,6 @@
 import logging
+from opik import jsonable_encoder
 from typing import Any, Dict, List, Mapping, Optional, Tuple
-
-from opik import exceptions, jsonable_encoder
-from opik.rest_api import OpikApi
-from opik.rest_api.types import experiment_public
 from .. import prompt
 
 LOGGER = logging.getLogger(__name__)
@@ -47,24 +44,6 @@ def build_metadata_and_prompt_versions(
     metadata = jsonable_encoder.jsonable_encoder(experiment_config)
 
     return metadata, prompt_versions
-
-
-def get_experiment_data_by_name(
-    rest_client: OpikApi, name: str
-) -> experiment_public.ExperimentPublic:
-    page = 0
-
-    while True:
-        page += 1
-        experiment_page_public = rest_client.experiments.find_experiments(name=name)
-        if len(experiment_page_public.content) == 0:
-            raise exceptions.ExperimentNotFound(
-                f"Experiment with the name {name} not found."
-            )
-
-        for experiment in experiment_page_public.content:
-            if experiment.name == name:
-                return experiment
 
 
 def handle_prompt_args(
