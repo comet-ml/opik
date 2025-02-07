6c5e808c7c75151c631f4ca845b94b50be0ea93c
Jacques Verre <jverre@gmail.com>
2025-02-07 19:31:50 +0000

    Docs update

--- apps/opik-documentation/documentation/dev_script.sh
+++ apps/opik-documentation/documentation/dev_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Convert the files to markdown if that has not already been done
-jupyter nbconvert -ClearOutputPreprocessor.enabled=True cookbook/*.ipynb --to markdown && for file in cookbook/*.md; do mv "$file" "${file%.md}.mdx"; done
+jupyter nbconvert -ClearOutputPreprocessor.enabled=True docs/cookbook/*.ipynb --to markdown && for file in docs/cookbook/*.md; do mv "$file" "${file%.md}.mdx"; done
 
 # Start the docs server and rebuild the cookbooks on change
 npx concurrently \
