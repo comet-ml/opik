835ab8027ed08225fe2ea08e35fe7988a1000c5c
Fernando Carril <fernando@comet.com>
2025-02-11 09:41:21 +0100

    Initial version of test AI SDK with tools

--- sdks/typescript/tests/ai-sdk.test.ts
+++ sdks/typescript/tests/ai-sdk.test.ts
@@ -1,16 +1,18 @@
+import { trace } from "@opentelemetry/api";
 import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
 import { NodeSDK } from "@opentelemetry/sdk-node";
-import { generateText } from "ai";
+import { generateText, tool } from "ai";
 import { MockLanguageModelV1 } from "ai/test";
 import { Opik } from "opik";
 import { OpikExporter } from "opik/vercel";
 import { MockInstance } from "vitest";
+import { z } from "zod";
 
 async function mockAPIPromise<T>() {
   return {} as T;
 }
 
-describe.only("Opik - Vercel AI SDK integration", () => {
+describe("Opik - Vercel AI SDK integration", () => {
   let client: Opik;
   let sdk: NodeSDK;
   let createSpansSpy: MockInstance<typeof client.api.spans.createSpans>;
@@ -19,6 +21,9 @@ describe.only("Opik - Vercel AI SDK integration", () => {
   let updateTracesSpy: MockInstance<typeof client.api.traces.updateTrace>;
 
   beforeEach(() => {
+    // Important! It resets the SDK to its default state
+    trace.disable();
+
     client = new Opik({
       projectName: "opik-sdk-typescript",
     });
@@ -92,6 +97,93 @@ describe.only("Opik - Vercel AI SDK integration", () => {
           metadata: {},
           name: traceName,
           output: { text },
+          projectName: "opik-sdk-typescript",
+          usage: {
+            completion_tokens: 20,
+            prompt_tokens: 10,
+            total_tokens: 30,
+          },
+        },
+      ],
+    });
+  });
+
+  it("generateText with tools", async () => {
+    const traceName = "trace-name-example";
+
+    sdk.start();
+
+    const calculator = tool({
+      description: "calculate the sum of two numbers",
+      parameters: z.object({ a: z.number(), b: z.number() }),
+      execute: async ({ a, b }) => a + b,
+    });
+
+    const response = await generateText({
+      model: new MockLanguageModelV1({
+        doGenerate: async () => ({
+          rawCall: { rawPrompt: null, rawSettings: {} },
+          finishReason: "tool-calls",
+          usage: { promptTokens: 10, completionTokens: 20 },
+          toolCalls: [
+            {
+              toolCallType: "function",
+              toolCallId: "1",
+              toolName: "calculator",
+              args: JSON.stringify({ a: 2, b: 4 }),
+            },
+          ],
+        }),
+      }),
+      messages: [
+        {
+          role: "user",
+          content: "How much is 2 + 4?",
+        },
+      ],
+      tools: { calculator },
+      experimental_telemetry: OpikExporter.getSettings({
+        name: traceName,
+      }),
+    });
+
+    let result = "";
+    if (response.toolCalls) {
+      const [toolCall] = response.toolCalls;
+      const toolResult = await calculator.execute(toolCall.args, {
+        toolCallId: toolCall.toolCallId,
+        messages: [
+          {
+            role: "user",
+            content: "How much is 2 + 4?",
+          },
+        ],
+      });
+      result = toolResult.toString();
+    }
+
+    await sdk.shutdown();
+
+    expect(result).toBe("6");
+    expect(createTracesSpy).toHaveBeenCalledTimes(1);
+    expect(createSpansSpy).toHaveBeenCalledTimes(1);
+    expect(updateSpansSpy).toHaveBeenCalledTimes(0);
+    expect(updateTracesSpy).toHaveBeenCalledTimes(0);
+    expect(createTracesSpy.mock.calls[0][0]).toMatchObject({
+      traces: [
+        {
+          input: {
+            messages: [
+              {
+                role: "user",
+                content: "How much is 2 + 4?",
+              },
+            ],
+          },
+          metadata: {},
+          name: traceName,
+          // @todo: if possible implement testing telemetry for tool calls
+          output: {},
           projectName: "opik-sdk-typescript",
           usage: {
             completion_tokens: 20,
