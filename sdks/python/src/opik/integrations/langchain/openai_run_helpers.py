36d3e54fa255345875d5595d9d66f4ccd8a81a73
Alexander Barannikov <japdubengsub@gmail.com>
2025-02-12 17:51:10 +0000

    more precise model version detection

--- sdks/python/src/opik/integrations/langchain/openai_run_helpers.py
+++ sdks/python/src/opik/integrations/langchain/openai_run_helpers.py
@@ -34,7 +34,7 @@ def _try_get_token_usage(run_dict: Dict[str, Any]) -> Optional[UsageDict]:
             token_usage = run_dict["outputs"]["llm_output"]["token_usage"]
 
         # streaming mode handling
-        # token usage data will be available at the end of streaming
+        # token usage data MAY be available at the end of streaming
         # in async mode may not provide token usage info
         elif token_usage_full := run_dict["outputs"]["generations"][-1][-1]["message"][
             "kwargs"
@@ -96,13 +96,21 @@ def _get_provider_and_model(
     provider = None
     model = None
 
+    # here will be always provider and model
+    # but model name may not be full (model+version)
     if metadata := run_dict["extra"].get("metadata"):
         provider = metadata.get("ls_provider")
         model = metadata.get("ls_model_name")
 
+    # try to detect model+version more precise way if possible
+    # .invoke() mode
     if llm_output := run_dict["outputs"].get("llm_output"):
         model = llm_output.get("model_name", model)
+    # streaming mode
+    elif generation_info := run_dict['outputs']['generations'][-1][-1]["generation_info"]:
+        model = generation_info.get('model_name', model)
 
+    # provider: check base url
     if base_url := run_dict["extra"].get("invocation_params", {}).get("base_url"):
         if base_url.host != "api.openai.com":
             provider = base_url.host
