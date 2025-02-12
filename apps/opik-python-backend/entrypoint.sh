c105e5403f3cc564a78c2e10e71ab463f8d7cfad
Andres Cruz <andresc@comet.com>
2025-02-12 18:09:58 +0100

    OPIK-980: Embed opik-sandbox-executor-python image during builds

--- apps/opik-python-backend/entrypoint.sh
+++ apps/opik-python-backend/entrypoint.sh
@@ -5,27 +5,27 @@ set -e
 echo "Starting the Docker daemon as background process"
 dockerd-entrypoint.sh &
 
-sleep_seconds=1
-attempts=1
-max_attempts=5
+SLEEP_SECONDS=1
+MAX_ATTEMPTS=5
 
-until docker info >/dev/null 2>&1 || [ $attempts -ge $max_attempts ]; do
-  echo "Waiting ${sleep_seconds}s for the Docker daemon to start, attempt: $attempts"
-  sleep $sleep_seconds
+attempts=1
+
+until docker info >/dev/null 2>&1 || [ $attempts -ge $MAX_ATTEMPTS ]; do
+  echo "Waiting ${SLEEP_SECONDS}s for the Docker daemon to start, attempt: $attempts"
+  sleep $SLEEP_SECONDS
   attempts=$((attempts+1))
 done
 
-if [ $attempts -ge $max_attempts ]; then
-  echo "Docker daemon did not start after $max_attempts attempts"
+if [ $attempts -ge $MAX_ATTEMPTS ]; then
+  echo "Docker daemon did not start after $MAX_ATTEMPTS attempts"
   exit 1
 fi
 
 echo "Docker daemon started successfully after $attempts attempts"
 
-# Pre-pulling the to warm up the server the first time it attempts to run the image
-echo "Pulling the Opik Sandbox Executor Python image"
-docker pull "$PYTHON_CODE_EXECUTOR_IMAGE_REGISTRY"/"$PYTHON_CODE_EXECUTOR_IMAGE_NAME":"$PYTHON_CODE_EXECUTOR_IMAGE_TAG"
-echo "Successfully pulled the Opik Sandbox Executor Python image"
+echo "Loading the Opik Sandbox Executor Python image"
+docker load < "./images/${PYTHON_CODE_EXECUTOR_ASSET_NAME}"
+echo "Successfully loaded the Opik Sandbox Executor Python image"
 
 echo "Starting the Opik Python Backend server"
 gunicorn --workers 4 --bind=0.0.0.0:8000 --chdir ./src 'opik_backend:create_app()'
