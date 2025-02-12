e09fe0a08d490fb120a5f7c4f34a8f1c2a359164
Jacques Verre <jverre@gmail.com>
2025-02-12 23:14:50 +0000

    Fix docs issues

--- apps/opik-documentation/documentation/dev_script.sh
+++ apps/opik-documentation/documentation/dev_script.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 # Convert the files to markdown if that has not already been done
-jupyter nbconvert -ClearOutputPreprocessor.enabled=True docs/cookbook/*.ipynb --to markdown && for file in docs/cookbook/*.md; do mv "$file" "${file%.md}.mdx"; done
+jupyter nbconvert -ClearOutputPreprocessor.enabled=True --output-dir=fern/docs/cookbook docs/cookbook/*.ipynb --to markdown
+
+for file in fern/docs/cookbook/*.md; do mv "$file" "${file%.md}.mdx"; done
 
 # Start the docs server and rebuild the cookbooks on change
 npx concurrently \
-  "mintlify dev" \
-  "nodemon --watch docs/cookbook/ -e ipynb --exec 'jupyter nbconvert --ClearOutputPreprocessor.enabled=True docs/cookbook/*.ipynb --to markdown && for file in docs/cookbook/*.md; do mv "$file" "${file%.md}.mdx"; done'"
+  "fern docs dev" \
+  "nodemon --watch docs/cookbook/ -e ipynb --exec 'jupyter nbconvert --ClearOutputPreprocessor.enabled=True --output-dir=fern/docs/cookbook docs/cookbook/*.ipynb --to markdown && for file in fern/docs/cookbook/*.md; do mv "$file" "${file%.md}.mdx"; done'"
