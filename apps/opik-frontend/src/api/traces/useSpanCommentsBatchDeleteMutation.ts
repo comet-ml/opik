fbdbacf8e716b47fe008fd7ee6873e3cc6e5ea1a
Yaroslav Boiko <y.boikodevelop@gmail.com>
2025-02-13 11:08:50 +0100

    [OPIK-994] comments API integration (#1266)
    Co-authored-by: Yaroslav Boiko <yaroslavboiko@ip-192-168-1-15.ec2.internal>

---
+++ apps/opik-frontend/src/api/traces/useSpanCommentsBatchDeleteMutation.ts
@@ -0,0 +1,51 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import get from "lodash/get";
+import { useToast } from "@/components/ui/use-toast";
+import api, { SPANS_KEY, SPANS_REST_ENDPOINT } from "@/api/api";
+
+type UseSpanCommentsBatchDeleteMutationParams = {
+  ids: string[];
+  projectId: string;
+};
+
+const useSpanCommentsBatchDeleteMutation = () => {
+  const queryClient = useQueryClient();
+  const { toast } = useToast();
+
+  return useMutation({
+    mutationFn: async ({ ids }: UseSpanCommentsBatchDeleteMutationParams) => {
+      const { data } = await api.post(
+        `${SPANS_REST_ENDPOINT}/comments/delete`,
+        {
+          ids: ids,
+        },
+      );
+      return data;
+    },
+    onError: (error) => {
+      const message = get(
+        error,
+        ["response", "data", "message"],
+        error.message,
+      );
+
+      toast({
+        title: "Error",
+        description: message,
+        variant: "destructive",
+      });
+    },
+    onSettled: (data, error, variables) => {
+      queryClient.invalidateQueries({
+        queryKey: [
+          SPANS_KEY,
+          {
+            projectId: variables.projectId,
+          },
+        ],
+      });
+    },
+  });
+};
+
+export default useSpanCommentsBatchDeleteMutation;
