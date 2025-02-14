61eba4bb0f988810145c0fe4776133cfe008670c
andrii.dudar <andriid@comet.com>
2025-02-14 13:49:28 +0100

    [OPIK-689] [FR]: Add Support for Additional AI Provider Configurations (Local Models) (#1276)
    * [OPIK-689] [FR]: Add Support for Additional AI Provider Configurations (Local Models)
    
    * - fix/add descriptions messages

--- apps/opik-frontend/src/lib/llm.ts
+++ apps/opik-frontend/src/lib/llm.ts
@@ -1,7 +1,5 @@
 import { LLM_MESSAGE_ROLE, LLMMessage } from "@/types/llm";
 import { generateRandomString } from "@/lib/utils";
-import { PROVIDER_MODEL_TYPE, PROVIDER_TYPE } from "@/types/providers";
-import { PROVIDER_MODELS } from "@/constants/llm";
 
 export const generateDefaultLLMPromptMessage = (
   message: Partial<LLMMessage> = {},
@@ -12,27 +10,6 @@ export const generateDefaultLLMPromptMessage = (
     ...message,
     id: generateRandomString(),
   };
-};
-export const getModelProvider = (
-  modelName: PROVIDER_MODEL_TYPE,
-): PROVIDER_TYPE | "" => {
-  const provider = Object.entries(PROVIDER_MODELS).find(
-    ([providerName, providerModels]) => {
-      if (providerModels.find((pm) => modelName === pm.value)) {
-        return providerName;
-      }
-
-      return false;
-    },
-  );
-
-  if (!provider) {
-    return "";
-  }
-
-  const [providerName] = provider;
-
-  return providerName as PROVIDER_TYPE;
 };
 
 export const getNextMessageType = (
