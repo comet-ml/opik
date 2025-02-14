69b9e75d111bb7c1fdd88f017dc71641e5ca6e85
Alexander Barannikov <32936723+japdubengsub@users.noreply.github.com>
2025-02-14 17:03:24 +0300

    [OPIK-993] [SDK] Update langchain integration to support distributed headers (#1289)
    * use cached client instead of creating a new one
    
    * add new `distributed_headers` argument to langchain tracer
    
    * add exception if `distributed_headers` are not set

--- sdks/python/tests/library_integration/langchain/test_langchain.py
+++ sdks/python/tests/library_integration/langchain/test_langchain.py
@@ -11,6 +11,7 @@ from opik import context_storage
 from opik.api_objects import opik_client, span, trace
 from opik.config import OPIK_PROJECT_DEFAULT_NAME
 from opik.integrations.langchain.opik_tracer import OpikTracer
+from opik.types import DistributedTraceHeadersDict
 from ...testlib import (
     ANY_BUT_NONE,
     ANY_DICT,
@@ -122,6 +123,149 @@ def test_langchain__happyflow(
 
     assert len(fake_backend.trace_trees) == 1
     assert len(callback.created_traces()) == 1
+    assert_equal(EXPECTED_TRACE_TREE, fake_backend.trace_trees[0])
+
+
+def test_langchain__distributed_headers__happyflow(
+    fake_backend,
+):
+    project_name = "langchain-integration-test--distributed-headers"
+    client = opik_client.get_client_cached()
+
+    # PREPARE DISTRIBUTED HEADERS
+    trace_data = trace.TraceData(
+        name="custom-distributed-headers--trace",
+        input={
+            "key1": 1,
+            "key2": "val2",
+        },
+        project_name=project_name,
+        tags=["tag_d1", "tag_d2"],
+    )
+    trace_data.init_end_time()
+    client.trace(**trace_data.__dict__)
+
+    span_data = span.SpanData(
+        trace_id=trace_data.id,
+        parent_span_id=None,
+        name="custom-distributed-headers--span",
+        input={
+            "input": "custom-distributed-headers--input",
+        },
+        project_name=project_name,
+        tags=["tag_d3", "tag_d4"],
+    )
+    span_data.init_end_time().update(
+        output={"output": "custom-distributed-headers--output"},
+    )
+    client.span(**span_data.__dict__)
+
+    distributed_headers = DistributedTraceHeadersDict(
+        opik_trace_id=span_data.trace_id,
+        opik_parent_span_id=span_data.id,
+    )
+
+    # CALL LLM
+    llm = fake.FakeListLLM(
+        responses=["I'm sorry, I don't think I'm talented enough to write a synopsis"]
+    )
+
+    template = "Given the title of play, write a synopsys for that. Title: {title}."
+
+    prompt_template = PromptTemplate(input_variables=["title"], template=template)
+
+    synopsis_chain = prompt_template | llm
+    test_prompts = {"title": "Documentary about Bigfoot in Paris"}
+
+    callback = OpikTracer(
+        project_name=project_name,
+        tags=["tag1", "tag2"],
+        metadata={"a": "b"},
+        distributed_headers=distributed_headers,
+    )
+    synopsis_chain.invoke(input=test_prompts, config={"callbacks": [callback]})
+
+    callback.flush()
+
+    EXPECTED_TRACE_TREE = TraceModel(
+        id=ANY_BUT_NONE,
+        name="custom-distributed-headers--trace",
+        input={"key1": 1, "key2": "val2"},
+        output=None,
+        tags=["tag_d1", "tag_d2"],
+        start_time=ANY_BUT_NONE,
+        end_time=ANY_BUT_NONE,
+        project_name=project_name,
+        spans=[
+            SpanModel(
+                id=ANY_BUT_NONE,
+                name="custom-distributed-headers--span",
+                input={"input": "custom-distributed-headers--input"},
+                output={"output": "custom-distributed-headers--output"},
+                tags=["tag_d3", "tag_d4"],
+                start_time=ANY_BUT_NONE,
+                end_time=ANY_BUT_NONE,
+                project_name=project_name,
+                spans=[
+                    SpanModel(
+                        id=ANY_BUT_NONE,
+                        name="RunnableSequence",
+                        input={"title": "Documentary about Bigfoot in Paris"},
+                        output=ANY_DICT,
+                        tags=["tag1", "tag2"],
+                        start_time=ANY_BUT_NONE,
+                        end_time=ANY_BUT_NONE,
+                        metadata={"a": "b"},
+                        project_name=project_name,
+                        spans=[
+                            SpanModel(
+                                id=ANY_BUT_NONE,
+                                type="tool",
+                                name="PromptTemplate",
+                                input={"title": "Documentary about Bigfoot in Paris"},
+                                output=ANY_DICT,
+                                metadata={},
+                                start_time=ANY_BUT_NONE,
+                                end_time=ANY_BUT_NONE,
+                                project_name=project_name,
+                                spans=[],
+                            ),
+                            SpanModel(
+                                id=ANY_BUT_NONE,
+                                type="llm",
+                                name="FakeListLLM",
+                                input={
+                                    "prompts": [
+                                        "Given the title of play, write a synopsys for that. Title: Documentary about Bigfoot in Paris."
+                                    ]
+                                },
+                                output=ANY_DICT,
+                                metadata={
+                                    "invocation_params": {
+                                        "responses": [
+                                            "I'm sorry, I don't think I'm talented enough to write a synopsis"
+                                        ],
+                                        "_type": "fake-list",
+                                        "stop": None,
+                                    },
+                                    "options": {"stop": None},
+                                    "batch_size": 1,
+                                    "metadata": ANY_BUT_NONE,
+                                },
+                                start_time=ANY_BUT_NONE,
+                                end_time=ANY_BUT_NONE,
+                                project_name=project_name,
+                                spans=[],
+                            ),
+                        ],
+                    )
+                ],
+            )
+        ],
+    )
+
+    assert len(fake_backend.trace_trees) == 1
+    assert len(callback.created_traces()) == 0
     assert_equal(EXPECTED_TRACE_TREE, fake_backend.trace_trees[0])
 
 
