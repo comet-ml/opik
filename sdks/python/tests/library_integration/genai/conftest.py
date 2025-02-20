4c333c484c328891140fc7b222447b34b27de2fe
Aliaksandr Kuzmik <alexander.kuzmik99@gmail.com>
2025-02-20 16:11:06 +0100

    Update setup fixture, add debug log

--- sdks/python/tests/library_integration/genai/conftest.py
+++ sdks/python/tests/library_integration/genai/conftest.py
@@ -1,5 +1,6 @@
 import os
 import pytest
+import json
 from ...testlib import patch_environ
 
 
@@ -7,8 +8,12 @@ from ...testlib import patch_environ
 def setup_genai_credentials():
     try:
         gcp_credentials = os.environ["GCP_CREDENTIALS_JSON"]
-        with open("gcp_credentials.json", mode="wt") as file:
-            file.write(gcp_credentials)
+        print("JSON str starts with ", gcp_credentials[:5])
+        gcp_credentials_dict = json.loads(gcp_credentials)
+
+        with open("gcp_credentials.json", mode="wt") as output_file:
+            json.dump(gcp_credentials_dict, output_file)
+
         os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "gcp_credentials.json"
 
         with patch_environ(
