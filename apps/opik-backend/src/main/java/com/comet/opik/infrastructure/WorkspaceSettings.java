3fb4bda2a1e819e974519cbf42443041b516bdcc
Thiago dos Santos Hora <thiagoh@comet.com>
2025-02-24 11:50:12 +0100

    OPIK-1015: Add workspace metadata endpoint (#1323)
    * OPIK-1015: Add sorting to traces endpoint
    
    * OPIK-1015: Add workspace metadata endpoint
    
    * Add sorting field to page
    
    * Add missing condition

---
+++ apps/opik-backend/src/main/java/com/comet/opik/infrastructure/WorkspaceSettings.java
@@ -0,0 +1,14 @@
+package com.comet.opik.infrastructure;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class WorkspaceSettings {
+
+    @Valid @NotNull @JsonProperty
+    private double maxSizeToAllowSorting;
+
+}
