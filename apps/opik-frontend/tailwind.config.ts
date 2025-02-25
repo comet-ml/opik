6459da6ac2b32a622137b2fea13aef3ea1a720c1
Yaroslav Boiko <y.boikodevelop@gmail.com>
2025-02-25 18:20:25 +0100

    [OPIK-1116] show/hide button labels based on section width (#1371)
    * [OPIK-1116] show/hide button labels based on section width
    
    * [OPIK-1020] dataset icon color change
    
    ---------
    
    Co-authored-by: Yaroslav Boiko <yaroslavboiko@ip-192-168-1-15.ec2.internal>

--- apps/opik-frontend/tailwind.config.ts
+++ apps/opik-frontend/tailwind.config.ts
@@ -96,9 +96,6 @@ module.exports = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
-      screens: {
-        "3xl": "1920px",
-      },
     },
   },
   plugins: [
