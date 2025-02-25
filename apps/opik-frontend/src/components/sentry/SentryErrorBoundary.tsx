ed9d4bc67460589233a3f4fedbb6a8b774e8ad44
aadereiko <aliaksandr@comet.com>
2025-02-25 09:00:32 +0100

    [OPIK-256]: remove the sentry error;

--- apps/opik-frontend/src/components/sentry/SentryErrorBoundary.tsx
+++ apps/opik-frontend/src/components/sentry/SentryErrorBoundary.tsx
@@ -2,18 +2,9 @@ import * as Sentry from "@sentry/react";
 import React from "react";
 import SentryErrorFallback from "@/components/sentry/SentryErrorFallback";
 
-const BrokenComponent = () => {
-  React.useEffect(() => {
-    throw new Error("Component sentry"); // Now it happens within the React tree
-  }, []);
-
-  return <div>Will never render</div>;
-};
-
 const SentryErrorBoundary = ({ children }: { children: React.ReactNode }) => {
   return (
     <Sentry.ErrorBoundary fallback={SentryErrorFallback}>
-      <BrokenComponent />
       {children}
     </Sentry.ErrorBoundary>
   );
