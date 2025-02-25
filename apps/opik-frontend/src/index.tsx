9d43814f48c0a40947bb4598940009b9865274d3
aadereiko <aliaksandr@comet.com>
2025-02-25 09:00:02 +0100

    [OPIK-256]: add sentry to the prod env;

--- apps/opik-frontend/src/index.tsx
+++ apps/opik-frontend/src/index.tsx
@@ -1,7 +1,12 @@
 import { createRoot } from "react-dom/client";
 import "tailwindcss/tailwind.css";
+
+import { IS_SENTRY_ENABLED, SENTRY_DSN, SENTRY_MODE } from "@/constants/sentry";
+
 import App from "@/components/App";
 import usePluginsStore from "@/store/PluginsStore";
+import * as Sentry from "@sentry/react";
+import { APP_VERSION } from "@/constants/app";
 
 import "./main.scss";
 // other styles
@@ -9,6 +14,16 @@ import "react18-json-view/src/style.css";
 
 const container = document.getElementById("root") as HTMLDivElement;
 const root = createRoot(container);
+
+if (IS_SENTRY_ENABLED) {
+  Sentry.init({
+    dsn: SENTRY_DSN,
+    integrations: [Sentry.browserTracingIntegration()],
+    tracesSampleRate: 1.0,
+    environment: SENTRY_MODE,
+    release: APP_VERSION,
+  });
+}
 
 usePluginsStore.getState().setupPlugins(import.meta.env.MODE);
 root.render(<App />);
