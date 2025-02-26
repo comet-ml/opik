e296acbc39cc2b1831e41a4b8e739cda186bc122
Alexander Barannikov <japdubengsub@gmail.com>
2025-02-26 16:25:27 +0000

    refactoring version func

--- sdks/python/src/opik/healthcheck/tools.py
+++ sdks/python/src/opik/healthcheck/tools.py
@@ -1,5 +1,3 @@
-import sys
-from importlib import metadata
 from pathlib import Path
 from typing import Any, Dict, Optional, Tuple
 from unittest.mock import patch
@@ -10,8 +8,8 @@ from rich.console import Console
 from rich.table import Table
 from rich.text import Text
 
-from opik import Opik, config
-from opik.environment import get_installed_packages
+import opik
+from opik import Opik, config, environment
 
 console = Console()
 
@@ -30,30 +28,19 @@ def print_header(text: str) -> None:
 
 
 def print_installed_packages() -> None:
-    for name, version in sorted(get_installed_packages().items()):
+    for name, version in sorted(environment.get_installed_packages().items()):
         name = Text(name, style=DEFAULT_KEY_COLOR)
         version = Text(version, style=DEFAULT_VALUE_COLOR)
 
         console.print(name, "==", version, sep="")
 
 
-def get_opik_version() -> Tuple[str, str]:
-    """
-    Retrieve and return the Python version and the Opik version.
-    """
-    try:
-        opik_version = metadata.version("opik")
-    except metadata.PackageNotFoundError:
-        opik_version = "0.0.0+dev"
-    return sys.version, opik_version
-
-
-def print_opik_version() -> None:
-    python_version, opik_version = get_opik_version()
+def print_version() -> None:
+    python_version = environment.get_python_version()
     python_version_label = Text("Python version:", style=DEFAULT_KEY_COLOR)
     python_version = Text(python_version, style=DEFAULT_VALUE_COLOR)
     opik_version_label = Text("Opik version:", style=DEFAULT_KEY_COLOR)
-    opik_version = Text(opik_version, style=DEFAULT_VALUE_COLOR)
+    opik_version = Text(opik.__version__, style=DEFAULT_VALUE_COLOR)
 
     console.print(python_version_label, python_version)
     console.print(opik_version_label, opik_version)
