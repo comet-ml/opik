44bfc49093b7a475c468d6da5f87bada023492ea
Alexander Barannikov <32936723+japdubengsub@users.noreply.github.com>
2025-03-06 19:52:29 +0300

    [OPIK-1157] [SDK] [FR]: Python SDK: Provide method to delete trace feedback scores (#1476)
    * add methods to delete trace/span feedback scores
    
    * add e2e test
    
    * rename methods

--- sdks/python/tests/e2e/test_feedback_scores.py
+++ sdks/python/tests/e2e/test_feedback_scores.py
@@ -78,6 +78,63 @@ def test_feedbacks_are_logged_via_trace_and_span__happyflow(opik_client: opik.Op
     )
 
 
+def test_feedbacks_are_logged_via_trace_and_span__and_deleted(opik_client: opik.Opik):
+    trace = opik_client.trace(
+        name="trace-name",
+    )
+    trace.log_feedback_score(
+        "trace-metric-1", value=0.5, category_name="category-1", reason="some-reason-1"
+    )
+    trace.log_feedback_score(
+        "trace-metric-2", value=0.95, category_name="category-2", reason="some-reason-2"
+    )
+
+    span = trace.span(
+        name="span-name",
+    )
+    span.log_feedback_score(
+        "span-metric-1", value=0.75, category_name="category-3", reason="some-reason-3"
+    )
+    span.log_feedback_score(
+        "span-metric-2", value=0.25, category_name="category-4", reason="some-reason-4"
+    )
+
+    opik_client.flush()
+
+    opik_client.delete_trace_feedback_score(trace.id, "trace-metric-1")
+    opik_client.delete_span_feedback_score(span.id, "span-metric-1")
+    opik_client.delete_span_feedback_score(span.id, "span-metric-2")
+
+    opik_client.flush()
+
+    EXPECTED_TRACE_FEEDBACK_SCORES: List[FeedbackScoreDict] = [
+        {
+            "id": trace.id,
+            "name": "trace-metric-2",
+            "value": 0.95,
+            "category_name": "category-2",
+            "reason": "some-reason-2",
+        },
+    ]
+
+    EXPECTED_SPAN_FEEDBACK_SCORES = None
+
+    verifiers.verify_trace(
+        opik_client=opik_client,
+        trace_id=trace.id,
+        name="trace-name",
+        feedback_scores=EXPECTED_TRACE_FEEDBACK_SCORES,
+    )
+    verifiers.verify_span(
+        opik_client=opik_client,
+        span_id=span.id,
+        trace_id=span.trace_id,
+        parent_span_id=None,
+        name="span-name",
+        feedback_scores=EXPECTED_SPAN_FEEDBACK_SCORES,
+    )
+
+
 def test_feedbacks_are_logged_via_client__happyflow(opik_client: opik.Opik):
     trace = opik_client.trace(name="trace-name-1")
     span = trace.span(name="span-name-1")
