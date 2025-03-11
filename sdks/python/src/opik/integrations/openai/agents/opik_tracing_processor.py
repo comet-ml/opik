ed127c7ea84543e6e50bf9a01a1f3d1d40352bb9
Aliaksandr Kuzmik <alexander.kuzmik99@gmail.com>
2025-03-11 23:43:39 +0100

    Draft (no content included to traces and spans except for name, type, and timestamps)

---
+++ sdks/python/src/opik/integrations/openai/agents/opik_tracing_processor.py
@@ -0,0 +1,60 @@
+from typing import Any, Optional, Dict
+from agents import tracing
+
+from opik.api_objects.span import span_data
+from opik.api_objects.trace import trace_data
+from opik.api_objects import opik_client
+
+from . import helpers
+
+
+class OpikTracingProcessor(tracing.TracingProcessor):
+    def __init__(
+        self,
+        project_name: Optional[str] = None,
+        **kwargs: Any,
+    ) -> None:
+        super().__init__(**kwargs)
+        self._span_data_map: Dict[str, span_data.SpanData] = {}
+        """Map from run id to span data."""
+
+        self._created_traces_data_map: Dict[str, trace_data.TraceData] = {}
+        """Map from run id to trace data."""
+
+        self._project_name = project_name
+
+        self._opik_client = opik_client.get_client_cached()
+
+    def on_trace_start(self, trace: tracing.Trace) -> None:
+        opik_trace_data = trace_data.TraceData(name=trace.name)
+        self._created_traces_data_map[trace.trace_id] = opik_trace_data
+
+    def on_trace_end(self, trace: tracing.Trace) -> None:
+        opik_trace_data = self._created_traces_data_map[trace.trace_id]
+        opik_trace_data.init_end_time()
+
+        self._opik_client.trace(**opik_trace_data.__dict__)
+
+    def on_span_start(self, span: tracing.Span[Any]) -> None:
+        parent_opik_span = self._span_data_map.get(span.parent_id)
+        parent_opik_span_id = parent_opik_span.id if parent_opik_span else None
+        opik_trace_id = self._created_traces_data_map[span.trace_id].id
+
+        opik_span_data = span_data.SpanData(
+            name=helpers.span_name(span),
+            parent_span_id=parent_opik_span_id,
+            trace_id=opik_trace_id,
+        )
+        self._span_data_map[span.span_id] = opik_span_data
+
+    def on_span_end(self, span: tracing.Span[Any]) -> None:
+        opik_span_data = self._span_data_map[span.span_id]
+        opik_span_data.init_end_time()
+
+        self._opik_client.span(**opik_span_data.__dict__)
+
+    def force_flush(self) -> None:
+        return self._opik_client.flush()
+
+    def shutdown(self) -> None:
+        self.force_flush()
