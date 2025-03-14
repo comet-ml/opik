f1d1c67d9fde4a34c25a0eb346a727e154b34c7c
andrii.dudar <fanat.web@gmail.com>
2025-03-14 10:13:20 +0100

    [OPIK-1159] [FR][FE]: Freeze table header during data scroll

---
+++ apps/opik-frontend/src/components/layout/PageBodyScrollContainer/PageBodyScrollContainer.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+
+import { useObserveResizeNode } from "@/hooks/useObserveResizeNode";
+import { cn } from "@/lib/utils";
+import { STICKY_ATTRIBUTE_VERTICAL } from "@/components/layout/PageBodyStickyContainer/PageBodyStickyContainer";
+
+type PageBodyScrollContainerProps = {
+  children: React.ReactNode;
+  className?: string;
+};
+
+const PageBodyScrollContainer: React.FC<PageBodyScrollContainerProps> = ({
+  children,
+  className,
+}) => {
+  const [width, setWidth] = useState<number>(0);
+
+  const { ref } = useObserveResizeNode<HTMLDivElement>((node) => {
+    setWidth(node.clientWidth);
+
+    const verticalElements = node.querySelectorAll(
+      `[${STICKY_ATTRIBUTE_VERTICAL}]`,
+    );
+    let offset = 0;
+    verticalElements.forEach((element) => {
+      element.setAttribute("style", `top: ${offset}px`);
+      offset += element.clientHeight;
+    });
+  });
+
+  return (
+    <div
+      ref={ref}
+      style={
+        {
+          "--scroll-body-client-width": `${width}px`,
+        } as React.CSSProperties
+      }
+      className={cn(
+        "relative h-[calc(100vh-var(--header-height))] overflow-auto -mx-6",
+        className,
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default PageBodyScrollContainer;
