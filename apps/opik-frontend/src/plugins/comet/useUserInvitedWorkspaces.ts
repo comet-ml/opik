537929385845afea7925fd0a43ea8c145bfdaee0
aadereiko <aliaksandr@comet.com>
2025-03-18 10:31:03 +0100

    add hardcoded param to useUserInvitedWorkspaces.ts

--- apps/opik-frontend/src/plugins/comet/useUserInvitedWorkspaces.ts
+++ apps/opik-frontend/src/plugins/comet/useUserInvitedWorkspaces.ts
@@ -1,7 +1,6 @@
 import { QueryFunctionContext, useQuery } from "@tanstack/react-query";
 import api, { QueryConfig } from "./api";
-import { ORGANIZATION_ROLE_TYPE, Workspace } from "./types";
-import useOrganizations from "@/plugins/comet/useOrganizations";
+import { Workspace } from "./types";
 
 const getUserInvitedWorkspaces = async ({ signal }: QueryFunctionContext) => {
   return await api
@@ -16,18 +15,9 @@ const getUserInvitedWorkspaces = async ({ signal }: QueryFunctionContext) => {
 export default function useUserInvitedWorkspaces(
   options?: QueryConfig<Workspace[]>,
 ) {
-  const { data: organizations } = useOrganizations({
-    enabled: options?.enabled,
-  });
-
-  const organizationIds = organizations
-    ?.filter((organization) => {
-      return organization.role === ORGANIZATION_ROLE_TYPE.admin;
-    })
-    .map((organization) => organization.id);
-
   return useQuery({
-    queryKey: ["user-invited-workspaces", { organizationIds }],
+    // enabled: true is needed just to overcome the eslint requirement to have parameters
+    queryKey: ["user-invited-workspaces", { enabled: true }],
     queryFn: (context) => getUserInvitedWorkspaces(context),
     ...options,
     enabled: Boolean(options?.enabled),
