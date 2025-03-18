b26125806034deeb2b37c2b37e91a6d3e94583ed
Borys Tkachenko <borystkachenko@Boryss-MacBook-Pro.local>
2025-03-18 18:29:23 +0100

    Revert Autogenerated code 18-03-2025
    This reverts commit e5542d85c6e49ea99875cd5264c801db8652bb6d.

--- sdks/typescript/src/opik/rest_api/api/resources/redirect/client/requests/index.ts
+++
@@ -1,3 +0,0 @@
-export { type DatasetsRedirectRequest } from "./DatasetsRedirectRequest";
-export { type ExperimentsRedirectRequest } from "./ExperimentsRedirectRequest";
-export { type ProjectsRedirectRequest } from "./ProjectsRedirectRequest";
