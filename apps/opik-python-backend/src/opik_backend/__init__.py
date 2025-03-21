08d8291afeee333e38a242cde48abed71fec6054
Thiago dos Santos Hora <thiagoh@comet.com>
2025-03-21 17:13:58 +0100

    OPIK-1260: Add trigger to create demo data (#1603)
    * OPIK-1260: Add trigger to create demo data
    
    * Add logs
    
    * Address PR suggestions
    
    * Add check for feedback definition
    
    * Add envvar to config URLs

--- apps/opik-python-backend/src/opik_backend/__init__.py
+++ apps/opik-python-backend/src/opik_backend/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
-
+import logging
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
+
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
@@ -13,6 +17,9 @@ def create_app(test_config=None):
 
     from opik_backend.evaluator import evaluator
     from opik_backend.post_user_signup import post_user_signup
+    from opik_backend.healthcheck import healthcheck
+
+    app.register_blueprint(healthcheck)
     app.register_blueprint(evaluator)
     app.register_blueprint(post_user_signup)
 
