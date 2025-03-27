47c410570f076896ff061c65b5067261c554941e
Andres Cruz <andresc@comet.com>
2025-03-27 09:11:59 +0100

    OPIK-1130: Fix AutomationRuleEvaluator Fern (#1670)
    * OPIK-1130: Fix AutomationRuleEvaluator Fern
    
    * Rev1: updated Open API spec and code

--- sdks/typescript/src/opik/rest_api/api/resources/attachments/client/requests/index.ts
+++ sdks/typescript/src/opik/rest_api/api/resources/attachments/client/requests/index.ts
@@ -1,3 +1,5 @@
+export { type AttachmentListRequest } from "./AttachmentListRequest";
 export { type CompleteMultipartUploadRequest } from "./CompleteMultipartUploadRequest";
+export { type DownloadAttachmentRequest } from "./DownloadAttachmentRequest";
 export { type StartMultipartUploadRequest } from "./StartMultipartUploadRequest";
 export { type UploadAttachmentRequest } from "./UploadAttachmentRequest";
