d2bc6a379007418bf83c980f86254705c73b9ab0
NatZol <natalia.zlkh@gmail.com>
2025-04-01 15:46:39 +0300

    fix flakiness, fix selectors

--- tests_end_to_end/page_objects/ThreadsPage.py
+++ tests_end_to_end/page_objects/ThreadsPage.py
@@ -1,3 +1,4 @@
+import time
 from playwright.sync_api import Page,expect
 
 class ThreadsPage:
@@ -5,14 +6,18 @@ class ThreadsPage:
         self.page = page
         self.threads_tab = self.page.get_by_role("tab", name="Threads")
         self.thread_row = self.page.locator("tbody tr")
-        self.message_in_thread = self.page.get_by_test_id("thread").locator("div.comet-markdown")
+        self.thread_container = self.page.get_by_test_id("thread")
+        self.output_container = self.page.get_by_test_id("thread").locator('.comet-markdown')
     
     def switch_to_page(self):
         self.threads_tab.click()
-        expect(self.page.get_by_test_id("search-input")).to_be_visible()
     
     def get_number_of_threads_on_page(self):
         try:
+            for i in range(5):
+                if(not self.thread_row.count()):
+                    time.sleep(1)
+                    self.page.reload()
             expect(self.thread_row.first).to_be_visible()
         except Exception:
             return 0
@@ -22,8 +27,11 @@ class ThreadsPage:
     def open_thread_content(self, thread_id):
         self.page.get_by_role("button", name=thread_id).click()
     
-    def check_message_in_thread(self, message, index):
-        expect(self.message_in_thread.nth(index).filter(has_text=message)).to_be_visible()
+    def check_message_in_thread(self, message, output = False):
+        if output:
+            expect(self.output_container.filter(has_text=message)).to_be_visible()
+        else:
+            expect(self.thread_container.get_by_text(message)).to_be_visible()
 
     def close_thread_content(self):
         self.page.keyboard.press('Escape')
