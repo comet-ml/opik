12d8519ea0f8c1bd78a2c3beaf14c6326014428c
Ido Berkovich <ido@comet.com>
2025-04-03 10:02:17 +0300

    OPIK-1354 add span type guardrail

---
+++ apps/opik-backend/src/main/resources/liquibase/db-app-analytics/migrations/000017_guardrails_span_type.sql
@@ -0,0 +1,13 @@
+--liquibase formatted sql
+--changeset idoberko2:0000017_guardrails_span_type
+
+
+ALTER TABLE ${ANALYTICS_DB_DATABASE_NAME}.spans MODIFY COLUMN type Enum8(
+    'unknown' = 0 ,
+    'general' = 1,
+    'tool' = 2,
+    'llm' = 3,
+    'guardrail' = 4);
+
+
+--rollback ALTER TABLE ${ANALYTICS_DB_DATABASE_NAME}.spans MODIFY COLUMN type Enum8('unknown' = 0 , 'general' = 1, 'tool' = 2, 'llm' = 3);
