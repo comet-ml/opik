eabdbff2718a669405930d2e048dcc971e922afe
Yaroslav Boiko <y.boikodevelop@gmail.com>
2025-04-04 12:56:31 +0200

    [OPIK-1391] langchain integration as package (#1715)
    * [OPIK-1391] npm package for langchain integration
    
    * [OPIK-1391] refactoring
    
    * [OPIK-1391] metadata type update
    
    * [OPIK-1391] fix linter settings
    
    ---------
    
    Co-authored-by: Yaroslav Boiko <yaroslavboiko@ip-192-168-1-11.ec2.internal>

--- sdks/typescript/src/opik/integrations/opik-langchain/eslint.config.js
+++ sdks/typescript/src/opik/integrations/opik-langchain/eslint.config.js
@@ -5,12 +5,7 @@ import tseslint from "typescript-eslint";
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {
-    ignores: [
-      "dist/**",
-      "node_modules/**",
-      "src/opik/rest_api/**",
-      "src/opik/integrations/opik-langchain/**",
-    ],
+    ignores: ["dist/**", "node_modules/**"],
   },
   { files: ["src/**/*.{js,mjs,cjs,ts}", "tests/**/*.{js,mjs,cjs,ts}"] },
   { languageOptions: { globals: globals.browser } },
