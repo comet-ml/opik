7972b65b07ed194c754def17b445444dde280050
Aliaksandr Kuzmik <98702584+alexkuzmik@users.noreply.github.com>
2025-04-08 16:53:19 +0200

    [NA] Alexkuzmik/agent optimizer (#1778)
    * Add skeleton for optimizer project
    
    * Add base optimizer
    
    * Add readme with the installation instruction

---
+++ sdks/opik_optimizer/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="opik_optimizer",
+    version="0.1.0",
+    description="Agent optimization with Opik",
+    author="Comet ML",
+    author_email="info@comet.ml",
+    url="https://github.com/comet-ml/opik",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    python_requires=">=3.10",
+    install_requires=[
+        "opik",
+        "dspy",
+        "tqdm",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
+    ],
+)
