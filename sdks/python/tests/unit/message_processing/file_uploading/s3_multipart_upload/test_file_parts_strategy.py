35b0f0cb37088aad3ffb0b15ca3dd8c922d0894c
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2025-04-08 20:52:42 +0300

    OPIK-725: Implemented strategy to create file parts for S3 multipart uploading. Implemented related test cases.

---
+++ sdks/python/tests/unit/message_processing/file_uploading/s3_multipart_upload/test_file_parts_strategy.py
@@ -0,0 +1,62 @@
+import math
+import pytest
+
+from opik.message_processing.file_uploading.s3_multipart_upload import (
+    file_parts_strategy,
+)
+from opik.message_processing.file_uploading.s3_multipart_upload import upload_error
+
+
+def test_base_strategy__top_max_file_parts_limits():
+    parts_expected = 10
+    strategy = file_parts_strategy.BaseFilePartsStrategy(
+        file="some_file",
+        file_size=file_parts_strategy.MAX_FILE_PART_SIZE * parts_expected,
+        max_file_part_size=file_parts_strategy.MAX_FILE_PART_SIZE,
+    )
+
+    assert strategy.max_file_part_size == file_parts_strategy.MAX_FILE_PART_SIZE
+    parts_number = strategy.calculate()
+    assert parts_number == parts_expected
+
+
+def test_base_strategy__bottom_max_file_parts_limits():
+    parts_expected = 10
+    strategy = file_parts_strategy.BaseFilePartsStrategy(
+        file="some_file",
+        file_size=file_parts_strategy.MIN_FILE_PART_SIZE * parts_expected,
+        max_file_part_size=file_parts_strategy.MIN_FILE_PART_SIZE,
+    )
+
+    assert strategy.max_file_part_size == file_parts_strategy.MIN_FILE_PART_SIZE
+    parts_number = strategy.calculate()
+    assert parts_number == parts_expected
+
+
+def test_base_strategy__file_is_too_large():
+    strategy = file_parts_strategy.BaseFilePartsStrategy(
+        file="some_file",
+        file_size=file_parts_strategy.MAX_FILE_PART_SIZE
+        * file_parts_strategy.MAX_SUPPORTED_PARTS_NUMBER
+        + 1,
+        max_file_part_size=file_parts_strategy.MAX_FILE_PART_SIZE,
+    )
+
+    with pytest.raises(upload_error.S3UploadErrorFileIsTooLarge):
+        strategy.calculate()
+
+
+def test_base_strategy__happy_path_small_files():
+    file_size = 11 * 1024 * 1024
+    strategy = file_parts_strategy.BaseFilePartsStrategy(
+        file="some_file",
+        file_size=file_size,
+        max_file_part_size=file_parts_strategy.MIN_FILE_PART_SIZE,
+    )
+
+    parts_number = strategy.calculate()
+    assert parts_number == math.ceil(file_size / file_parts_strategy.MIN_FILE_PART_SIZE)
+    assert strategy.max_file_part_size == file_parts_strategy.MIN_FILE_PART_SIZE
+    projected_size = parts_number * strategy.max_file_part_size
+    remainder = projected_size - file_size
+    assert remainder < file_parts_strategy.MIN_FILE_PART_SIZE
