60a5f9da01c7a56a5f176fb59e971082fbf365e8
Boris Feld <boris@comet.com>
2025-04-08 16:21:39 +0200

    Fix healthcheck for guardrail server

--- apps/opik-guardrails-backend/entrypoint.sh
+++ apps/opik-guardrails-backend/entrypoint.sh
@@ -5,5 +5,5 @@ set -e
 export OPIK_GUARDRAILS_DEVICE=${OPIK_GUARDRAILS_DEVICE:-cuda:0}
 
 echo "Starting the Opik Guardrails Backend server"
-gunicorn --workers=1 --threads=4 --bind=0.0.0.0:5000 'opik_guardrails:create_app()'
+gunicorn --workers=1 --threads=4 --bind=0.0.0.0:5000 --access-logfile=- --log-level=info 'opik_guardrails:create_app()'
 #gunicorn --workers=2 --bind=0.0.0.0:5000 'opik_guardrails:create_app()'
