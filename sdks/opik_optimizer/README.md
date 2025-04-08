f53bcd8770cea7c152ad395e906f6cfdf133c4ae
Aliaksandr Kuzmik <98702584+alexkuzmik@users.noreply.github.com>
2025-04-08 16:53:19 +0200

    [NA] Alexkuzmik/agent optimizer (#1778)
    * Add skeleton for optimizer project
    
    * Add base optimizer
    
    * Add readme with the installation instruction

---
+++ sdks/opik_optimizer/README.md
@@ -0,0 +1,9 @@
+# Opik Optimizer
+
+## Installation
+
+Development mode (editable)
+```bash
+cd sdks/opik_optimizer
+pip install -e .
+```
---
+++ sdks/opik_optimizer/src/opik_optimizer/utils.py
@@ -0,0 +1,15 @@
+class State(dict):
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError as e:
+            raise AttributeError(e)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+    def __delattr__(self, key):
+        try:
+            del self[key]
+        except KeyError as e:
+            raise AttributeError(e)
