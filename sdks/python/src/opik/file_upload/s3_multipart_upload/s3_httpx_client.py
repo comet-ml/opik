028705895a5390700537f61083332e5811bd1780
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2025-04-09 13:22:44 +0300

    OPIK-725: Implemented retry specific to S3 file upload.

--- sdks/python/src/opik/file_upload/s3_multipart_upload/s3_httpx_client.py
+++ sdks/python/src/opik/file_upload/s3_multipart_upload/s3_httpx_client.py
@@ -1,10 +1,13 @@
 import httpx
+import tenacity
 
 KEEPALIVE_EXPIRY_SECONDS = 10
 CONNECT_TIMEOUT_SECONDS = 20
 READ_TIMEOUT_SECONDS = 100
 WRITE_TIMEOUT_SECONDS = 100
 POOL_TIMEOUT_SECONDS = 20
+
+RETRYABLE_STATUS_CODES = [500]
 
 
 def get() -> httpx.Client:
@@ -18,3 +21,27 @@ def get() -> httpx.Client:
     )
 
     return httpx.Client(limits=limits, timeout=timeout)
+
+
+def _allowed_to_retry(exception: Exception) -> bool:
+    if isinstance(
+        exception,
+        (
+            httpx.ConnectError,
+            httpx.TimeoutException,
+        ),
+    ):
+        return True
+
+    if isinstance(exception, httpx.HTTPStatusError):
+        if exception.response.status_code in RETRYABLE_STATUS_CODES:
+            return True
+
+    return False
+
+
+s3_retry = tenacity.retry(
+    stop=tenacity.stop_after_attempt(3),
+    wait=tenacity.wait_exponential(multiplier=5, min=10, max=45),
+    retry=tenacity.retry_if_exception(_allowed_to_retry),
+)
