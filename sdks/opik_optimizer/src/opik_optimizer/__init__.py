b95ebb261b1cec06be9cd1acb5614d25837db037
Aliaksandr Kuzmik <alexander.kuzmik99@gmail.com>
2025-04-09 17:48:41 +0200

    Bump version to 0.2.0 and add __all__ to __init__.py

--- sdks/opik_optimizer/src/opik_optimizer/__init__.py
+++ sdks/opik_optimizer/src/opik_optimizer/__init__.py
@@ -5,3 +5,10 @@ __version__ = importlib.metadata.version("opik_optimizer")
 from .mipro_optimizer import MiproOptimizer
 from .few_shot_optimizer import FewShotOptimizer
 from .few_shot_bayesian_optimizer import FewShotBayesianOptimizer
+
+
+__all__ = [
+    "FewShotBayesianOptimizer",
+    "FewShotOptimizer",
+    "MiproOptimizer",
+]
