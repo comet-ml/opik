1106723557ce80869a815e21dd9606b908745783
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2025-04-10 14:50:35 +0300

    OPIK-1444: Fixed to use Numpy random bytes generator

--- sdks/python/tests/unit/file_upload/s3_multipart_upload/test_s3_file_uploader.py
+++ sdks/python/tests/unit/file_upload/s3_multipart_upload/test_s3_file_uploader.py
@@ -1,4 +1,4 @@
-import random
+import numpy as np
 import re
 import tempfile
 
@@ -16,7 +16,7 @@ FILE_SIZE = 12 * 1024 * 1024
 @pytest.fixture
 def data_file():
     with tempfile.NamedTemporaryFile(delete=True) as file:
-        file.write(random.randbytes(FILE_SIZE))
+        file.write(np.random.bytes(FILE_SIZE))
         file.seek(0)
 
         yield file
