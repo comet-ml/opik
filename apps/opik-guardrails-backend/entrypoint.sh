18e893bb8ac895d53f7482768a45b81fdeb03b6d
Boris Feld <boris@comet.com>
2025-04-14 15:33:57 +0200

    [OPIK-1361] Add opik-guardrails-backend to docker compose and update the routing (#1780)
    * Start adding guardrails server to Docker compose based deployment
    
    * Reduce guardrails image size
    
    * Fix healthcheck for guardrail server
    
    * Add quiet to healthcheck command
    
    * Handle review comments
    
    * Fix docker-compose service dependencies

--- apps/opik-guardrails-backend/entrypoint.sh
+++ apps/opik-guardrails-backend/entrypoint.sh
@@ -5,5 +5,6 @@ set -e
 export OPIK_GUARDRAILS_DEVICE=${OPIK_GUARDRAILS_DEVICE:-cuda:0}
 
 echo "Starting the Opik Guardrails Backend server"
-gunicorn --workers=1 --threads=4 --bind=0.0.0.0:5000 'opik_guardrails:create_app()'
-#gunicorn --workers=2 --bind=0.0.0.0:5000 'opik_guardrails:create_app()'
+gunicorn --workers=1 --threads=4 --bind=0.0.0.0:5000 --access-logfile=- \
+    --access-logformat '{"body_bytes_sent": %(B)s, "http_referer": "%(f)s", "http_user_agent": "%(a)s", "remote_addr": "%(h)s", "remote_user": "%(u)s", "request_length": 0, "request_time": %(L)s, "request": "%(r)s", "source": "gunicorn", "status": %(s)s, "time_local": "%(t)s", "time": %(T)s, "x_forwarded_for": "%(h)s"}' \
+    --log-level=info 'opik_guardrails:create_app()'
