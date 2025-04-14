2097415d01fb9702172fe45ce6801fdd543b1db3
Yaroslav Boiko <y.boikodevelop@gmail.com>
2025-04-14 09:26:08 +0200

    [OPIK-1421] experiment feedback scores UX improvements (#1827)
    * [OPIK-1421] feedback scores editor
    
    * [OPIK-1421] height fixes
    
    * [OPIK-1421] new button size
    
    * [OPIK-1421] refactoring
    
    ---------
    
    Co-authored-by: Yaroslav Boiko <yaroslavboiko@ip-192-168-1-18.ec2.internal>
    Co-authored-by: Yaroslav Boiko <yaroslavboiko@Yaroslavs-MacBook-Pro.local>
    Co-authored-by: Yaroslav Boiko <yaroslavboiko@ip-192-168-1-15.ec2.internal>

--- apps/opik-frontend/src/components/ui/button.tsx
+++ apps/opik-frontend/src/components/ui/button.tsx
@@ -30,7 +30,9 @@ const buttonVariants = cva(
       size: {
         default: "h-10 px-4 py-2",
         sm: "h-8 px-3",
-        "2xs": "comet-body-xs h-6 px-2",
+        "2xs":
+          "comet-body-xs-accented h-6 gap-1 px-2 [&>svg]:size-3.5 [&>svg]:shrink-0",
+        "3xs": "comet-body-xs h-6 px-2",
         lg: "h-11 px-8",
         icon: "size-10 [&>svg]:size-4 [&>svg]:shrink-0",
         "icon-sm": "size-8 [&>svg]:size-3.5 [&>svg]:shrink-0",
