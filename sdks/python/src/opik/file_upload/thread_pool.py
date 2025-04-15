ddba1fbccc0c1e6b49ced056ca6e01b938c8efbc
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2025-04-15 20:09:47 +0300

    OPIK-1445: Refactored thread pool factory to calculate pool size using `worker_count` or default heuristic if `worker_count` is `None`

--- sdks/python/src/opik/file_upload/thread_pool.py
+++ sdks/python/src/opik/file_upload/thread_pool.py
@@ -1,37 +1,29 @@
 import logging
-import multiprocessing
+import os
 from concurrent.futures import ThreadPoolExecutor
 from typing import Optional
 
-DEFAULT_POOL_RATIO = 4
 MAX_POOL_SIZE = 32
-DEFAULT_CPU_COUNT = 1
 
 LOGGER = logging.getLogger(__name__)
 
 
-def get_thread_pool(
-    worker_cpu_ratio: int, worker_count: Optional[int] = None
-) -> ThreadPoolExecutor:
-    try:
-        cpu_count = multiprocessing.cpu_count() or 1
-    except NotImplementedError:
-        cpu_count = DEFAULT_CPU_COUNT
+def get_thread_pool(worker_count: Optional[int] = None) -> ThreadPoolExecutor:
+    cpu_count = os.cpu_count() or 1
 
     if worker_count is not None:
         pool_size = worker_count
+        LOGGER.debug(
+            "Creating file upload thread pool with requested workers count: %d, detected CPU count: %d ",
+            worker_count,
+            cpu_count,
+        )
     else:
-        if not isinstance(worker_cpu_ratio, int) or worker_cpu_ratio <= 0:
-            LOGGER.debug("Invalid worker_cpu_ratio %r", worker_cpu_ratio)
-            worker_cpu_ratio = DEFAULT_POOL_RATIO
+        pool_size = min(MAX_POOL_SIZE, cpu_count + 4)
+        LOGGER.debug(
+            "Creating file upload thread pool with pool_size: %d, detected CPU count: %d ",
+            pool_size,
+            cpu_count,
+        )
 
-        pool_size = min(MAX_POOL_SIZE, cpu_count * worker_cpu_ratio)
-
-    LOGGER.debug(
-        "Creating thread pool with pool_size: %d, worker per CPU ratio: %d, detected CPU count: %d ",
-        pool_size,
-        worker_cpu_ratio,
-        cpu_count,
-    )
-
-    return ThreadPoolExecutor(pool_size)
+    return ThreadPoolExecutor(pool_size, thread_name_prefix="opik_file_upload")
