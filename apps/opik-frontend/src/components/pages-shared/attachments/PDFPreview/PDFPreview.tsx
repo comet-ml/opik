e4613b2521390fe44b3a5efb3186bb378ab32d8f
andrii.dudar <fanat.web@gmail.com>
2025-04-22 15:08:15 +0200

    [OPIK-1437] [FE] [Attachments] integrate with BE and SDK

--- apps/opik-frontend/src/components/pages-shared/attachments/PDFPreview/PDFPreview.tsx
+++ apps/opik-frontend/src/components/pages-shared/attachments/PDFPreview/PDFPreview.tsx
@@ -1,15 +1,13 @@
 import React, { useState } from "react";
 import { Document, Page, pdfjs } from "react-pdf";
+import PDFWorker from "pdfjs-dist/build/pdf.worker.min.mjs?worker";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 
 import Loader from "@/components/shared/Loader/Loader";
 import { Button } from "@/components/ui/button";
 
 // Set the worker source for react-pdf
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  "pdfjs-dist/build/pdf.worker.min.mjs",
-  import.meta.url,
-).toString();
+pdfjs.GlobalWorkerOptions.workerPort = new PDFWorker();
 
 interface PDFPreviewProps {
   url: string;
--- apps/opik-frontend/src/constants/attachments.ts
+++ apps/opik-frontend/src/constants/attachments.ts
@@ -18,11 +18,14 @@ export const MINE_TYPE_TO_ATTACHMENT_TYPE_MAP: Record<string, ATTACHMENT_TYPE> =
     "image/gif": ATTACHMENT_TYPE.IMAGE,
     "video/mp4": ATTACHMENT_TYPE.VIDEO,
     "video/mpeg": ATTACHMENT_TYPE.VIDEO,
+    "video/webm": ATTACHMENT_TYPE.VIDEO,
     "audio/mpeg": ATTACHMENT_TYPE.AUDIO,
     "audio/vorbis": ATTACHMENT_TYPE.AUDIO,
     "audio/wav": ATTACHMENT_TYPE.AUDIO,
+    "audio/x-wav": ATTACHMENT_TYPE.AUDIO,
     "text/plain": ATTACHMENT_TYPE.TEXT,
     "text/markdown": ATTACHMENT_TYPE.TEXT,
+    "application/json": ATTACHMENT_TYPE.TEXT,
     "application/octet-stream": ATTACHMENT_TYPE.OTHER,
   };
 
