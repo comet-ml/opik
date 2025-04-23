82627584bc6dbc0488c6198e153035e3c38f767d
Boris Feld <boris@comet.com>
2025-04-23 18:52:59 +0200

    Update E2E tests to cover created span

--- sdks/python/tests/e2e/test_guardrails.py
+++ sdks/python/tests/e2e/test_guardrails.py
@@ -6,8 +6,31 @@ from opik import exceptions
 import mock
 import pytest
 
+from .conftest import OPIK_E2E_TESTS_PROJECT_NAME
 
-def test_passing_guardrails__happyflow(opik_client: opik.Opik):
+
+def find_guardrail_span(opik_client, trace_id, project_name):
+    # We cannot easily access the created span id from the test so we need to query it dynamically
+    spans = opik_client.search_spans(
+        trace_id=trace_id,
+        project_name=project_name,
+        filter_string='name = "Guardrail"',
+    )
+
+    assert len(spans) == 1, "Expected to find 1 span"
+    span = spans[0]
+
+    return span.id
+
+
+@pytest.mark.parametrize(
+    "project_name",
+    [
+        "e2e-tests-manual-project-name",
+        None,
+    ],
+)
+def test_passing_guardrails__happyflow(opik_client, project_name):
     # Setup
     ID_STORAGE = {}
 
@@ -18,11 +41,13 @@ def test_passing_guardrails__happyflow(opik_client: opik.Opik):
         ]
     )
 
-    @opik.track
+    generation = "Hi how are you doing?"
+
+    @opik.track(project_name=project_name)
     def test_function():
         ID_STORAGE["trace-id"] = opik_context.get_current_trace_data().id
+        ID_STORAGE["parent-span-id"] = opik_context.get_current_span_data().id
 
-        generation = "Hi how are you doing?"
         result = guard.validate(generation)
 
         return result
@@ -31,6 +56,7 @@ def test_passing_guardrails__happyflow(opik_client: opik.Opik):
 
     opik_client.flush()
 
+    # Check trace guardrails validations
     EXPECTED_TRACE_GUARDRAILS_VALIDATIONS = [
         {
             "span_id": mock.ANY,
@@ -45,6 +71,26 @@ def test_passing_guardrails__happyflow(opik_client: opik.Opik):
         opik_client=opik_client,
         trace_id=ID_STORAGE["trace-id"],
         guardrails_validations=EXPECTED_TRACE_GUARDRAILS_VALIDATIONS,
+        project_name=project_name or OPIK_E2E_TESTS_PROJECT_NAME,
+    )
+
+    span_id = find_guardrail_span(opik_client, ID_STORAGE["trace-id"], project_name)
+
+    EXPECTED_OUTPUT = {
+        "guardrail_result": "passed",
+        "validations": [mock.ANY, mock.ANY],
+        "validation_passed": mock.ANY,
+    }
+
+    verifiers.verify_span(
+        opik_client=opik_client,
+        span_id=span_id,
+        parent_span_id=ID_STORAGE["parent-span-id"],
+        trace_id=ID_STORAGE["trace-id"],
+        name="Guardrail",
+        input={"generation": generation},
+        output=EXPECTED_OUTPUT,
+        project_name=project_name or OPIK_E2E_TESTS_PROJECT_NAME,
     )
 
 
@@ -59,11 +105,12 @@ def test_failing_guardrails__happyflow(opik_client: opik.Opik):
         ]
     )
 
+    generation = "First name: Samantha Last name: Martinez"
+
     @opik.track
     def test_function():
         ID_STORAGE["trace-id"] = opik_context.get_current_trace_data().id
-
-        generation = "First name: Samantha Last name: Martinez"
+        ID_STORAGE["parent-span-id"] = opik_context.get_current_span_data().id
 
         with pytest.raises(exceptions.GuardrailValidationFailed):
             guard.validate(generation)
@@ -86,4 +133,24 @@ def test_failing_guardrails__happyflow(opik_client: opik.Opik):
         opik_client=opik_client,
         trace_id=ID_STORAGE["trace-id"],
         guardrails_validations=EXPECTED_TRACE_GUARDRAILS_VALIDATIONS,
+    )
+
+    span_id = find_guardrail_span(
+        opik_client, ID_STORAGE["trace-id"], OPIK_E2E_TESTS_PROJECT_NAME
+    )
+
+    EXPECTED_OUTPUT = {
+        "guardrail_result": "failed",
+        "validations": [mock.ANY, mock.ANY],
+        "validation_passed": mock.ANY,
+    }
+
+    verifiers.verify_span(
+        opik_client=opik_client,
+        span_id=span_id,
+        parent_span_id=ID_STORAGE["parent-span-id"],
+        trace_id=ID_STORAGE["trace-id"],
+        name="Guardrail",
+        input={"generation": generation},
+        output=EXPECTED_OUTPUT,
     )
