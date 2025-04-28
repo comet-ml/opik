1b7d3a6191473ce9c02ec1944ddfdb29ccaeed4d
andrii.dudar <fanat.web@gmail.com>
2025-04-28 10:51:35 +0200

    - add adjustments after testing

--- apps/opik-frontend/src/components/pages-shared/attachments/PDFPreview/PDFPreview.tsx
+++ apps/opik-frontend/src/components/pages-shared/attachments/PDFPreview/PDFPreview.tsx
@@ -75,7 +75,7 @@ const PDFPreview: React.FC<PDFPreviewProps> = ({
 
   return (
     <div className="size-full">
-      <div className="relative flex size-full items-center justify-center overflow-auto pb-10">
+      <div className="relative flex size-full justify-center overflow-auto pb-10">
         {renderContent()}
       </div>
       {renderNavigation()}
