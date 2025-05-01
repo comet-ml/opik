46fb5ba2d8f8de265101ceca4afc4a9e7cc1fbba
Doug Blank <doug.blank@gmail.com>
2025-05-01 15:32:22 -0400

    Added console_util tests

---
+++ sdks/python/tests/unit/test_console_utils.py
@@ -0,0 +1,102 @@
+import pytest
+import types
+import sys
+from typing import Dict, List
+import tqdm
+from unittest.mock import patch
+from opik.console_utils import (
+    _in_colab_environment,
+    _in_ipython_environment,
+    _in_jupyter_environment,
+    get_tqdm,
+)
+
+
+class IPythonObject(object):
+    def __init__(self, user_local=None, colab=False):
+        self.kernel = True
+        self.colab = colab
+        if user_local is None:
+            user_local = {"_ih": ["import IPython"], "_oh": {}}
+
+        # save
+        self.ns_table = {"user_local": user_local}
+
+    def __repr__(self):
+        if self.colab:
+            return "<google.colab._shell.Shell object at 0x7f22f3a203d0>"
+        else:
+            return "<IPython.terminal.interactiveshell.TerminalInteractiveShell object at 0x7f41dfc7ca60>"
+
+
+class TestConsoleUtils:
+    @classmethod
+    def setUpClass(cls):
+        sys.modules["IPython"] = types.ModuleType("IPython")
+        sys.modules["IPython"].get_ipython = lambda: None
+
+    @classmethod
+    def tearDownClass(cls):
+        del sys.modules["IPython"]
+
+    def test_colab_environment(self):
+        import IPython
+
+        assert _in_colab_environment() is False
+
+        with patch.object(
+            IPython, "get_ipython", side_effect=lambda: IPythonObject(colab=True)
+        ):
+            assert _in_colab_environment() is True
+
+    def test_ipython_environment(self):
+        import IPython
+
+        assert _in_jupyter_environment() is False
+        assert _in_ipython_environment() is False
+
+        class FakeIPythonObject(object):
+            pass
+
+        # Simulate an ipython environment:
+        with patch.object(
+            IPython, "get_ipython", side_effect=lambda: FakeIPythonObject()
+        ):
+            assert _in_jupyter_environment() is False
+            assert _in_ipython_environment() is True
+
+    def test_jupyter_environment(self):
+        import IPython
+
+        assert _in_jupyter_environment() is False
+        assert _in_ipython_environment() is False
+
+        # Simulate a jupyter environment, something with a kernel
+        with patch.object(IPython, "get_ipython", side_effect=lambda: IPythonObject()):
+            assert _in_jupyter_environment() is True
+            assert _in_ipython_environment() is True
+
+    def test_get_tqdm(self):
+        import IPython
+
+        _tqdm = get_tqdm()
+
+        assert _tqdm is tqdm.tqdm
+
+    def test_get_tqdm_jupyter(self):
+        import IPython
+
+        with patch.object(IPython, "get_ipython", side_effect=lambda: IPythonObject()):
+            _tqdm = get_tqdm()
+
+        assert _tqdm is tqdm.tqdm_notebook
+
+    def test_get_tqdm_colab(self):
+        import IPython
+
+        with patch.object(
+            IPython, "get_ipython", side_effect=lambda: IPythonObject(colab=True)
+        ):
+            _tqdm = get_tqdm()
+
+        assert _tqdm is tqdm.tqdm_notebook
