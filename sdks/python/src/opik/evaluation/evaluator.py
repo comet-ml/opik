2db31156b13d47f49def190aa9cda87d27a2b911
Aliaksandr Kuzmik <98702584+alexkuzmik@users.noreply.github.com>
2025-05-05 20:51:04 +0200

    [NA] Implement Optimization API client, add e2e test for it (#2032)
    * Implement Optimization API client, add e2e test for it
    
    * Add evaluate_optimization_trial function
    
    * [OPIK-1188] [SDK] Address failing test for DSPy integration logging (#2029)
    
    * OPIK-1188 [SDK] Address failing test for DSPy integration (error_info is not logged)
    
    * remove redundant span from test
    
    * [OPIK-1479]   [FE] [Agent optimizations] Agent Optimizations page (#2023)
    
    * [OPIK-1479] Agent Optimizations page
    
    * - hide menu item
    
    * - implement FE filtering by name
    
    * - improve chart visibility
    
    ---------
    
    Co-authored-by: Alexander Barannikov <32936723+japdubengsub@users.noreply.github.com>
    Co-authored-by: andrii.dudar <andriid@comet.com>

--- sdks/python/src/opik/evaluation/evaluator.py
+++ sdks/python/src/opik/evaluation/evaluator.py
@@ -4,7 +4,7 @@ from typing import Any, Callable, Dict, List, Optional, Union
 
 from .. import Prompt
 from ..api_objects import opik_client
-from ..api_objects.dataset import dataset
+from ..api_objects import dataset, experiment
 from ..api_objects.experiment import helpers as experiment_helpers
 from ..api_objects.prompt import prompt_template
 from . import asyncio_support, engine, evaluation_result, report, rest_operations
@@ -91,6 +91,35 @@ def evaluate(
         prompts=checked_prompts,
     )
 
+    return _evaluate_task(
+        client=client,
+        experiment=experiment,
+        dataset=dataset,
+        task=task,
+        scoring_metrics=scoring_metrics,
+        project_name=project_name,
+        verbose=verbose,
+        nb_samples=nb_samples,
+        task_threads=task_threads,
+        scoring_key_mapping=scoring_key_mapping,
+        dataset_item_ids=dataset_item_ids,
+    )
+
+
+def _evaluate_task(
+    *,
+    client: opik_client.Opik,
+    experiment: experiment.Experiment,
+    dataset: dataset.Dataset,
+    task: LLMTask,
+    scoring_metrics: List[base_metric.BaseMetric],
+    project_name: Optional[str],
+    verbose: int,
+    nb_samples: Optional[int],
+    task_threads: int,
+    scoring_key_mapping: Optional[ScoringKeyMappingType],
+    dataset_item_ids: Optional[List[str]],
+) -> evaluation_result.EvaluationResult:
     start_time = time.time()
 
     with asyncio_support.async_http_connections_expire_immediately():
@@ -356,3 +385,99 @@ def evaluate_prompt(
     )
 
     return evaluation_result_
+
+
+def evaluate_optimization_trial(
+    optimization_id: str,
+    dataset: dataset.Dataset,
+    task: LLMTask,
+    scoring_metrics: Optional[List[base_metric.BaseMetric]] = None,
+    experiment_name: Optional[str] = None,
+    project_name: Optional[str] = None,
+    experiment_config: Optional[Dict[str, Any]] = None,
+    verbose: int = 1,
+    nb_samples: Optional[int] = None,
+    task_threads: int = 16,
+    prompt: Optional[Prompt] = None,
+    prompts: Optional[List[Prompt]] = None,
+    scoring_key_mapping: Optional[ScoringKeyMappingType] = None,
+    dataset_item_ids: Optional[List[str]] = None,
+) -> evaluation_result.EvaluationResult:
+    """
+    Performs task evaluation on a given dataset.
+
+    Args:
+        optimization_id: The ID of the optimization associated with the experiment.
+
+        dataset: An Opik dataset instance
+
+        task: A callable object that takes dict with dataset item content
+            as input and returns dict which will later be used for scoring.
+
+        experiment_name: The name of the experiment associated with evaluation run.
+            If None, a generated name will be used.
+
+        project_name: The name of the project. If not provided, traces and spans will be logged to the `Default Project`
+
+        experiment_config: The dictionary with parameters that describe experiment
+
+        scoring_metrics: List of metrics to calculate during evaluation.
+            Each metric has `score(...)` method, arguments for this method
+            are taken from the `task` output, check the signature
+            of the `score` method in metrics that you need to find out which keys
+            are mandatory in `task`-returned dictionary.
+            If no value provided, the experiment won't have any scoring metrics.
+
+        verbose: an integer value that controls evaluation output logs such as summary and tqdm progress bar.
+            0 - no outputs, 1 - outputs are enabled (default).
+
+        nb_samples: number of samples to evaluate. If no value is provided, all samples in the dataset will be evaluated.
+
+        task_threads: number of thread workers to run tasks. If set to 1, no additional
+            threads are created, all tasks executed in the current thread sequentially.
+            are executed sequentially in the current thread.
+            Use more than 1 worker if your task object is compatible with sharing across threads.
+
+        prompt: Prompt object to link with experiment. Deprecated, use `prompts` argument instead.
+
+        prompts: A list of Prompt objects to link with experiment.
+
+        scoring_key_mapping: A dictionary that allows you to rename keys present in either the dataset item or the task output
+            so that they match the keys expected by the scoring metrics. For example if you have a dataset item with the following content:
+            {"user_question": "What is Opik ?"} and a scoring metric that expects a key "input", you can use scoring_key_mapping
+            `{"input": "user_question"}` to map the "user_question" key to "input".
+
+        dataset_item_ids: list of dataset item ids to evaluate. If not provided, all samples in the dataset will be evaluated.
+    """
+    if scoring_metrics is None:
+        scoring_metrics = []
+
+    checked_prompts = experiment_helpers.handle_prompt_args(
+        prompt=prompt,
+        prompts=prompts,
+    )
+
+    client = opik_client.get_client_cached()
+
+    experiment = client.create_experiment(
+        name=experiment_name,
+        dataset_name=dataset.name,
+        experiment_config=experiment_config,
+        prompts=checked_prompts,
+        type="trial",
+        optimization_id=optimization_id,
+    )
+
+    return _evaluate_task(
+        client=client,
+        experiment=experiment,
+        dataset=dataset,
+        task=task,
+        scoring_metrics=scoring_metrics,
+        project_name=project_name,
+        verbose=verbose,
+        nb_samples=nb_samples,
+        task_threads=task_threads,
+        scoring_key_mapping=scoring_key_mapping,
+        dataset_item_ids=dataset_item_ids,
+    )
