9d4350b544d513bd1adbd5c1f35d2a2ea27e6cca
Thiago Hora <thiagoh@comet.com>
2025-05-05 15:21:57 +0200

    Initial commit

--- apps/opik-backend/run_db_migrations.sh
+++ apps/opik-backend/run_db_migrations.sh
@@ -6,6 +6,6 @@ echo "Current directory is: $(pwd)"
 echo "OPIK_VERSION=$OPIK_VERSION"
 
 java -jar opik-backend-"$OPIK_VERSION".jar db migrate config.yml \
-  && java -jar opik-backend-"$OPIK_VERSION".jar dbAnalytics migrate config.yml
+  && java -Dliquibase.clickhouse.configfile=liquibaseClickhouse.conf -jar opik-backend-"$OPIK_VERSION".jar dbAnalytics migrate config.yml
 
 echo "Database migrations completed successfully"
