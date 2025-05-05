1ed3d87d9cc10140bb86c32470ea24241d85abea
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2025-05-05 15:44:43 +0300

    [OPIK-1501]: Implemented ADK library integration tests for token count tracking

--- sdks/python/tests/e2e_library_integration/adk/sample_agent/agent.py
+++ sdks/python/tests/e2e_library_integration/adk/sample_agent/agent.py
@@ -1,6 +1,9 @@
 import datetime
+import os
+from typing import Any
 from zoneinfo import ZoneInfo
 from google.adk.agents import Agent
+from google.adk.agents.callback_context import CallbackContext
 
 from opik.integrations.adk import OpikTracer
 
@@ -54,17 +57,29 @@ def get_current_time(city: str) -> dict:
 
 
 opik_tracer = OpikTracer()
+print(
+    "Project name:",
+    os.environ.get("OPIK_PROJECT_NAME", "no project name set in env vars!"),
+)
+
+
+def after_agent_callback(
+    callback_context: CallbackContext, *args: Any, **kwargs: Any
+) -> None:
+    opik_tracer.after_agent_callback(callback_context, *args, **kwargs)
+    opik_tracer._client.flush()
+
 
 root_agent = Agent(
     name="weather_time_agent",
     model="gemini-2.0-flash",
-    description=("Agent to answer questions about the time and weather in a city."),
+    description="Agent to answer questions about the time and weather in a city.",
     instruction=(
         "You are a helpful agent who can answer user questions about the time and weather in a city."
     ),
     tools=[get_weather, get_current_time],
     before_agent_callback=opik_tracer.before_agent_callback,
-    after_agent_callback=opik_tracer.after_agent_callback,
+    after_agent_callback=after_agent_callback,
     before_model_callback=opik_tracer.before_model_callback,
     after_model_callback=opik_tracer.after_model_callback,
     before_tool_callback=opik_tracer.before_tool_callback,
