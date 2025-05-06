fe63cea7e7e547ceb85015caacfd1cf2f6ab30b8
andrii.dudar <fanat.web@gmail.com>
2025-05-06 10:37:58 +0200

    [ISSUE-2015] [Bug]: A trace's Agent graph doesn't show if there isn't one at top level #2015

--- apps/opik-frontend/src/components/shared/MermaidDiagram/MermaidDiagram.tsx
+++ apps/opik-frontend/src/components/shared/MermaidDiagram/MermaidDiagram.tsx
@@ -1,17 +1,40 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import mermaid from "mermaid";
+
+mermaid.initialize({
+  startOnLoad: false,
+  htmlLabels: true,
+  securityLevel: "loose",
+});
 
 type MermaidDiagramProps = {
   chart: string;
 };
 
 const MermaidDiagram: React.FC<MermaidDiagramProps> = ({ chart }) => {
+  const [svg, setSvg] = useState<string>("");
+
   useEffect(() => {
-    mermaid.initialize({ startOnLoad: true });
-    mermaid.contentLoaded();
+    const renderChart = async () => {
+      try {
+        const { svg } = await mermaid.render("mermaid-diagram", chart);
+        setSvg(svg);
+      } catch (error) {
+        console.error("Failed to render mermaid diagram", error);
+      }
+    };
+
+    renderChart();
   }, [chart]);
 
-  return <div className="mermaid size-full">{chart}</div>;
+  return (
+    <div
+      dangerouslySetInnerHTML={{
+        __html: svg,
+      }}
+      className="mermaid flex size-full items-center justify-center"
+    />
+  );
 };
 
 export default MermaidDiagram;
