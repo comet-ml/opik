a10591c7d61f2a5648db3c667f613c51b65b255d
andrii.dudar <fanat.web@gmail.com>
2025-05-06 13:19:02 +0200

    init 1

--- apps/opik-frontend/src/components/pages/CompareOptimizationsPage/OptimizationProgressChartContent.tsx
+++ apps/opik-frontend/src/components/pages/CompareOptimizationsPage/OptimizationProgressChartContent.tsx
@@ -13,6 +13,11 @@ import {
 } from "@/components/ui/chart";
 import { DEFAULT_CHART_TICK } from "@/constants/chart";
 import useChartTickDefaultConfig from "@/hooks/charts/useChartTickDefaultConfig";
+import {
+  Popover,
+  PopoverAnchor,
+  PopoverContent,
+} from "@/components/ui/popover";
 
 export type DataRecord = {
   entityId: string;
@@ -36,10 +41,15 @@ const OptimizationProgressChartContent: React.FC<
 > = ({ chartData, bestEntityId }) => {
   const { line, data } = chartData;
   const [, setActiveLine] = useState<string | null>(null);
+  const [position, setPosition] = useState<
+    { x: number; y: number } | undefined
+  >();
 
   const config = useMemo(() => {
     return getDefaultHashedColorsChartConfig([line]);
   }, [line]);
+
+  const lineColor = config[line].color as string;
 
   const values = useMemo(() => data.map((d) => d.value), [data]);
 
@@ -72,12 +82,56 @@ const OptimizationProgressChartContent: React.FC<
     [],
   );
 
+  const renderPopover = () => {
+    if (!position) return null;
+
+    const leftCorrection = -9;
+
+    const bgStyles: React.CSSProperties = {
+      backgroundColor: `color-mix(in srgb, ${lineColor} 10%, white 100%)`,
+    };
+
+    return (
+      <div
+        style={
+          {
+            "--line-color": lineColor,
+            top: position.y,
+            left: position.x + leftCorrection,
+          } as React.CSSProperties
+        }
+        className="pointer-events-none absolute z-10"
+      >
+        <div
+          className="comet-body-s rounded px-2 py-1 text-[--line-color]"
+          style={bgStyles}
+        >
+          Best prompt
+        </div>
+        <div
+          className="mx-auto -mt-1.5 size-2.5 rotate-45 rounded-[2px]"
+          style={bgStyles}
+        ></div>
+      </div>
+    );
+  };
+
   const renderDot: LineDot = (props) => {
     const { key, ...rest } = props;
     const color = config[props.name as string].color;
     const height = 80;
     const radius = 8;
     if (props.payload.entityId === bestEntityId) {
+      setPosition((state) => {
+        if (state?.x !== props.cx || state?.y !== props.cy) {
+          return {
+            x: props.cx,
+            y: props.cy,
+          };
+        }
+
+        return state;
+      });
       return (
         <React.Fragment key={key}>
           <Dot {...rest} fill={color} strokeWidth={0} r={radius} />
@@ -106,72 +160,67 @@ const OptimizationProgressChartContent: React.FC<
   };
 
   return (
-    <ChartContainer config={config} className="h-40 w-full">
-      <AreaChart
-        data={chartData.data}
-        margin={{ top: 10, bottom: 10, left: 10, right: 10 }}
-      >
-        <CartesianGrid vertical={false} />
-        <XAxis
-          dataKey="time"
-          axisLine={false}
-          tickLine={false}
-          tick={DEFAULT_CHART_TICK}
-          tickFormatter={(value, index) => data[index]?.entityName}
-        />
-        <YAxis
-          width={tickWidth}
-          axisLine={false}
-          tickLine={false}
-          tick={DEFAULT_CHART_TICK}
-          interval={tickInterval}
-          ticks={ticks}
-          tickFormatter={yTickFormatter}
-          domain={domain}
-        />
-        <ChartTooltip
-          isAnimationActive={false}
-          content={<ChartTooltipContent renderHeader={renderHeader} />}
-        />
-        <ChartLegend
-          content={
-            <ChartHorizontalLegendContent
-              setActiveLine={setActiveLine}
-              chartId="optimization-progress-chart"
-            />
-          }
-        />
-        <defs>
-          <linearGradient id="area" x1="0" y1="0" x2="0" y2="1">
-            <stop
-              offset="5%"
-              stopColor={config[line].color as string}
-              stopOpacity={0.2}
-            />
-            <stop
-              offset="75%"
-              stopColor={config[line].color as string}
-              stopOpacity={0}
-            />
-          </linearGradient>
-        </defs>
-        <Area
-          type="linear"
-          key={line}
-          dataKey={(record) => record.value}
-          name={config[line].label as string}
-          stroke={config[line].color as string}
-          fillOpacity={1}
-          fill="url(#area)"
-          dot={renderDot}
-          activeDot={{ strokeWidth: 2, stroke: "white" }}
-          strokeWidth={1.5}
-          strokeOpacity={1}
-          animationDuration={800}
-          connectNulls={false}
-        />
-      </AreaChart>
-    </ChartContainer>
+    <>
+      {renderPopover()}
+      <ChartContainer config={config} className="h-40 w-full">
+        <AreaChart
+          data={chartData.data}
+          margin={{ top: 10, bottom: 10, left: 10, right: 10 }}
+        >
+          <CartesianGrid vertical={false} />
+          <XAxis
+            dataKey="time"
+            axisLine={false}
+            tickLine={false}
+            tick={DEFAULT_CHART_TICK}
+            tickFormatter={(value, index) => data[index]?.entityName}
+          />
+          <YAxis
+            width={tickWidth}
+            axisLine={false}
+            tickLine={false}
+            tick={DEFAULT_CHART_TICK}
+            interval={tickInterval}
+            ticks={ticks}
+            tickFormatter={yTickFormatter}
+            domain={domain}
+          />
+          <ChartTooltip
+            isAnimationActive={false}
+            content={<ChartTooltipContent renderHeader={renderHeader} />}
+          />
+          <ChartLegend
+            content={
+              <ChartHorizontalLegendContent
+                setActiveLine={setActiveLine}
+                chartId="optimization-progress-chart"
+              />
+            }
+          />
+          <defs>
+            <linearGradient id="area" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor={lineColor} stopOpacity={0.2} />
+              <stop offset="75%" stopColor={lineColor} stopOpacity={0} />
+            </linearGradient>
+          </defs>
+          <Area
+            type="linear"
+            key={line}
+            dataKey={(record) => record.value}
+            name={config[line].label as string}
+            stroke={lineColor}
+            fillOpacity={1}
+            fill="url(#area)"
+            dot={renderDot}
+            activeDot={{ strokeWidth: 2, stroke: "white" }}
+            strokeWidth={1.5}
+            strokeOpacity={1}
+            animationDuration={800}
+            connectNulls={false}
+          />
+        </AreaChart>
+      </ChartContainer>
+    </>
   );
 };
 
