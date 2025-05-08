7f0323b34f8078eda17e9498c3ef8a39de55df66
Doug Blank <doug.blank@gmail.com>
2025-05-08 19:04:32 -0400

    Optimizer: version 0.7.1

--- sdks/opik_optimizer/setup.py
+++ sdks/opik_optimizer/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="opik_optimizer",
-    version="0.7.0",
+    version="0.7.1",
     description="Agent optimization with Opik",
     author="Comet ML",
     author_email="info@comet.ml",
