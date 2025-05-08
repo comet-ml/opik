8478cc4c25884ec358b104f8724e2e6c41a8ac23
aadereiko <aliaksandr@comet.com>
2025-05-08 12:57:13 +0200

    [OPIK-1514]: retention banner updates;

---
+++ apps/opik-frontend/src/plugins/comet/api/useOrganizationMembers.ts
@@ -0,0 +1,38 @@
+import { QueryFunctionContext, useQuery } from "@tanstack/react-query";
+import api, { QueryConfig } from "../api";
+
+interface OrganizationMember {
+  username: string;
+  email: string;
+}
+
+interface UseOrganizationMembers {
+  organizationId: string;
+}
+
+const getOrganizationMembers = async (
+  context: QueryFunctionContext,
+  organizationId: string,
+) => {
+  const response = await api.get<OrganizationMember[]>(
+    `/organizations/${organizationId}/members`,
+    {
+      signal: context.signal,
+    },
+  );
+
+  return response?.data;
+};
+
+const useOrganizationMembers = (
+  { organizationId }: UseOrganizationMembers,
+  options?: QueryConfig<OrganizationMember[]>,
+) => {
+  return useQuery({
+    queryKey: ["organization-members", { organizationId }],
+    queryFn: (context) => getOrganizationMembers(context, organizationId),
+    ...options,
+  });
+};
+
+export default useOrganizationMembers;
