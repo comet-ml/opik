75ddd0ba58bd2fb5702693f7e7f0b987af3d8b33
Thiago Hora <thiagoh@comet.com>
2025-05-09 10:59:14 +0200

    Remove settings from migrations as alter_sync is the default

--- apps/opik-backend/src/main/resources/liquibase/db-app-analytics/migrations/000024_add_output_input_pre_computed_columns_to_spans_and_traces.sql
+++ apps/opik-backend/src/main/resources/liquibase/db-app-analytics/migrations/000024_add_output_input_pre_computed_columns_to_spans_and_traces.sql
@@ -4,14 +4,12 @@
 ALTER TABLE ${ANALYTICS_DB_DATABASE_NAME}.traces ON CLUSTER '{cluster}'
     ADD COLUMN IF NOT EXISTS has_input BOOL MATERIALIZED notEmpty(input),
     ADD COLUMN IF NOT EXISTS has_output BOOL MATERIALIZED notEmpty(output),
-    ADD COLUMN IF NOT EXISTS has_metadata BOOL MATERIALIZED notEmpty(metadata)
-    SETTINGS alter_sync=1;
+    ADD COLUMN IF NOT EXISTS has_metadata BOOL MATERIALIZED notEmpty(metadata);
 
 ALTER TABLE ${ANALYTICS_DB_DATABASE_NAME}.spans ON CLUSTER '{cluster}'
     ADD COLUMN IF NOT EXISTS has_input BOOL MATERIALIZED notEmpty(input),
     ADD COLUMN IF NOT EXISTS has_output BOOL MATERIALIZED notEmpty(output),
-    ADD COLUMN IF NOT EXISTS has_metadata BOOL MATERIALIZED notEmpty(metadata)
-    SETTINGS alter_sync=1;
+    ADD COLUMN IF NOT EXISTS has_metadata BOOL MATERIALIZED notEmpty(metadata);
 
 --rollback ALTER TABLE ${ANALYTICS_DB_DATABASE_NAME}.spans ON CLUSTER '{cluster}' DROP COLUMN IF EXISTS has_input, DROP COLUMN IF EXISTS has_output, DROP COLUMN IF EXISTS has_metadata;
 --rollback ALTER TABLE ${ANALYTICS_DB_DATABASE_NAME}.traces ON CLUSTER '{cluster}' DROP COLUMN IF EXISTS has_input, DROP COLUMN IF EXISTS has_output, DROP COLUMN IF EXISTS has_metadata;
