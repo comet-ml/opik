578258be119576eafda0a0893d6517d4e4ba9997
Thiago Hora <thiagoh@comet.com>
2025-05-09 10:49:49 +0200

    Use BOOL instead of BOOLEAN

--- apps/opik-backend/src/main/resources/liquibase/db-app-analytics/migrations/000024_add_output_input_pre_computed_columns_to_spans_and_traces.sql
+++ apps/opik-backend/src/main/resources/liquibase/db-app-analytics/migrations/000024_add_output_input_pre_computed_columns_to_spans_and_traces.sql
@@ -2,15 +2,15 @@
 --changeset thiagohora:add_output_input_pre_computed_columns_to_spans_and_traces
 
 ALTER TABLE ${ANALYTICS_DB_DATABASE_NAME}.traces ON CLUSTER '{cluster}'
-    ADD COLUMN IF NOT EXISTS has_input BOOLEAN MATERIALIZED notEmpty(input),
-    ADD COLUMN IF NOT EXISTS has_output BOOLEAN MATERIALIZED notEmpty(output),
-    ADD COLUMN IF NOT EXISTS has_metadata BOOLEAN MATERIALIZED notEmpty(metadata)
+    ADD COLUMN IF NOT EXISTS has_input BOOL MATERIALIZED notEmpty(input),
+    ADD COLUMN IF NOT EXISTS has_output BOOL MATERIALIZED notEmpty(output),
+    ADD COLUMN IF NOT EXISTS has_metadata BOOL MATERIALIZED notEmpty(metadata)
     SETTINGS alter_sync=1;
 
 ALTER TABLE ${ANALYTICS_DB_DATABASE_NAME}.spans ON CLUSTER '{cluster}'
-    ADD COLUMN IF NOT EXISTS has_input BOOLEAN MATERIALIZED notEmpty(input),
-    ADD COLUMN IF NOT EXISTS has_output BOOLEAN MATERIALIZED notEmpty(output),
-    ADD COLUMN IF NOT EXISTS has_metadata BOOLEAN MATERIALIZED notEmpty(metadata)
+    ADD COLUMN IF NOT EXISTS has_input BOOL MATERIALIZED notEmpty(input),
+    ADD COLUMN IF NOT EXISTS has_output BOOL MATERIALIZED notEmpty(output),
+    ADD COLUMN IF NOT EXISTS has_metadata BOOL MATERIALIZED notEmpty(metadata)
     SETTINGS alter_sync=1;
 
 --rollback ALTER TABLE ${ANALYTICS_DB_DATABASE_NAME}.spans ON CLUSTER '{cluster}' DROP COLUMN IF EXISTS has_input, DROP COLUMN IF EXISTS has_output, DROP COLUMN IF EXISTS has_metadata;
