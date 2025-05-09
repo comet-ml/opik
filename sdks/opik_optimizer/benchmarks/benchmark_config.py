746a7811266c3464df552adf3e80d47c89e29f0a
Vincent Koc <vincentkoc@ieee.org>
2025-05-09 09:23:04 +0100

    Update benchmark_config.py

--- sdks/opik_optimizer/benchmarks/benchmark_config.py
+++ sdks/opik_optimizer/benchmarks/benchmark_config.py
@@ -113,28 +113,28 @@ OPTIMIZER_CONFIGS = {
     #         "verbose": 0,
     #     },
     # },
-    # "meta_prompt": {
-    #     "class": "MetaPromptOptimizer",
-    #     "params": {
-    #         "max_rounds": 3,
-    #         "num_prompts_per_round": 4,
-    #         "improvement_threshold": 0.01,
-    #         "temperature": 0.1,
-    #         "max_completion_tokens": 5000,
-    #         "num_threads": 5,
-    #         "subsample_size": 10,
-    #         "verbose": 0,
-    #     },
-    # },
-    "mipro": {
-        "class": "MiproOptimizer",
+    "meta_prompt": {
+        "class": "MetaPromptOptimizer",
         "params": {
+            "max_rounds": 3,
+            "num_prompts_per_round": 4,
+            "improvement_threshold": 0.01,
             "temperature": 0.1,
-            "max_tokens": 5000,
-            "num_threads": 10,
+            "max_completion_tokens": 9000,
+            "num_threads": 5,
+            "subsample_size": 10,
             "verbose": 0,
         },
     },
+    # "mipro": {
+    #     "class": "MiproOptimizer",
+    #     "params": {
+    #         "temperature": 0.1,
+    #         "max_tokens": 5000,
+    #         "num_threads": 10,
+    #         "verbose": 0,
+    #     },
+    # },
     # "external_dspy_mipro": {
     #     "class": "ExternalDspyMiproOptimizer",
     #     "params": {
