34825d8de80a5796d2ecc6f362d55ffe00ee3127
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2025-05-23 18:40:33 +0300

    [OPIK-1625]: Fixed distributed tracing support for threaded spans
    Fixed `span_creation_handler.create_span_for_current_context` method to avoid assertion error if there is no `current_trace_data` but only `current_span_data` which can happen in multithreaded environment. Added related test case.

--- sdks/python/tests/unit/decorator/test_tracker.py
+++ sdks/python/tests/unit/decorator/test_tracker.py
@@ -1,5 +1,7 @@
+import threading
 from unittest import mock
 
+from opik import opik_context
 from opik.api_objects import opik_client
 from opik.decorator import tracker
 
@@ -17,3 +19,34 @@ def test_track__get_client_cached_raised_error__error_is_caught__user_function_i
         assert f() == 42
 
         mock_get_client_cached.assert_called_once()
+
+
+def test_track__using_distributed_headers__headers_are_set_correctly(fake_backend):
+    @tracker.track
+    def inner_thread(x):
+        print("Inner thread")
+        return "inner-thread-output"
+
+    @tracker.track
+    def top_thread(x):
+        print("TOP THREAD")
+        inner_thread(x)
+        return "top-thread-output"
+
+    @tracker.track
+    def do_distributed_trace():
+        headers = opik_context.get_distributed_trace_headers()
+
+        t = threading.Thread(
+            target=top_thread,
+            kwargs={
+                "x": "inner-thread-input",
+                "opik_distributed_trace_headers": headers,
+            },
+        )
+        t.start()
+        t.join()
+
+        return "distributed-trace-output"
+
+    do_distributed_trace()
