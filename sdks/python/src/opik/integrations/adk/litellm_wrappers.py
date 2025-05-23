f32095970de1e0aba1469ed3b56ab55d4a13835c
Alexander Barannikov <japdubengsub@gmail.com>
2025-05-23 17:43:16 +0100

    OPIK-1630 [SDK] Implement token&cost tracking support for LiteLLM models calls in ADK integration

---
+++ sdks/python/src/opik/integrations/adk/litellm_wrappers.py
@@ -0,0 +1,58 @@
+import logging
+from functools import wraps
+from typing import Optional, Tuple
+
+from ... import LLMProvider
+
+LOGGER = logging.Logger(__name__)
+
+
+def parse_provider_and_model(model: str) -> Tuple[Optional[LLMProvider], str]:
+    parts = model.split("/", 1)
+    if len(parts) != 2:
+        return None, parts[0]
+
+    provider = parts[0]
+    try:
+        provider = LLMProvider(provider)
+    except ValueError:
+        pass
+    return provider, parts[1]
+
+
+def generate_content_response_decorator(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        """
+        This wrapper puts token usage data into custom metadata to use it later
+        """
+        result = func(*args, **kwargs)
+
+        model_response = args[0]
+        model_response_dict = model_response.to_dict()
+
+        if result.custom_metadata is None:
+            result.custom_metadata = {}
+
+        provider, model = parse_provider_and_model(model_response_dict["provider"])
+
+        result.custom_metadata["opik_usage"] = model_response_dict["usage"]
+        result.custom_metadata["provider"] = provider
+        result.custom_metadata["model_version"] = model_response_dict["model"]
+
+        return result
+
+    return wrapper
+
+
+def litellm_client_acompletion_decorator(func):
+    @wraps(func)
+    async def wrapper(*args, **kwargs):
+        """
+        this adds more precise provider/model name and it's version
+        """
+        result = await func(*args, **kwargs)
+        result.provider = kwargs.get("model", None)
+        return result
+
+    return wrapper
