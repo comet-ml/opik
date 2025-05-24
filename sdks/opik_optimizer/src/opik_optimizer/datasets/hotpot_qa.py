7139cb895b5cd2cd57d51c84706ee78ed065df61
Jacques Verre <jverre@gmail.com>
2025-05-24 23:35:16 +0100

    Fixed tests

--- sdks/opik_optimizer/src/opik_optimizer/datasets/hotpot_qa.py
+++ sdks/opik_optimizer/src/opik_optimizer/datasets/hotpot_qa.py
@@ -2,6 +2,7 @@ import opik
 from importlib.resources import files
 import json
 
+
 def hotpot_300(
     test_mode: bool = False
 ) -> opik.Dataset:
@@ -9,7 +10,7 @@ def hotpot_300(
     Dataset containing the first 300 samples of the HotpotQA dataset.
     """
     dataset_name = "hotpot_300" if not test_mode else "hotpot_300_test"
-    nb_items = 300 if not test_mode else 3
+    nb_items = 300 if not test_mode else 5
 
     client = opik.Opik()
     dataset = client.get_or_create_dataset(dataset_name)
