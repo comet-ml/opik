7ff508d6cdd88cb950862bcf5b69e71052a4d909
Jacques Verre <jverre@gmail.com>
2025-05-24 23:35:43 +0100

    Release optimizer 0.8.1

--- sdks/opik_optimizer/setup.py
+++ sdks/opik_optimizer/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="opik_optimizer",
-    version="0.8.0",
+    version="0.8.1",
     description="Agent optimization with Opik",
     author="Comet ML",
     author_email="support@comet.com",
