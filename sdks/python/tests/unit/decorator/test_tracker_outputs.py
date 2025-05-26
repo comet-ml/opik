2e0f09ac1170661ef00248cb03c91f694d042305
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2025-05-26 18:14:45 +0300

    [OPIK-1625]: Refactor test functions to clarify naming in tracker outputs
    Renamed functions from "inner_thread" and "top_thread" to "inner_function_in_thread" and "top_function_in_thread" respectively for improved clarity and consistency. Updated all related inputs, outputs, and references in the test cases to reflect the changes.

--- sdks/python/tests/unit/decorator/test_tracker_outputs.py
+++ sdks/python/tests/unit/decorator/test_tracker_outputs.py
@@ -1638,22 +1638,22 @@ def test_track__function_called_with_mutable_input_which_changed_afterward__chec
 
 def test_track__using_distributed_headers__spans_are_created_correctly(fake_backend):
     @tracker.track
-    def inner_thread(x):
-        return "inner_thread-output"
+    def inner_function_in_thread(x):
+        return "inner_function_in_thread-output"
 
     @tracker.track
-    def top_thread(x):
-        inner_thread(x)
-        return "top_thread-output"
+    def top_function_in_thread(x):
+        inner_function_in_thread(x)
+        return "top_function_in_thread-output"
 
     @tracker.track
     def do_distributed_trace(x):
         headers = opik_context.get_distributed_trace_headers()
 
         t = threading.Thread(
-            target=top_thread,
+            target=top_function_in_thread,
             kwargs={
-                "x": "inner_thread-input",
+                "x": "inner_function_in_thread-input",
                 "opik_distributed_trace_headers": headers,
             },
         )
@@ -1685,17 +1685,17 @@ def test_track__using_distributed_headers__spans_are_created_correctly(fake_back
                     SpanModel(
                         id=ANY_BUT_NONE,
                         start_time=ANY_BUT_NONE,
-                        name="top_thread",
-                        input={"x": "inner_thread-input"},
-                        output={"output": "top_thread-output"},
+                        name="top_function_in_thread",
+                        input={"x": "inner_function_in_thread-input"},
+                        output={"output": "top_function_in_thread-output"},
                         end_time=ANY_BUT_NONE,
                         spans=[
                             SpanModel(
                                 id=ANY_BUT_NONE,
                                 start_time=ANY_BUT_NONE,
-                                name="inner_thread",
-                                input={"x": "inner_thread-input"},
-                                output={"output": "inner_thread-output"},
+                                name="inner_function_in_thread",
+                                input={"x": "inner_function_in_thread-input"},
+                                output={"output": "inner_function_in_thread-output"},
                                 end_time=ANY_BUT_NONE,
                             )
                         ],
@@ -1715,11 +1715,11 @@ def test_track__using_distributed_headers__through_node__spans_are_created_corre
     fake_backend,
 ):
     @tracker.track
-    def inner_thread(x):
-        return "inner_thread-output"
+    def inner_function_in_thread(x):
+        return "inner_function_in_thread-output"
 
     def node(x, opik_headers):
-        inner_thread(x, opik_distributed_trace_headers=opik_headers)
+        inner_function_in_thread(x, opik_distributed_trace_headers=opik_headers)
         return "node-output"
 
     @tracker.track
@@ -1729,7 +1729,7 @@ def test_track__using_distributed_headers__through_node__spans_are_created_corre
         t = threading.Thread(
             target=node,
             kwargs={
-                "x": "inner_thread-input",
+                "x": "inner_function_in_thread-input",
                 "opik_headers": headers,
             },
         )
@@ -1761,9 +1761,9 @@ def test_track__using_distributed_headers__through_node__spans_are_created_corre
                     SpanModel(
                         id=ANY_BUT_NONE,
                         start_time=ANY_BUT_NONE,
-                        name="inner_thread",
-                        input={"x": "inner_thread-input"},
-                        output={"output": "inner_thread-output"},
+                        name="inner_function_in_thread",
+                        input={"x": "inner_function_in_thread-input"},
+                        output={"output": "inner_function_in_thread-output"},
                         end_time=ANY_BUT_NONE,
                     )
                 ],
