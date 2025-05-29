36ae9034b8d0a0e0dd5b2796d0633d7213c75ec2
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2025-05-29 16:53:35 +0300

    [OPIK-1698]: [P SDK] Update dynamic rate limiting to use new response headers sent by the backend (#2293)
    * [OPIK-1698]: Refactor rate limit parsing for simplified implementation
    
    Simplified the `CloudRateLimit` data structure by removing unused fields and updated the parsing logic to detect a new rate limit header. Added handling for standard and lowercase header variations to ensure compatibility.
    
    * [OPIK-1698]: Fixed streamer flushing when rate limiting is applied to the last message. This will guarantee that the streamer will wait until message send retry is completed. Refactor naming for queue consumer and waiting status.
    
    Renamed `workers_waiting` to `workers_idling` and updated related logic and test cases to reflect this change. Improved naming consistency and clarified the functionality around consumer idle states in `QueueConsumer` and `Streamer`. Added a new test to validate `flush` behavior when dynamic rate limits are applied at the streamer flush stage.

--- sdks/python/src/opik/rate_limit/rate_limit.py
+++ sdks/python/src/opik/rate_limit/rate_limit.py
@@ -1,43 +1,25 @@
 import dataclasses
-import re
 from typing import Any, Dict, Optional
 
 
 @dataclasses.dataclass
 class CloudRateLimit:
-    bucket_name: str
-    rate_limit_name: str
     remaining_limit: int
-    remaining_limit_reset_time_ms: int
+    remaining_limit_reset_time: int
 
     def retry_after(self) -> float:
-        return self.remaining_limit_reset_time_ms / 1000.0
+        return self.remaining_limit_reset_time
 
 
 def parse_rate_limit(rate_limit_response: Dict[str, Any]) -> Optional[CloudRateLimit]:
-    pattern = re.compile(r"opik-(.\w*)-limit")
+    rate_limit_reset_time = int(rate_limit_response.get("RateLimit-Reset", 0))
+    if rate_limit_reset_time == 0:
+        rate_limit_reset_time = int(rate_limit_response.get("ratelimit-reset", 0))
 
-    for key in rate_limit_response.keys():
-        match = pattern.match(key)
-        if match:
-            rate_limit_name = match.group(1)
-            rate_limit = CloudRateLimit(
-                bucket_name=rate_limit_response.get(
-                    f"opik-{rate_limit_name}-limit", ""
-                ),
-                rate_limit_name=rate_limit_name,
-                remaining_limit=int(
-                    rate_limit_response.get(
-                        f"opik-{rate_limit_name}-remaining-limit", 0
-                    )
-                ),
-                remaining_limit_reset_time_ms=int(
-                    rate_limit_response.get(
-                        f"opik-{rate_limit_name}-remaining-limit-ttl-millis", 0
-                    )
-                ),
-            )
-            if rate_limit.remaining_limit == 0:
-                return rate_limit
+    if rate_limit_reset_time > 0:
+        return CloudRateLimit(
+            remaining_limit=0,
+            remaining_limit_reset_time=rate_limit_reset_time,
+        )
 
     return None
