d6562db377f29a862441f6050b893dfd28bd47f9
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2025-06-02 14:23:46 +0300

    [OPIK-1511]: Update langchain integration to use `trace_end_parameters` for trace creation.

--- sdks/python/src/opik/integrations/langchain/opik_tracer.py
+++ sdks/python/src/opik/integrations/langchain/opik_tracer.py
@@ -139,7 +139,7 @@ class OpikTracer(BaseTracer):
                 trace_data.input = run_dict["inputs"]
 
             trace_data.init_end_time().update(output=output, error_info=error_info)
-            trace_ = self._opik_client.trace(**trace_data.__dict__)
+            trace_ = self._opik_client.trace(**trace_data.trace_end_parameters)
             self._created_traces.append(trace_)
             self._opik_context_storage.pop_trace_data(ensure_id=trace_data.id)
 
