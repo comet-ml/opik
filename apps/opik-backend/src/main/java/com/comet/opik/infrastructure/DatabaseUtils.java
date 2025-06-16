3814f874a6917b4ed1ad776b3aa81c154bfe35e1
Borys Tkachenko <borystkachenko@Boryss-MacBook-Pro.local>
2025-06-16 14:09:29 +0200

    Remove allowPublicKeyRetrieval from MySQL config

---
+++ apps/opik-backend/src/main/java/com/comet/opik/infrastructure/DatabaseUtils.java
@@ -0,0 +1,28 @@
+package com.comet.opik.infrastructure;
+
+import io.dropwizard.db.DataSourceFactory;
+import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.stream.Collectors;
+
+@UtilityClass
+@Slf4j
+public class DatabaseUtils {
+    public static DataSourceFactory filterProperties(DataSourceFactory dataSourceFactory) {
+        log.info("Initial MySQL properties: {}", dataSourceFactory.getProperties());
+        var  filteredProperties = dataSourceFactory.getProperties()
+                .entrySet()
+                .stream()
+                .filter(entry -> StringUtils.isNotBlank(entry.getValue()))
+                .collect(Collectors.toMap(
+                        java.util.Map.Entry::getKey,
+                        java.util.Map.Entry::getValue
+                ));
+        dataSourceFactory.setProperties(filteredProperties);
+        log.info("Filtered MySQL properties: {}", filteredProperties);
+
+        return dataSourceFactory;
+    }
+}
