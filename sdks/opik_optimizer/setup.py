274bac90741c2ad54c0c677fdeb3614bdc925fa2
Doug Blank <doug.blank@gmail.com>
2025-06-25 14:31:52 -0400

    Don't import mipro by default

--- sdks/opik_optimizer/setup.py
+++ sdks/opik_optimizer/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="opik_optimizer",
-    version="1.0.0",
+    version="1.0.1",
     description="Agent optimization with Opik",
     author="Comet ML",
     author_email="support@comet.com",
