48c22462cea0c11c343b4f3c3ab47ab636da3a99
AndreiCautisanu <30831438+AndreiCautisanu@users.noreply.github.com>
2025-06-30 12:34:41 +0300

    OPIK-1957 Andrei - Regression fixes and improved debugging (#2609)
    * delay and playwright tracing
    
    * fix tracing
    
    * zip file type for traces
    
    * change value in test data
    
    * tracing at test level
    
    ---------
    
    Co-authored-by: Andrei Căutișanu <andreicautisanu@ip-192-168-1-158.eu-west-1.compute.internal>

--- tests_end_to_end/tests/Traces/test_trace_spans.py
+++ tests_end_to_end/tests/Traces/test_trace_spans.py
@@ -5,6 +5,7 @@ from page_objects.ProjectsPage import ProjectsPage
 from page_objects.TracesPage import TracesPage
 import logging
 import allure
+import time
 
 logger = logging.getLogger(__name__)
 
@@ -158,12 +159,12 @@ class TestTraceSpans:
                 f"Project name: {project_name}\n"
                 f"Error: {str(e)}"
             ) from e
-
         # Verify details for each trace
         for trace in trace_names:
             logger.info(f"Checking details for trace '{trace}'")
             try:
                 traces_page.click_first_trace_that_has_name(trace)
+                time.sleep(0.5)
                 spans_menu = TracesPageSpansMenu(page)
 
                 # Check each span's details
@@ -179,6 +180,7 @@ class TestTraceSpans:
                     logger.info("Checking feedback scores")
                     try:
                         spans_menu.get_feedback_scores_tab().click()
+                        time.sleep(0.25)
                         for score in span_config["feedback_scores"]:
                             expect(
                                 page.get_by_role("cell", name=score["name"], exact=True)
