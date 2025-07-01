4091e871b81ea82d78a081a531c36a026165106a
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2025-07-01 20:51:16 +0300

    [OPIK-1772]: Add verification of thread feedback scores and end-to-end test for logging threads feedback scores.
    - Added `verify_thread` and `_assert_feedback_scores` utilities to validate feedback scores.
    - Introduced E2E test for logging and verifying logging thread feedback scores.
    - Refactored duplicated feedback score verification logic for reuse.

--- sdks/python/tests/e2e/test_threads.py
+++ sdks/python/tests/e2e/test_threads.py
@@ -2,6 +2,9 @@ import uuid
 
 import pytest
 
+from opik import synchronization
+from opik.types import FeedbackScoreDict
+from . import verifiers
 from .conftest import OPIK_E2E_TESTS_PROJECT_NAME
 
 
@@ -49,3 +52,59 @@ def test_threads_client__search_threads__happy_path(opik_client, active_threads)
     assert len(threads) == 2
     for thread in threads:
         assert thread.id in active_threads
+
+
+def test_threads_client__log_threads_feedback_scores__happy_path(
+    opik_client, active_threads
+):
+    threads_client = opik_client.get_threads_client()
+
+    # close threads before logging scores - otherwise backend will return 409 error
+    for thread_id in active_threads:
+        opik_client.rest_client.traces.close_trace_thread(
+            project_name=OPIK_E2E_TESTS_PROJECT_NAME, thread_id=thread_id
+        )
+
+    def check_threads_closed() -> bool:
+        threads = threads_client.search_threads(
+            project_name=OPIK_E2E_TESTS_PROJECT_NAME, filter_string='status = "active"'
+        )
+        return len(threads) == 0
+
+    # wait for closed threads to propagate
+    synchronization.wait_for_done(lambda: check_threads_closed(), timeout=30)
+
+    # log feedback scores
+    scores = [
+        FeedbackScoreDict(
+            id=active_threads[0],
+            name="thread-metric-1",
+            value=0.75,
+            category_name="category-3",
+            reason="some-reason-3",
+        ),
+        FeedbackScoreDict(
+            id=active_threads[1],
+            name="thread-metric-2",
+            value=0.25,
+            category_name="category-4",
+            reason="some-reason-4",
+        ),
+    ]
+    threads_client.log_threads_feedback_scores(
+        scores=scores, project_name=OPIK_E2E_TESTS_PROJECT_NAME
+    )
+
+    verifiers.verify_thread(
+        opik_client=opik_client,
+        thread_id=active_threads[0],
+        project_name=OPIK_E2E_TESTS_PROJECT_NAME,
+        feedback_scores=[scores[0]],
+    )
+
+    verifiers.verify_thread(
+        opik_client=opik_client,
+        thread_id=active_threads[1],
+        project_name=OPIK_E2E_TESTS_PROJECT_NAME,
+        feedback_scores=[scores[1]],
+    )
