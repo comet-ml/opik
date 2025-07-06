42aa933ebc48f164dbbd1d03a625395be760b8df
Ido Berkovich <ido@comet.com>
2025-07-06 09:31:54 +0300

    patch update failing test green

--- apps/opik-backend/src/main/java/com/comet/opik/api/ProviderApiKeyUpdate.java
+++ apps/opik-backend/src/main/java/com/comet/opik/api/ProviderApiKeyUpdate.java
@@ -6,7 +6,6 @@ import com.fasterxml.jackson.annotation.JsonView;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
 
@@ -18,7 +17,7 @@ import java.util.Map;
 public record ProviderApiKeyUpdate(
         @JsonView( {
                 ProviderApiKey.View.Public.class,
-                ProviderApiKey.View.Write.class}) @NotBlank @JsonDeserialize(using = ProviderApiKeyDeserializer.class) String apiKey,
+                ProviderApiKey.View.Write.class}) @JsonDeserialize(using = ProviderApiKeyDeserializer.class) String apiKey,
         @JsonView({ProviderApiKey.View.Public.class, ProviderApiKey.View.Write.class}) @Size(max = 150) String name,
         @JsonView({ProviderApiKey.View.Public.class, ProviderApiKey.View.Write.class}) Map<String, String> headers,
         @JsonView({ProviderApiKey.View.Public.class,
