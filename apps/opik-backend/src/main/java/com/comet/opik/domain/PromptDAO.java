ac47a24bad8010a800fe2978c1479e395f133a88
Thiago dos Santos Hora <thiagoh@comet.com>
2025-07-21 10:21:01 +0200

    NA: Reduce cost prompt queries (#2775)
    * NA: Reduce cost prompt queries
    
    * Fix prompt queries

--- apps/opik-backend/src/main/java/com/comet/opik/domain/PromptDAO.java
+++ apps/opik-backend/src/main/java/com/comet/opik/domain/PromptDAO.java
@@ -69,22 +69,24 @@ interface PromptDAO {
     Prompt findById(@Bind("id") UUID id, @Bind("workspace_id") String workspaceId);
 
     @SqlQuery("""
-                WITH prompt_full AS (
-                    SELECT
-                    p.*,
-                    count(pv.id) as version_count
-                    FROM prompts p
-                    LEFT JOIN prompt_versions pv ON pv.prompt_id = p.id
-                    WHERE p.workspace_id = :workspace_id
-                    GROUP BY p.id
-                )
-                SELECT *
-                FROM prompt_full
-                WHERE 1 = 1
-                <if(filters)> AND <filters> <endif>
+            SELECT
+                *
+            FROM (
+                SELECT
+                  p.*,
+                  (
+                    SELECT COUNT(pv.id)
+                      FROM prompt_versions pv
+                     WHERE pv.workspace_id = p.workspace_id
+                     AND pv.prompt_id = p.id
+                  ) AS version_count
+                FROM prompts AS p
+                WHERE workspace_id = :workspace_id
                 <if(name)> AND name like concat('%', :name, '%') <endif>
-                ORDER BY <if(sort_fields)> <sort_fields>, <endif> id DESC
-                LIMIT :limit OFFSET :offset
+            ) AS prompt_full
+            <if(filters)> WHERE <filters> <endif>
+            ORDER BY <if(sort_fields)> <sort_fields>, <endif> id DESC
+            LIMIT :limit OFFSET :offset
             """)
     @UseStringTemplateEngine
     @AllowUnusedBindings
@@ -95,20 +97,22 @@ interface PromptDAO {
             @BindMap Map<String, Object> filterMapping);
 
     @SqlQuery("""
-                WITH prompt_full AS (
-                    SELECT
-                    p.*,
-                    count(pv.id) as version_count
-                    FROM prompts p
-                    LEFT JOIN prompt_versions pv ON pv.prompt_id = p.id
-                    WHERE p.workspace_id = :workspace_id
-                    GROUP BY p.id
-                )
-                SELECT COUNT(id)
-                FROM prompt_full
-                WHERE 1 = 1
-                <if(filters)> AND <filters> <endif>
+             SELECT
+                count(id)
+             FROM (
+                SELECT
+                  p.*,
+                  (
+                    SELECT COUNT(pv.id)
+                      FROM prompt_versions pv
+                     WHERE pv.workspace_id = p.workspace_id
+                     AND pv.prompt_id = p.id
+                  ) AS version_count
+                FROM prompts AS p
+                WHERE workspace_id = :workspace_id
                 <if(name)> AND name like concat('%', :name, '%') <endif>
+            ) AS prompt_full
+            <if(filters)> WHERE <filters> <endif>
             """)
     @UseStringTemplateEngine
     @AllowUnusedBindings
