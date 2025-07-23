a18979e1f81cb4e9ab8c7549ecaf64b763cb85d0
vladimirrotariu <rotariuvladimir@gmail.com>
2025-07-23 17:51:42 -0600

    revert eslint and prettier applied to the frontend

--- apps/opik-frontend/src/plugins/comet/newrelic/index.ts
+++
@@ -1,7 +0,0 @@
-import initSnippet from "./snippet";
-
-export const initNewRelic = (licenseKey?: string, applicationID?: string) => {
-  if (licenseKey && applicationID) {
-    initSnippet(licenseKey, applicationID);
-  }
-};
