a3d7f75322bf50044f83965b864515ed6de4c312
Alexander Kuzmik <alexander.kuzmik99@gmail.com>
2025-07-24 13:40:48 +0200

    Add python-multipart for python 3.9 test requirements since it's now required by adk

--- sdks/python/src/opik/integrations/adk/__init__.py
+++ sdks/python/src/opik/integrations/adk/__init__.py
@@ -1,10 +1,10 @@
 import google.adk
 from opik import semantic_version
 
-if semantic_version.SemanticVersion.parse(google.adk.__version__) >= "1.3.0":  # type: ignore
-    from .opik_tracer import OpikTracer
-else:
+if semantic_version.SemanticVersion.parse(google.adk.__version__) < "1.3.0":  # type: ignore
     from .legacy_opik_tracer import LegacyOpikTracer as OpikTracer  # type: ignore
+else:
+    from .opik_tracer import OpikTracer  # type: ignore
 
 from .recursive_callback_injector import track_adk_agent_recursive
 from .graph.mermaid_graph_builder import build_mermaid_graph_definition
