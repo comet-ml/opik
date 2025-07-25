dea8ec4aaedcd4b7cf69435a72f6212700368923
Yaroslav Boiko <y.boikodevelop@gmail.com>
2025-07-25 15:15:45 +0200

    [OPIK-1988] custom date range select for project metrics (#2823)
    * [OPIK-1988] date range picker
    
    * [OPIK-1988] partial selection fix
    
    * [OPIK-1988] date picker as select
    
    * [OPIK-1988] date picket new behavior
    
    * [OPIK-1988] metric date range select refactoring
    
    * [OPIK-1988] apply to a new home page
    
    * [OPIK-1988] remove disableLoadingData prop
    
    * [OPIK-1988] add weekly interval if range has +30 days
    
    * [OPIK-1988] dynamic description based on interval for charts
    
    ---------
    
    Co-authored-by: Yaroslav Boiko <yaroslavboiko@ip-192-168-1-15.ec2.internal>

--- apps/opik-frontend/src/components/pages/HomePage/OverallPerformanceActionsPanel.tsx
+++ apps/opik-frontend/src/components/pages/HomePage/OverallPerformanceActionsPanel.tsx
@@ -1,54 +1,24 @@
 import React from "react";
 
 import { Project } from "@/types/projects";
-import { DropdownOption } from "@/types/shared";
-import SelectBox from "@/components/shared/SelectBox/SelectBox";
 import ProjectSelector from "@/components/pages/HomePage/ProjectSelector";
 
-export enum PERIOD_OPTION_TYPE {
-  THREE_DAYS = "3",
-  SEVEN_DAYS = "7",
-  FOURTEEN_DAYS = "14",
-  THIRTY_DAYS = "30",
-}
-
-const PERIOD_OPTIONS: DropdownOption<PERIOD_OPTION_TYPE>[] = [
-  {
-    value: PERIOD_OPTION_TYPE.THREE_DAYS,
-    label: "3 days",
-  },
-  {
-    value: PERIOD_OPTION_TYPE.SEVEN_DAYS,
-    label: "7 days",
-  },
-  {
-    value: PERIOD_OPTION_TYPE.FOURTEEN_DAYS,
-    label: "14 days",
-  },
-  {
-    value: PERIOD_OPTION_TYPE.THIRTY_DAYS,
-    label: "30 days",
-  },
-];
-
 type OverallPerformanceActionsPanelProps = {
-  period: PERIOD_OPTION_TYPE;
-  setPeriod: (period: PERIOD_OPTION_TYPE) => void;
   projectsIds: string[];
   setProjectsIds: (projectsIds: string[]) => void;
   projects: Project[];
   totalProjects: number;
+  rightSection: React.ReactNode;
 };
 
 const OverallPerformanceActionsPanel: React.FC<
   OverallPerformanceActionsPanelProps
 > = ({
-  period,
-  setPeriod,
   projectsIds,
   setProjectsIds,
   projects,
   totalProjects,
+  rightSection,
 }) => {
   return (
     <div className="flex items-center justify-between gap-4 pt-3">
@@ -58,14 +28,7 @@ const OverallPerformanceActionsPanel: React.FC<
         projects={projects}
         totalProjects={totalProjects}
       ></ProjectSelector>
-      <div className="w-32 shrink-0">
-        <SelectBox
-          value={period}
-          onChange={setPeriod}
-          options={PERIOD_OPTIONS}
-          className="h-8"
-        />
-      </div>
+      <div className="shrink-0">{rightSection}</div>
     </div>
   );
 };
