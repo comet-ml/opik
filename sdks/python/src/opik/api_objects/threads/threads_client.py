eaa3bb0ef49d03b0e3ec9da2d2f2fa72cfba634f
Alexander Kuzmik <alexander.kuzmik99@gmail.com>
2025-07-28 17:35:41 +0200

    Implement threads client, add tests for it, update old threads test to use temporary project to avoid accidental keeping of non-closed threads that can break tests

--- sdks/python/src/opik/api_objects/threads/threads_client.py
+++ sdks/python/src/opik/api_objects/threads/threads_client.py
@@ -134,6 +134,20 @@ class ThreadsClient:
 
             self._opik_client._streamer.put(add_threads_feedback_scores_batch_message)
 
+    def close_thread(self, id: str, project_name: str) -> None:
+        """
+        Closes a thread in a specific project.
+
+        Args:
+            id: The ID of the thread to close.
+            project_name: The name of the project to close the thread in. If not provided, the thread will be closed in the project the client is associated with.
+        """
+        project_name = project_name or self._opik_client.project_name
+        self._opik_client.rest_client.traces.close_trace_thread(
+            thread_id=id,
+            project_name=project_name
+        )
+
     @property
     def opik_client(self) -> "opik.Opik":
         return self._opik_client
--- sdks/python/tests/e2e/test_threads.py
+++ sdks/python/tests/e2e/test_threads.py
@@ -1,15 +1,17 @@
+from typing import Tuple
+
 import uuid
 
 import pytest
 
+import opik
 from opik import synchronization
 from opik.types import FeedbackScoreDict
 from . import verifiers
-from .conftest import OPIK_E2E_TESTS_PROJECT_NAME
 
 
 @pytest.fixture
-def active_threads(opik_client):
+def active_threads_and_project(opik_client: opik.Opik, temporary_project_name: str) -> Tuple[list[str], str]:
     thread_ids = [str(uuid.uuid4())[-6:], str(uuid.uuid4())[-6:]]
 
     for thread_id in thread_ids:
@@ -18,28 +20,23 @@ def active_threads(opik_client):
                 name=f"trace-name-{i}:{thread_id}",
                 input={"input": f"trace-input-{i}:{thread_id}"},
                 output={"output": f"trace-output-{i}:{thread_id}"},
-                project_name=OPIK_E2E_TESTS_PROJECT_NAME,
+                project_name=temporary_project_name,
                 thread_id=thread_id,
             )
 
     opik_client.flush()
 
-    yield thread_ids
-
-    # close threads
-    for thread_id in thread_ids:
-        opik_client.rest_client.traces.close_trace_thread(
-            project_name=OPIK_E2E_TESTS_PROJECT_NAME, thread_id=thread_id
-        )
+    return thread_ids, temporary_project_name
 
 
-def test_threads_client__search_threads__happy_path(opik_client, active_threads):
+def test_threads_client__search_threads__happy_path(opik_client: opik.Opik, active_threads_and_project: Tuple[list[str], str]):
+    active_threads, temporary_project_name = active_threads_and_project
     threads_client = opik_client.get_threads_client()
 
     # Search for the first thread by ID filter
     filter_string = f'id = "{active_threads[0]}"'
     threads = threads_client.search_threads(
-        project_name=OPIK_E2E_TESTS_PROJECT_NAME, filter_string=filter_string
+        project_name=temporary_project_name, filter_string=filter_string
     )
     assert len(threads) == 1
     assert threads[0].id == active_threads[0]
@@ -47,7 +44,7 @@ def test_threads_client__search_threads__happy_path(opik_client, active_threads)
     # Search for all threads with active status
     filter_string = 'status = "active"'
     threads = threads_client.search_threads(
-        project_name=OPIK_E2E_TESTS_PROJECT_NAME, filter_string=filter_string
+        project_name=temporary_project_name, filter_string=filter_string
     )
     assert len(threads) == 2
     for thread in threads:
@@ -55,19 +52,20 @@ def test_threads_client__search_threads__happy_path(opik_client, active_threads)
 
 
 def test_threads_client__log_threads_feedback_scores__happy_path(
-    opik_client, active_threads
+    opik_client: opik.Opik, active_threads_and_project: Tuple[list[str], str]
 ):
+    active_threads, temporary_project_name = active_threads_and_project
     threads_client = opik_client.get_threads_client()
 
     # close threads before logging scores - otherwise backend will return 409 error
     for thread_id in active_threads:
         opik_client.rest_client.traces.close_trace_thread(
-            project_name=OPIK_E2E_TESTS_PROJECT_NAME, thread_id=thread_id
+            project_name=temporary_project_name, thread_id=thread_id
         )
 
     def check_threads_closed() -> bool:
         threads = threads_client.search_threads(
-            project_name=OPIK_E2E_TESTS_PROJECT_NAME, filter_string='status = "active"'
+            project_name=temporary_project_name, filter_string='status = "active"'
         )
         return len(threads) == 0
 
@@ -92,19 +90,50 @@ def test_threads_client__log_threads_feedback_scores__happy_path(
         ),
     ]
     threads_client.log_threads_feedback_scores(
-        scores=scores, project_name=OPIK_E2E_TESTS_PROJECT_NAME
+        scores=scores, project_name=temporary_project_name
     )
 
     verifiers.verify_thread(
         opik_client=opik_client,
         thread_id=active_threads[0],
-        project_name=OPIK_E2E_TESTS_PROJECT_NAME,
+        project_name=temporary_project_name,
         feedback_scores=[scores[0]],
     )
 
     verifiers.verify_thread(
         opik_client=opik_client,
         thread_id=active_threads[1],
-        project_name=OPIK_E2E_TESTS_PROJECT_NAME,
+        project_name=temporary_project_name,
         feedback_scores=[scores[1]],
     )
+
+
+def test_threads_client__close_thread__happy_path(opik_client: opik.Opik, temporary_project_name):
+    threads_client = opik_client.get_threads_client()
+
+    thread_id = str(uuid.uuid4())[-6:]
+    opik_client.trace(
+        name="some-trace-name",
+        thread_id=thread_id,
+        project_name=temporary_project_name,
+    )
+
+    opik_client.flush()
+
+    threads = threads_client.search_threads(
+        project_name=temporary_project_name,
+        filter_string=f'status = "active"',
+    )
+    assert len(threads) == 1
+    assert threads[0].id == thread_id
+
+    threads_client.close_thread(
+        id=thread_id,
+        project_name=temporary_project_name,
+    )
+
+    threads = threads_client.search_threads(
+        project_name=temporary_project_name,
+        filter_string='status = "active"',
+    )
+    assert len(threads) == 0
