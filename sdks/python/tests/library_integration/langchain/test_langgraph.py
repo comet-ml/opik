ff863f7e418e77ce50e1b278bd2a0d45e73422e1
Alexander Kuzmik <alexander.kuzmik99@gmail.com>
2025-07-28 14:59:55 +0200

    Add one more test

--- sdks/python/tests/library_integration/langchain/test_langgraph.py
+++ sdks/python/tests/library_integration/langchain/test_langgraph.py
@@ -219,3 +219,120 @@ def test_langgraph__ChatOpenAI_used_in_the_node_with_config__langchain_looses_pa
     assert len(fake_backend.trace_trees) == 1
     assert len(opik_tracer.created_traces()) == 1
     assert_equal(EXPECTED_TRACE_TREE, fake_backend.trace_trees[0])
+
+
+def test_langgraph__ChatOpenAI_used_in_the_node_with_config__langchain_looses_parent_child_relationship_for_Run__invoked_from_opik_tracked_function__creates_nested_trace_structure(
+    fake_backend,
+):
+    class State(TypedDict):
+        # Messages have the type "list". The `add_messages` function
+        # in the annotation defines how this state key should be updated
+        # (in this case, it appends messages to the list, rather than overwriting them)
+        messages: Annotated[list, langgraph_message.add_messages]
+
+    opik_tracer = OpikTracer()
+    llm = langchain_openai.ChatOpenAI(
+        model=OPENAI_MODEL_FOR_TESTS,
+    )
+
+    graph_builder = StateGraph(State)
+
+    def chatbot_with_config_passed(state: State):
+        """
+        If we pass config with OpikTracer callback in invoke method, Langchain will lose
+        parent-child relationship for Run (it will work but will be considered a root span).
+        OpikTracer restores it via its context.
+        """
+        config = {"callbacks": [opik_tracer]}
+
+        return {"messages": [llm.invoke(state["messages"], config=config)]}
+
+    graph_builder.add_node("chatbot_with_config_passed", chatbot_with_config_passed)
+    graph_builder.add_edge(START, "chatbot_with_config_passed")
+    graph_builder.add_edge("chatbot_with_config_passed", END)
+
+    graph = graph_builder.compile()
+
+    @opik.track(name="f")
+    def invoke_graph_from_tracked_function(input_data):
+        return graph.invoke(
+            input=input_data,
+            config={"callbacks": [opik_tracer]},
+        )
+
+    input = {"messages": [HumanMessage(content="Tell a short joke?")]}
+    result = invoke_graph_from_tracked_function(input)
+
+    expected_input = jsonable_encoder.encode(input)
+    expected_trace_output = jsonable_encoder.encode(result)
+    opik_tracer.flush()
+
+    EXPECTED_TRACE_TREE = TraceModel(
+        id=ANY_BUT_NONE,
+        name="f",
+        input={"input_data": expected_input},
+        output=expected_trace_output,
+        start_time=ANY_BUT_NONE,
+        end_time=ANY_BUT_NONE,
+        last_updated_at=ANY_BUT_NONE,
+        spans=[
+            SpanModel(
+                id=ANY_BUT_NONE,
+                name="f",
+                input={"input_data": expected_input},
+                output=expected_trace_output,
+                start_time=ANY_BUT_NONE,
+                end_time=ANY_BUT_NONE,
+                spans=[
+                    SpanModel(
+                        id=ANY_BUT_NONE,
+                        name="LangGraph",
+                        input=expected_input,
+                        output=ANY_DICT.containing({"messages": ANY_LIST}),
+                        metadata=ANY_DICT.containing({"created_from": "langchain"}),
+                        start_time=ANY_BUT_NONE,
+                        end_time=ANY_BUT_NONE,
+                        spans=[
+                            SpanModel(
+                                id=ANY_BUT_NONE,
+                                name="chatbot_with_config_passed",
+                                input=expected_input,
+                                output=ANY_DICT.containing({"messages": ANY_LIST}),
+                                metadata=ANY_DICT.containing(
+                                    {"created_from": "langchain"}
+                                ),
+                                start_time=ANY_BUT_NONE,
+                                end_time=ANY_BUT_NONE,
+                                spans=[
+                                    SpanModel(
+                                        id=ANY_BUT_NONE,
+                                        name="ChatOpenAI",
+                                        input={"messages": ANY_LIST},
+                                        output=ANY_DICT.containing(
+                                            {"generations": ANY_LIST}
+                                        ),
+                                        metadata=ANY_DICT.containing(
+                                            {"created_from": "langchain"}
+                                        ),
+                                        start_time=ANY_BUT_NONE,
+                                        end_time=ANY_BUT_NONE,
+                                        usage=EXPECTED_FULL_OPENAI_USAGE_LOGGED_FORMAT,
+                                        model=ANY_STRING.starting_with(
+                                            OPENAI_MODEL_FOR_TESTS
+                                        ),
+                                        provider="openai",
+                                        type="llm",
+                                        spans=[],
+                                    ),
+                                ],
+                            ),
+                        ],
+                    ),
+                ],
+            ),
+        ],
+    )
+
+    assert len(fake_backend.trace_trees) == 1
+    assert len(opik_tracer.created_traces()) == 0
+    assert_equal(EXPECTED_TRACE_TREE, fake_backend.trace_trees[0])
