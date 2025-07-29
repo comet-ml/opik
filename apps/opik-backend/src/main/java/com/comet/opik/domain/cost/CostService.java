ba50b15af88caeacd18c6b17fedc93182308b4b4
vladimirrotariu <rotariuvladimir@gmail.com>
2025-07-29 00:18:16 -0600

    fix syntax errors

--- apps/opik-backend/src/main/java/com/comet/opik/domain/cost/CostService.java
+++ apps/opik-backend/src/main/java/com/comet/opik/domain/cost/CostService.java
@@ -1,5 +1,13 @@
 package com.comet.opik.domain.cost;
 
+import com.comet.opik.api.ModelCostData;
+import com.comet.opik.utils.JsonUtils;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.annotation.Nullable;
+
 import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.math.BigDecimal;
@@ -9,123 +17,115 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.function.BiFunction;
 
-import javax.annotation.Nullable;
-
-import com.comet.opik.api.ModelCostData;
-import com.comet.opik.utils.JsonUtils;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.JsonNode;
-
-import lombok.extern.slf4j.Slf4j;
-
 @Slf4j
 public class CostService {
-        private static final char MODEL_PROVIDER_SEPARATOR = '/';
-        private static final Map<String, ModelPrice> modelProviderPrices;
-        private static final Map<String, String> PROVIDERS_MAPPING = Map.of(
-                        "openai", "openai",
-                        "vertex_ai-language-models", "google_vertexai",
-                        "gemini", "google_ai",
-                        "anthropic", "anthropic",
-                        "vertex_ai-anthropic_models", "anthropic_vertexai",
-                        "_bedrock", "bedrock",
-                        "bedrock_converse", "bedrock");
-        private static final String PRICES_FILE = "model_prices_and_context_window.json";
-        private static final Map<String, BiFunction<ModelPrice, Map<String, Integer>, BigDecimal>> PROVIDERS_CACHE_COST_CALCULATOR = Map
-                        .of("anthropic", SpanCostCalculator::textGenerationWithCacheCostAnthropic,
-                                        "openai", SpanCostCalculator::textGenerationWithCacheCostOpenAI);
+    private static final char MODEL_PROVIDER_SEPARATOR = '/';
+    private static final Map<String, ModelPrice> modelProviderPrices;
+    private static final Map<String, String> PROVIDERS_MAPPING = Map.of(
+            "openai", "openai",
+            "vertex_ai-language-models", "google_vertexai",
+            "gemini", "google_ai",
+            "anthropic", "anthropic",
+            "vertex_ai-anthropic_models", "anthropic_vertexai",
+            "_bedrock", "bedrock",
+            "bedrock_converse", "bedrock");
+    private static final String PRICES_FILE = "model_prices_and_context_window.json";
+    private static final Map<String, BiFunction<ModelPrice, Map<String, Integer>, BigDecimal>> PROVIDERS_CACHE_COST_CALCULATOR = Map
+            .of("anthropic", SpanCostCalculator::textGenerationWithCacheCostAnthropic,
+                    "openai", SpanCostCalculator::textGenerationWithCacheCostOpenAI);
 
-        static {
-                try {
-                        modelProviderPrices = Collections.unmodifiableMap(parseModelPrices());
-                } catch (IOException e) {
-                        log.error("Failed to load model prices", e);
-                        throw new UncheckedIOException(e);
-                }
+    static {
+        try {
+            modelProviderPrices = Collections.unmodifiableMap(parseModelPrices());
+        } catch (IOException e) {
+            log.error("Failed to load model prices", e);
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    private static final ModelPrice DEFAULT_COST = new ModelPrice(new BigDecimal("0"),
+            new BigDecimal("0"), new BigDecimal("0"), new BigDecimal("0"), SpanCostCalculator::defaultCost);
+
+    public static BigDecimal calculateCost(@Nullable String modelName, @Nullable String provider,
+            @Nullable Map<String, Integer> usage, @Nullable JsonNode metadata) {
+        ModelPrice modelPrice = Optional.ofNullable(modelName)
+                .flatMap(mn -> Optional.ofNullable(provider).map(p -> createModelProviderKey(mn, p)))
+                .map(modelProviderPrices::get)
+                .orElse(DEFAULT_COST);
+
+        BigDecimal estimatedCost = modelPrice.calculator().apply(modelPrice,
+                Optional.ofNullable(usage).orElse(Map.of()));
+
+        return estimatedCost.compareTo(BigDecimal.ZERO) > 0 ? estimatedCost : getCostFromMetadata(metadata);
+    }
+
+    public static BigDecimal getCostFromMetadata(JsonNode metadata) {
+        return Optional.ofNullable(metadata)
+                .map(md -> md.get("cost"))
+                .map(cost -> Optional.ofNullable(cost.get("currency"))
+                        .map(JsonNode::asText)
+                        .filter("USD"::equals)
+                        .map(currency -> cost.get("total_tokens"))
+                        .map(JsonNode::decimalValue)
+                        .orElse(BigDecimal.ZERO))
+                .orElse(BigDecimal.ZERO);
+    }
+
+    private static Map<String, ModelPrice> parseModelPrices() throws IOException {
+        Map<String, ModelCostData> modelCosts = JsonUtils.readJsonFile(PRICES_FILE, new TypeReference<>() {
+        });
+        if (modelCosts.isEmpty()) {
+            throw new UncheckedIOException(new IOException("Failed to load model prices"));
         }
 
-        private static final ModelPrice DEFAULT_COST = new ModelPrice(new BigDecimal("0"),
-                        new BigDecimal("0"), new BigDecimal("0"), new BigDecimal("0"), SpanCostCalculator::defaultCost);
+        Map<String, ModelPrice> parsedModelPrices = new HashMap<>();
+        modelCosts.forEach((modelName, modelCost) -> {
+            String provider = Optional.ofNullable(modelCost.litellmProvider()).orElse("");
+            if (PROVIDERS_MAPPING.containsKey(provider)) {
 
-        public static BigDecimal calculateCost(@Nullable String modelName, @Nullable String provider,
-                        @Nullable Map<String, Integer> usage, @Nullable JsonNode metadata) {
-                ModelPrice modelPrice = Optional.ofNullable(modelName)
-                                .flatMap(mn -> Optional.ofNullable(provider).map(p -> createModelProviderKey(mn, p)))
-                                .map(modelProviderPrices::get)
-                                .orElse(DEFAULT_COST);
+                BigDecimal inputPrice = Optional.ofNullable(modelCost.inputCostPerToken())
+                        .map(BigDecimal::new)
+                        .orElse(BigDecimal.ZERO);
+                BigDecimal outputPrice = Optional.ofNullable(modelCost.outputCostPerToken())
+                        .map(BigDecimal::new)
+                        .orElse(BigDecimal.ZERO);
+                BigDecimal cacheCreationInputTokenPrice = Optional
+                        .ofNullable(modelCost.cacheCreationInputTokenCost())
+                        .map(BigDecimal::new)
+                        .orElse(BigDecimal.ZERO);
+                BigDecimal cacheReadInputTokenPrice = Optional
+                        .ofNullable(modelCost.cacheReadInputTokenCost())
+                        .map(BigDecimal::new)
+                        .orElse(BigDecimal.ZERO);
 
-                BigDecimal estimatedCost = modelPrice.calculator().apply(modelPrice,
-                                Optional.ofNullable(usage).orElse(Map.of()));
+                BiFunction<ModelPrice, Map<String, Integer>, BigDecimal> calculator = SpanCostCalculator::defaultCost;
+                if (cacheCreationInputTokenPrice.compareTo(BigDecimal.ZERO) > 0
+                        || cacheReadInputTokenPrice.compareTo(BigDecimal.ZERO) > 0) {
+                    calculator = PROVIDERS_CACHE_COST_CALCULATOR.getOrDefault(provider,
+                            SpanCostCalculator::textGenerationCost);
+                } else
+                    if (inputPrice.compareTo(BigDecimal.ZERO) > 0
+                            || outputPrice.compareTo(BigDecimal.ZERO) > 0) {
+                                calculator = SpanCostCalculator::textGenerationCost;
+                            }
 
-                return estimatedCost.compareTo(BigDecimal.ZERO) > 0 ? estimatedCost : getCostFromMetadata(metadata);
-        }
+                parsedModelPrices.put(
+                        createModelProviderKey(parseModelName(modelName),
+                                PROVIDERS_MAPPING.get(provider)),
+                        new ModelPrice(inputPrice, outputPrice, cacheCreationInputTokenPrice,
+                                cacheReadInputTokenPrice, calculator));
+            }
+        });
 
-        public static BigDecimal getCostFromMetadata(JsonNode metadata) {
-                return Optional.ofNullable(metadata)
-                                .map(md -> md.get("cost"))
-                                .map(cost -> Optional.ofNullable(cost.get("currency"))
-                                                .map(JsonNode::asText)
-                                                .filter("USD"::equals)
-                                                .map(currency -> cost.get("total_tokens"))
-                                                .map(JsonNode::decimalValue)
-                                                .orElse(BigDecimal.ZERO))
-                                .orElse(BigDecimal.ZERO);
-        }
+        return parsedModelPrices;
+    }
 
-        private static Map<String, ModelPrice> parseModelPrices() throws IOException {
-                Map<String, ModelCostData> modelCosts = JsonUtils.readJsonFile(PRICES_FILE, new TypeReference<>() {
-                });
-                if (modelCosts.isEmpty()) {
-                        throw new UncheckedIOException(new IOException("Failed to load model prices"));
-                }
+    private static String parseModelName(String modelName) {
+        int prefixIndex = modelName.indexOf('/');
+        return prefixIndex == -1 ? modelName : modelName.substring(prefixIndex + 1);
+    }
 
-                Map<String, ModelPrice> parsedModelPrices = new HashMap<>();
-                modelCosts.forEach((modelName, modelCost) -> {
-                        String provider = Optional.ofNullable(modelCost.litellmProvider()).orElse("");
-                        if (PROVIDERS_MAPPING.containsKey(provider)) {
-
-                                BigDecimal inputPrice = Optional.ofNullable(modelCost.inputCostPerToken())
-                                                .map(BigDecimal::new)
-                                                .orElse(BigDecimal.ZERO);
-                                BigDecimal outputPrice = Optional.ofNullable(modelCost.outputCostPerToken())
-                                                .map(BigDecimal::new)
-                                                .orElse(BigDecimal.ZERO);
-                                BigDecimal cacheCreationInputTokenPrice = Optional
-                                                .ofNullable(modelCost.cacheCreationInputTokenCost())
-                                                .map(BigDecimal::new)
-                                                .orElse(BigDecimal.ZERO);
-                                BigDecimal cacheReadInputTokenPrice = Optional
-                                                .ofNullable(modelCost.cacheReadInputTokenCost())
-                                                .map(BigDecimal::new)
-                                                .orElse(BigDecimal.ZERO);
-
-                                BiFunction<ModelPrice, Map<String, Integer>, BigDecimal> calculator = SpanCostCalculator::defaultCost;
-                                if (cacheCreationInputTokenPrice.compareTo(BigDecimal.ZERO) > 0
-                                                || cacheReadInputTokenPrice.compareTo(BigDecimal.ZERO) > 0) {
-                                        calculator = PROVIDERS_CACHE_COST_CALCULATOR.getOrDefault(provider,
-                                                        SpanCostCalculator::textGenerationCost);
-                                } else if (inputPrice.compareTo(BigDecimal.ZERO) > 0
-                                                || outputPrice.compareTo(BigDecimal.ZERO) > 0) {
-                                        calculator = SpanCostCalculator::textGenerationCost;
-                                }
-
-                                parsedModelPrices.put(
-                                                createModelProviderKey(parseModelName(modelName),
-                                                                PROVIDERS_MAPPING.get(provider)),
-                                                new ModelPrice(inputPrice, outputPrice, cacheCreationInputTokenPrice,
-                                                                cacheReadInputTokenPrice, calculator));
-                        }
-                });
-
-                return parsedModelPrices;
-        }
-
-        private static String parseModelName(String modelName) {
-                int prefixIndex = modelName.indexOf('/');
-                return prefixIndex == -1 ? modelName : modelName.substring(prefixIndex + 1);
-        }
-
-        private static String createModelProviderKey(String modelName, String provider) {
-                return modelName + MODEL_PROVIDER_SEPARATOR + provider;
-        }
+    private static String createModelProviderKey(String modelName, String provider) {
+        return modelName + MODEL_PROVIDER_SEPARATOR + provider;
+    }
 }
