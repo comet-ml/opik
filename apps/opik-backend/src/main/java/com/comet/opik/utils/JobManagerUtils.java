07f73932f7f1573a02542d5c49460601479eaad2
BorisTkachenko <35521895+BorisTkachenko@users.noreply.github.com>
2025-08-04 14:44:22 +0200

    Fix JobManagerUtils (#2882)

--- apps/opik-backend/src/main/java/com/comet/opik/utils/JobManagerUtils.java
+++ apps/opik-backend/src/main/java/com/comet/opik/utils/JobManagerUtils.java
@@ -4,23 +4,33 @@ import io.dropwizard.jobs.JobManager;
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @UtilityClass
 public class JobManagerUtils {
 
     @Getter
-    private static JobManager jobManager;
+    private static volatile JobManager jobManager;
 
     public static synchronized void setJobManager(@NonNull JobManager jobManager) {
 
         if (JobManagerUtils.jobManager != null) {
-            throw new IllegalStateException("JobManager already set");
+            if (JobManagerUtils.jobManager == jobManager) {
+                // Same instance, no need to set again
+                log.debug("JobManager already set with the same instance, skipping");
+                return;
+            }
+
+            clearJobManager();
         }
 
+        log.debug("Setting JobManager instance");
         JobManagerUtils.jobManager = jobManager;
     }
 
     public static synchronized void clearJobManager() {
+        log.debug("Clearing JobManager instance");
         jobManager = null;
     }
 }
