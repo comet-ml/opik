11446f8b2dac7a75956759df6f55cbec23c85593
Ido Berkovich <ido@comet.com>
2025-08-11 09:09:29 +0300

    [OPIK-2120] Extend Feedback Score trace and spans endpoints with multi value scores (#2935)
    * OPIK-2120 add authored_feedback_scores table
    
    * OPIK-2120 change FeedbackScoreDAO to read from the new table
    
    * OPIK-2120 cover usage of the new table
    
    * OPIK-2120 trace dao - some queries work
    
    * OPIK-2120 trace dao - some queries work
    
    * OPIK-2120 trace dao - fix deletion
    
    * OPIK-2120 trace dao - fix assertion
    
    * OPIK-2120 feedbackscore dao - fix delete
    
    * OPIK-2120 span dao - fix queries
    
    * OPIK-2120 span dao - fix queries
    
    * OPIK-2120 span dao - fix queries
    
    * OPIK-2120 added a testing rule
    
    * OPIK-2120 fixed filtering
    
    * OPIK-2120 touch up
    
    * OPIK-2120 fixed source selection
    
    * OPIK-2120 fixed mapping
    
    * OPIK-2120 simplified filters query
    
    * OPIK-2120 simplified filters query
    
    * OPIK-2120 fixes
    
    * OPIK-2120 removed unused code
    
    * OPIK-2120 code structure
    
    * OPIK-2120 code structure
    
    * OPIK-2120 fixing tests
    
    * OPIK-2120 pr comments
    
    * OPIK-2120 pr comments - fix code duplication
    
    * OPIK-2120 pr comments - remove dead code
    
    * OPIK-2120 pr comments - fix last_updated_at
    
    * OPIK-2120 pr comments - remove delete code duplication
    
    * OPIK-2120 changed aggregation order
    
    * OPIK-2120 restored IN logic

---
+++ apps/opik-backend/src/main/java/com/comet/opik/api/ValueEntry.java
@@ -0,0 +1,16 @@
+package com.comet.opik.api;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Builder;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+@Builder(toBuilder = true)
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public record ValueEntry(BigDecimal value, String reason, String categoryName, ScoreSource source,
+        Instant lastUpdatedAt) {
+}
