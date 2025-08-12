ca2b2f20b258681c8dd5e0547aa6b293e3e705b8
Andres Cruz <andresc@comet.com>
2025-08-12 08:54:58 +0200

    [NA]: Thread closing job hanging investigation (#2986)
    * [NA]: Fix thread closing job hanging
    
    * Lock but not action
    
    * Enqueing but not action
    
    * Add to pending queue but not enqueing
    
    * Removed collectList
    
    * Undo temporary investigation changes

--- apps/opik-backend/src/main/java/com/comet/opik/infrastructure/bi/DailyUsageReportJob.java
+++ apps/opik-backend/src/main/java/com/comet/opik/infrastructure/bi/DailyUsageReportJob.java
@@ -194,6 +194,6 @@ public class DailyUsageReportJob extends Job implements InterruptableJob {
     @Override
     public void interrupt() {
         interrupted.set(true);
-        log.info("Daily usage job successfully called interruption");
+        log.info("Daily usage report job successfully called interruption");
     }
 }
