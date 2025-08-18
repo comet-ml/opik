88ce0c561b3d5e230614c7c483ee4f56dabcbcf7
Thiago Hora <thiagoh@comet.com>
2025-08-18 11:15:17 +0200

    OPIK-2252: Address issues for win image build

--- opik.sh
+++ opik.sh
@@ -136,6 +136,7 @@ start_missing_containers() {
   # Export persistent install UUID so docker-compose and services can consume it
   export OPIK_ANONYMOUS_ID="$uuid"
   send_install_report "$uuid" "false" "$start_time"
+  echo "OPIK_ANONYMOUS_ID=$uuid"
 
   debugLog "🔍 Checking required containers..."
   all_running=true
