7152cc7b7f79888f4d6e910be39c2e44932b1997
Vikas_pal8923 <145531494+Vikaspal8923@users.noreply.github.com>
2025-08-18 16:21:51 +0530

    Update sdks/python/src/opik/evaluation/metrics/llm_judges/structure_output_compliance/schema.py
    Co-authored-by: Iaroslav Omelianenko <yaric_mail@yahoo.com>

--- sdks/python/src/opik/evaluation/metrics/llm_judges/structure_output_compliance/schema.py
+++ sdks/python/src/opik/evaluation/metrics/llm_judges/structure_output_compliance/schema.py
@@ -2,7 +2,7 @@ from typing import Optional
 from pydantic import BaseModel
 
 
-class FewShotExampleStructuredOutputCompliance(BaseModel):
+class FewShotExampleStructuredOutputCompliance(pydantic.BaseModel):
     title: str
     output: str
     schema: Optional[str] = None
