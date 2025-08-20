13243784f2a768a605ddf615cbf278c8bdb0e7fd
andrii.dudar <fanat.web@gmail.com>
2025-08-20 14:34:04 +0200

    init 4

--- apps/opik-frontend/src/types/feature-toggles.ts
+++ apps/opik-frontend/src/types/feature-toggles.ts
@@ -3,5 +3,5 @@ export type FeatureToggles = Record<FeatureToggleKeys, boolean>;
 export enum FeatureToggleKeys {
   PYTHON_EVALUATOR_ENABLED = "python_evaluator_enabled",
   GUARDRAILS_ENABLED = "guardrails_enabled",
-  TOGGLE_OPIK_AI_ENABLED = "toggle_opik_ai_enabled",
+  TOGGLE_OPIK_AI_ENABLED = "opik_aienabled",
 }
