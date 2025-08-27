ccdd7772488a2af3767640521cff2f2c35496613
riturajFi <riturajtripathy10x@gmail.com>
2025-08-27 22:15:50 +0530

    feat: integrated update experiment feature in ts sdk

--- sdks/typescript/src/opik/rest_api/api/types/index.ts
+++ sdks/typescript/src/opik/rest_api/api/types/index.ts
@@ -306,3 +306,4 @@ export * from "./WorkspaceMetricResponse";
 export * from "./WorkspaceMetricRequest";
 export * from "./WorkspaceConfiguration";
 export * from "./WorkspaceMetricsSummaryResponse";
+export * from "./UpdateExperimentRequest"
