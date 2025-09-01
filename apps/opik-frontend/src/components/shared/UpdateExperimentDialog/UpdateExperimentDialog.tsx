1d89fe40763a13c0e4b7c65035ecf8f0b1f40b01
riturajFi <riturajtripathy10x@gmail.com>
2025-09-01 20:26:09 +0530

    better json editor for metadata

--- apps/opik-frontend/src/components/shared/UpdateExperimentDialog/UpdateExperimentDialog.tsx
+++ apps/opik-frontend/src/components/shared/UpdateExperimentDialog/UpdateExperimentDialog.tsx
@@ -7,7 +7,7 @@ import {
   DialogAutoScrollBody, 
   DialogFooter 
 } from "@/components/ui/dialog" 
-
+import Editor from "@monaco-editor/react";
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
 import { DialogClose } from "@radix-ui/react-dialog";
@@ -21,7 +21,9 @@ export function UpdateExperimentDialog({
 }) {
 
   const [name, setName] = useState("")
-  const [metadata, setMetadata] = useState("")
+  const [metadata, setMetadata] = useState(
+    JSON.stringify(latestMetadata, null, 2)
+  );
 
   return (
     <Dialog
@@ -48,14 +50,45 @@ export function UpdateExperimentDialog({
 
           {/* Metadata Input */}
           <div className="flex flex-col gap-2 pb-4">
-            <label className="block text-sm font-medium mb-1">Metadata</label>
-            <textarea
+            {/* <label className="block text-sm font-medium mb-1">Metadata</label> */}
+            {/* <textarea
               className="w-full border rounded-md px-3 py-2 text-sm h-32 resize-y focus:outline"
               placeholder={JSON.stringify(latestMetadata, null, 2)}
               onChange={(val) => {
                 setMetadata(val.target.value)
               }}
+            /> */}
+
+            <div className="flex flex-col gap-2 pb-4">
+              <label className="block text-sm font-medium mb-1">Metadata</label>
+              <Editor
+                height="160px"
+                defaultLanguage="json"
+                value={metadata}
+                onChange={(val) => setMetadata(val || "")}
+                options={{
+                  minimap: { enabled: false },
+                  lineNumbers: "off",          // hide line numbers
+                  glyphMargin: false,          // no gutter icons
+                  folding: false,              // no folding arrows
+                  lineDecorationsWidth: 0,
+                  lineNumbersMinChars: 0,
+                  renderLineHighlight: "none", // no line highlight
+                  overviewRulerLanes: 0,       // remove right margin bar
+                  scrollbar: {
+                    verticalScrollbarSize: 6,
+                    horizontalScrollbarSize: 6,
+                  },
+                  fontSize: 13,
+                  wordWrap: "on",
+                  scrollBeyondLastLine: false,
+                  guides: {
+                    indentation: false
+                  } 
+                }}
+                className="rounded-md border border-gray-300 text-sm"
             />
+            </div>
           </div>
         </DialogAutoScrollBody>
         <DialogFooter>
