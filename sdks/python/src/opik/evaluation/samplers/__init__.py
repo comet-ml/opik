610b48b649cf9a3f5ea177e9d0568582dcdf020a
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2025-09-04 19:00:30 +0300

    [OPIK-2286] Add dataset sampling support to the evaluation framework
    - Introduced `BaseDatasetSampler` and `RandomDatasetSampler` for modular dataset sampling.
    - Updated evaluation methods to accept an optional dataset sampler for selective sampling logic.
    - Enhanced unit tests with scenarios for random sampling.
    - Added `samplers` module and integrated it into the evaluation engine.

---
+++ sdks/python/src/opik/evaluation/samplers/__init__.py
@@ -0,0 +1,3 @@
+from .dataset_sampler import BaseDatasetSampler, RandomDatasetSampler
+
+__all__ = ["BaseDatasetSampler", "RandomDatasetSampler"]
---
+++ sdks/python/src/opik/evaluation/samplers/dataset_sampler.py
@@ -0,0 +1,62 @@
+import abc
+from typing import List, Optional
+
+import numpy as np
+
+from opik.api_objects.dataset import dataset_item
+
+
+class BaseDatasetSampler(abc.ABC):
+    """
+    Defines the BaseDatasetSampler for sampling dataset items.
+
+    This is an abstract base class that provides the definition
+    for dataset sampling. It requires implementation of the `sample`
+    method in subclasses, which specifies the sampling logic tailored
+    to specific needs.
+
+    Methods in this class are enforced to be redefined in any
+    concrete implementation.
+
+    """
+
+    @abc.abstractmethod
+    def sample(
+        self, data_item: List[dataset_item.DatasetItem]
+    ) -> List[dataset_item.DatasetItem]:
+        pass
+
+
+class RandomDatasetSampler(BaseDatasetSampler):
+    def __init__(
+        self, max_samples: int, shuffle: bool = True, seed: Optional[int] = None
+    ):
+        """Samples a random subset of dataset items.
+
+        This class is a dataset sampler that selects a random subset of items from a dataset.
+        The number of items to sample can be specified, and shuffling can be enabled or disabled.
+        An optional random seed can be provided for reproducibility.
+
+        Args:
+            max_samples: The maximum number of samples to generate.
+            shuffle: Whether to shuffle the samples. Default is True, False provides a speedup
+                for large datasets.
+            seed: Seed for the random number generator. If None, then fresh, unpredictable
+                entropy will be pulled from the OS.
+        """
+        self.max_samples = max_samples
+        self.shuffle = shuffle
+        self.generator = np.random.default_rng(seed)
+
+    def sample(
+        self, data_item: List[dataset_item.DatasetItem]
+    ) -> List[dataset_item.DatasetItem]:
+        if len(data_item) == 0:
+            return []
+
+        return self.generator.choice(
+            data_item,
+            size=min(len(data_item), self.max_samples),
+            replace=False,
+            shuffle=self.shuffle,
+        ).tolist()
