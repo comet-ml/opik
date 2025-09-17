089283f1b0afa8d1134c7d58da7b393e50445357
vladimirrotariu <rotariuvladimir@gmail.com>
2025-09-17 16:34:08 -0600

    switch to uniform UUIDs, add validation for them

--- apps/opik-backend/src/main/java/com/comet/opik/api/CommentsBatchCreate.java
+++ apps/opik-backend/src/main/java/com/comet/opik/api/CommentsBatchCreate.java
@@ -6,16 +6,19 @@ import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 
 import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 @Builder(toBuilder = true)
 public record CommentsBatchCreate(
-		@Schema(description = "IDs of entities to comment on") @NotEmpty List<UUID> ids,
+		@Schema(description = "IDs of entities to comment on") @NotNull @Size(min = 1, max = 1000) Set<@NotNull UUID> ids,
 		@Schema(description = "Comment text to apply to all entities") @NotBlank String text
 ) {}
 
