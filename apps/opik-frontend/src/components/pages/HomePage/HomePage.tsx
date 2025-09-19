2e73dfca192de2fa8050f6451fb9f312d675e45e
Yaroslav Boiko <yaroslavboiko@ip-192-168-1-15.ec2.internal>
2025-09-19 13:31:28 +0200

    [OPIK-2437] posthog integration

--- apps/opik-frontend/src/components/pages/HomePage/HomePage.tsx
+++ apps/opik-frontend/src/components/pages/HomePage/HomePage.tsx
@@ -6,6 +6,7 @@ import OverallPerformanceSection from "@/components/pages/HomePage/OverallPerfor
 import ObservabilitySection from "@/components/pages/HomePage/ObservabilitySection";
 import EvaluationSection from "@/components/pages/HomePage/EvaluationSection";
 import WelcomeBanner from "@/components/pages/HomePage/WecomeBanner";
+import { PostHogFeature } from "posthog-js/react";
 
 const SHOW_WELCOME_MESSAGE_KEY = "home-welcome-message";
 
@@ -21,9 +22,26 @@ const HomePage = () => {
         <WelcomeBanner setOpen={setShowWelcomeMessage} />
       ) : (
         <div className="mb-4 flex items-center justify-between">
-          <h1 className="comet-title-l truncate break-words">
-            Welcome back to Opik
-          </h1>
+          <PostHogFeature flag="home-page-title">
+            <h1 className="comet-title-l truncate break-words">
+              Welcome back to Opik
+            </h1>
+          </PostHogFeature>
+          <PostHogFeature flag="home-page-title" match={"home-page-title-old"}>
+            <h1 className="comet-title-l truncate break-words">
+              Old Welcome back to Opik
+            </h1>
+          </PostHogFeature>
+          <PostHogFeature flag="home-page-title" match={"home-page-title-new"}>
+            <h1 className="comet-title-l truncate break-words">
+              New Welcome back to Opik
+            </h1>
+          </PostHogFeature>
+          <PostHogFeature flag="home-page-title" match={"control"}>
+            <h1 className="comet-title-l truncate break-words">
+              Control Welcome back to Opik
+            </h1>
+          </PostHogFeature>
         </div>
       )}
       <WorkspaceStatisticSection />
--- apps/opik-frontend/src/config.ts
+++ apps/opik-frontend/src/config.ts
@@ -1,5 +1,7 @@
 export const SENTRY_DSN = import.meta.env.VITE_SENTRY_DSN;
 export const SENTRY_ENABLED = import.meta.env.VITE_SENTRY_ENABLED === "true";
 export const SENTRY_MODE = import.meta.env.VITE_SENTRY_ENVIRONMENT;
+export const POSTHOG_KEY = import.meta.env.VITE_POSTHOG_KEY;
+export const POSTHOG_HOST = import.meta.env.VITE_POSTHOG_HOST;
 
 export const IS_SENTRY_ENABLED = Boolean(SENTRY_DSN && SENTRY_ENABLED);
