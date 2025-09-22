549b8cd40a70d870957e4cafc8e7a293ca49c3d6
Thiago dos Santos Hora <thiagoh@comet.com>
2025-09-22 20:34:23 +0200

    Update 000041_add_truncated_input_output_to_spans_and_traces_tables.sql

--- apps/opik-backend/src/main/resources/liquibase/db-app-analytics/migrations/000041_add_truncated_input_output_to_spans_and_traces_tables.sql
+++ apps/opik-backend/src/main/resources/liquibase/db-app-analytics/migrations/000041_add_truncated_input_output_to_spans_and_traces_tables.sql
@@ -4,8 +4,8 @@
 ALTER TABLE ${ANALYTICS_DB_DATABASE_NAME}.traces ON CLUSTER '{cluster}'
     -- 10001 is used as the truncation threshold (10KB + 1 byte) for input/output columns
     ADD COLUMN IF NOT EXISTS truncation_threshold UInt64 DEFAULT 10001,
-    ADD COLUMN IF NOT EXISTS truncated_input String MATERIALIZED if(length(input) > truncation_threshold, substring(input, 1, truncation_threshold), input),
-    ADD COLUMN IF NOT EXISTS truncated_output String MATERIALIZED if(length(output) > truncation_threshold, substring(output, 1, truncation_threshold), output);
+    ADD COLUMN IF NOT EXISTS truncated_input String MATERIALIZED if(length(input) >= truncation_threshold, substring(input, 1, truncation_threshold), input),
+    ADD COLUMN IF NOT EXISTS truncated_output String MATERIALIZED if(length(output) >= truncation_threshold, substring(output, 1, truncation_threshold), output);
 
 ALTER TABLE ${ANALYTICS_DB_DATABASE_NAME}.spans ON CLUSTER '{cluster}'
     -- 10001 is used as the truncation threshold (10KB + 1 byte) for input/output columns
