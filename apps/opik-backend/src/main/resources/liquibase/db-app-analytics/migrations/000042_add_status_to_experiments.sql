cfb92869dfd2c4574c11e6ee75dffd4df60cc5d9
Ido Berkovich <ido@comet.com>
2025-09-25 14:00:45 +0300

    issue-2099 fix db migration file

--- apps/opik-backend/src/main/resources/liquibase/db-app-analytics/migrations/000042_add_status_to_experiments.sql
+++ apps/opik-backend/src/main/resources/liquibase/db-app-analytics/migrations/000042_add_status_to_experiments.sql
@@ -5,4 +5,4 @@
 ALTER TABLE ${ANALYTICS_DB_DATABASE_NAME}.experiments ON CLUSTER '{cluster}'
     ADD COLUMN IF NOT EXISTS status ENUM('unknown' = 0, 'running' = 1, 'completed' = 2, 'cancelled' = 3);
 
---rollback ALTER TABLE ${ANALYTICS_DB_DATABASE_NAME}.experiments ON CLUSTER '{cluster} DROP COLUMN IF EXISTS status;
+--rollback ALTER TABLE ${ANALYTICS_DB_DATABASE_NAME}.experiments ON CLUSTER '{cluster}' DROP COLUMN IF EXISTS status;
