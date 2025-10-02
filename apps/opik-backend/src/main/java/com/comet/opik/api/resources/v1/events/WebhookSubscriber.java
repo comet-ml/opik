8a43f2da8d5c7a75366ac7013a5946eb7cdbc760
Borys Tkachenko <borystkachenko@ip-192-168-0-122.eu-west-1.compute.internal>
2025-10-02 13:27:53 +0200

    OPIK-2103 Webhook test endpoint

--- apps/opik-backend/src/main/java/com/comet/opik/api/resources/v1/events/WebhookSubscriber.java
+++ apps/opik-backend/src/main/java/com/comet/opik/api/resources/v1/events/WebhookSubscriber.java
@@ -70,7 +70,11 @@ public class WebhookSubscriber extends BaseRedisSubscriber<WebhookEvent<?>> {
                             .build()
                             .add(1, attributes.toBuilder().put("status", "success").build());
                 })
-                .onErrorResume(throwable -> handlePermanentFailure(event, throwable));
+                .onErrorResume(throwable -> {
+                    handlePermanentFailure(event, throwable).subscribe();
+                    return Mono.empty();
+                })
+                .then();
     }
 
     private Mono<Void> handlePermanentFailure(@NonNull WebhookEvent<?> event, @NonNull Throwable throwable) {
