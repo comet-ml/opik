dcab6141cffa73bc62bff2e181f011fbb80e0088
Nimrod Lahav <nimrod@comet.com>
2025-10-07 21:39:19 +0300

    Revision 12: Remove unused OpenSourceWelcomeWizardView class

--- apps/opik-backend/src/main/java/com/comet/opik/api/opensourcewelcomewizard/OpenSourceWelcomeWizardView.java
+++
@@ -1,9 +0,0 @@
-package com.comet.opik.api.opensourcewelcomewizard;
-
-public class OpenSourceWelcomeWizardView {
-    public interface Public {
-    }
-
-    public interface Internal extends Public {
-    }
-}
