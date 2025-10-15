0aea47ec79eeb2406c838c0dd28819389617a454
Boris Feld <boris@comet.com>
2025-10-15 14:30:28 +0200

    [NA] [FE] Disable truncation in experiment comparison to show full trace data (#3664)

--- apps/opik-frontend/src/components/pages/CompareExperimentsPage/ExperimentItemsTab/ExperimentItemsTab.tsx
+++ apps/opik-frontend/src/components/pages/CompareExperimentsPage/ExperimentItemsTab/ExperimentItemsTab.tsx
@@ -237,7 +237,7 @@ const ExperimentItemsTab: React.FunctionComponent<ExperimentItemsTabProps> = ({
       datasetId,
       experimentsIds,
       filters,
-      truncate: true,
+      truncate: false,
       page: page as number,
       size: size as number,
     },
