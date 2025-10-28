0d7cc0e7078e84600e04edc159a30d24358ede37
Vincent Koc <vincentkoc@ieee.org>
2025-10-28 15:52:22 -0700

    Update __init__.py

--- sdks/python/src/opik/evaluation/models/__init__.py
+++ sdks/python/src/opik/evaluation/models/__init__.py
@@ -1,3 +1,4 @@
+from . import message_renderer, model_capabilities
 from .base_model import OpikBaseModel
 from .litellm.litellm_chat_model import LiteLLMChatModel
 from .langchain.langchain_chat_model import LangchainChatModel
@@ -6,4 +7,6 @@ __all__ = [
     "OpikBaseModel",
     "LiteLLMChatModel",
     "LangchainChatModel",
+    "message_renderer",
+    "model_capabilities",
 ]
