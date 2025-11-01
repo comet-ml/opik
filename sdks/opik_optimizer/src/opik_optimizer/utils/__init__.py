d171d7864b6ad382a45dc5289db2605496a01500
Vincent Koc <vincentkoc@ieee.org>
2025-10-31 21:56:06 -0700

    Update __init__.py

--- sdks/opik_optimizer/src/opik_optimizer/utils/__init__.py
+++ sdks/opik_optimizer/src/opik_optimizer/utils/__init__.py
@@ -4,18 +4,15 @@ from .core import *  # noqa: F401,F403
 from .dataset_utils import *  # noqa: F401,F403
 from .prompt_segments import *  # noqa: F401,F403
 from .candidate_utils import *  # noqa: F401,F403
-from .validation import *  # noqa: F401,F403
 
 from . import core as _core
 from . import dataset_utils as _dataset_utils
 from . import prompt_segments as _prompt_segments
 from . import candidate_utils as _candidate_utils
-from . import validation as _validation
 
 __all__: list[str] = [
     *getattr(_core, "__all__", []),
     *getattr(_dataset_utils, "__all__", []),
     *getattr(_prompt_segments, "__all__", []),
     *getattr(_candidate_utils, "__all__", []),
-    *getattr(_validation, "__all__", []),
 ]
