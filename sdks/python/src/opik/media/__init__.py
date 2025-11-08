dcd2f338fa9de23d353441b0ddab83d813f6909a
Vincent Koc <vincentkoc@ieee.org>
2025-11-07 16:34:46 -0800

    Create __init__.py

---
+++ sdks/python/src/opik/media/__init__.py
@@ -0,0 +1,13 @@
+from .video_artifacts import (
+    VIDEO_METADATA_KEY,
+    VideoArtifactCollection,
+    build_collection_from_bytes,
+    collect_video_artifacts,
+)
+
+__all__ = [
+    "VIDEO_METADATA_KEY",
+    "VideoArtifactCollection",
+    "build_collection_from_bytes",
+    "collect_video_artifacts",
+]
