d3fef02b40c62e9501886af56aabed10c5c55403
Thiago Hora <thiagoh@comet.com>
2025-11-13 15:28:23 +0100

    Fix issue

--- apps/opik-backend/src/main/resources/liquibase/db-app-state/migrations/000033_add_threshold_cost_latency_config_types.sql
+++ apps/opik-backend/src/main/resources/liquibase/db-app-state/migrations/000033_add_threshold_cost_latency_config_types.sql
@@ -1,5 +1,5 @@
 --liquibase formatted sql
---changeset BorisTkachenko:000032_add_threshold_cost_latency_config_types
+--changeset BorisTkachenko:000033_add_threshold_cost_latency_config_types
 --comment: Add threshold:cost and threshold:latency config types to alert_trigger_configs
 
 ALTER TABLE alert_trigger_configs
