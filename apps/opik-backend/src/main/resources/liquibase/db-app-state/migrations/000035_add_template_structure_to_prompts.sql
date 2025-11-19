ef692939cb559e2cb56a7dc463114f5e64a84ea8
Alexander Kuzmik <alexander.kuzmik99@gmail.com>
2025-11-19 17:13:27 +0100

    Update migration script to follow the guidelines

--- apps/opik-backend/src/main/resources/liquibase/db-app-state/migrations/000035_add_template_structure_to_prompts.sql
+++ apps/opik-backend/src/main/resources/liquibase/db-app-state/migrations/000035_add_template_structure_to_prompts.sql
@@ -1,7 +1,6 @@
 --liquibase formatted sql
---changeset aliaksandrk:add_template_structure_to_prompts
+--changeset alexkuzmik:000035_add_template_structure_to_prompts
 
 ALTER TABLE prompts ADD COLUMN template_structure ENUM('text', 'chat') NOT NULL DEFAULT 'text';
 
 --rollback ALTER TABLE prompts DROP COLUMN template_structure;
-
