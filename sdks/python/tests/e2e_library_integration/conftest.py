c5552e4c69122b463362d41beb2257be3dd62e0c
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2025-11-27 19:43:23 +0200

    [OPIK-3308] [P SDK] Refactor library integration test skip condition
    - Replaced `should_skip_package` with `should_skip_library_integration_tests` for consistency and reuse.
    - Centralized logic to check for OpenAI API key availability in `environment.py`.
    - Updated fixtures in library and E2E integration test configurations to use the new method.

--- sdks/python/tests/e2e_library_integration/conftest.py
+++ sdks/python/tests/e2e_library_integration/conftest.py
@@ -13,14 +13,9 @@ OPIK_E2E_LIB_INTEGRATION_TESTS_PROJECT_NAME: Final[str] = (
 )
 
 
-# Define a condition to skip the entire package
-def should_skip_package():
-    return not environment.has_openai_api_key()
-
-
 @pytest.fixture(scope="session", autouse=True)
 def skip_all_tests_in_package(request):
-    if should_skip_package():
+    if environment.should_skip_library_integration_tests():
         pytest.skip(
             "Skipping entire 'E2E library integration tests' because authorization credentials not found",
             allow_module_level=True,
--- sdks/python/tests/library_integration/conftest.py
+++ sdks/python/tests/library_integration/conftest.py
@@ -11,14 +11,9 @@ def reset_tracing_to_config_default():
     opik.reset_tracing_to_config_default()
 
 
-# Define a condition to skip the entire package
-def should_skip_package():
-    return not environment.has_openai_api_key()
-
-
 @pytest.fixture(scope="session", autouse=True)
 def skip_all_tests_in_package(request):
-    if should_skip_package():
+    if environment.should_skip_library_integration_tests():
         pytest.skip(
             "Skipping entire 'library integration tests' because authorization credentials not found",
             allow_module_level=True,
