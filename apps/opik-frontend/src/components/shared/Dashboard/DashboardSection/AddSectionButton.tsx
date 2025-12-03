2daf9d9bfdaad1c2140dc8b40dba681dd0c94821
andrii.dudar <fanat.web@gmail.com>
2025-12-03 17:48:26 +0100

    - second day

--- apps/opik-frontend/src/components/shared/Dashboard/DashboardSection/AddSectionButton.tsx
+++ apps/opik-frontend/src/components/shared/Dashboard/DashboardSection/AddSectionButton.tsx
@@ -9,10 +9,10 @@ const AddSectionButton: React.FunctionComponent<AddSectionButtonProps> = ({
   onAddSection,
 }) => {
   return (
-    <div className="flex h-14 w-full items-center justify-center border-t">
+    <div className="flex h-14 w-full items-center border-t">
       <button
         onClick={() => onAddSection()}
-        className="flex h-8 items-center gap-1.5 rounded-md px-3 py-1.5 hover:bg-muted"
+        className="flex h-8 w-full items-center justify-center gap-1.5 rounded-md px-3 py-1.5 hover:bg-muted"
       >
         <Plus className="size-3 text-foreground" />
         <span className="text-sm text-foreground">Add section</span>
