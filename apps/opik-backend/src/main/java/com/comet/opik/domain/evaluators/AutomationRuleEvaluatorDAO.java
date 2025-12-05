10ff89ab6d0370d3490a0589d24ef470f262af77
Daniel Dimenshtein <danield@comet.com>
2025-12-05 09:11:32 +0200

    Revision 12: Add missing SpanLlmAsJudgeAutomationRuleEvaluatorModel constructor mapper

--- apps/opik-backend/src/main/java/com/comet/opik/domain/evaluators/AutomationRuleEvaluatorDAO.java
+++ apps/opik-backend/src/main/java/com/comet/opik/domain/evaluators/AutomationRuleEvaluatorDAO.java
@@ -28,6 +28,7 @@ import java.util.UUID;
 @RegisterConstructorMapper(UserDefinedMetricPythonAutomationRuleEvaluatorModel.class)
 @RegisterConstructorMapper(TraceThreadLlmAsJudgeAutomationRuleEvaluatorModel.class)
 @RegisterConstructorMapper(TraceThreadUserDefinedMetricPythonAutomationRuleEvaluatorModel.class)
+@RegisterConstructorMapper(SpanLlmAsJudgeAutomationRuleEvaluatorModel.class)
 @RegisterRowMapper(AutomationRuleEvaluatorRowMapper.class)
 @RegisterRowMapper(AutomationRuleEvaluatorWithProjectRowMapper.class)
 public interface AutomationRuleEvaluatorDAO extends AutomationRuleDAO {
