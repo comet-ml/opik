be8e4dcacf8c2187375e6860bc2d8011695babc5
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2025-12-09 19:26:43 +0200

    [OPIK-4231] [P SDK] Removed obsolete flush as it is already flushed when getting trace trees

--- sdks/python/tests/e2e/test_local_recording.py
+++ sdks/python/tests/e2e/test_local_recording.py
@@ -1,9 +1,9 @@
-import opik
 import pytest
+
+import opik
+from opik.api_objects import opik_client
 from opik.message_processing import message_processors_chain
 from opik.message_processing.emulation.models import TraceModel, SpanModel
-from opik.api_objects import opik_client
-
 from ..testlib import assert_equal, ANY_BUT_NONE
 
 
@@ -74,8 +74,6 @@ def test_cleanup_and_reuse_after_exit__should_save_new_data():
         )
         assert local is not None and local.is_active()
 
-    opik.flush_tracker()
-
     EXPECTED_TRACE_TREE = TraceModel(
         id=ANY_BUT_NONE,
         start_time=ANY_BUT_NONE,
@@ -116,8 +114,6 @@ def test_cleanup_and_reuse_after_exit__should_save_new_data():
         assert len(storage.trace_trees) == 1
 
         trace_trees = storage.trace_trees
-
-    opik.flush_tracker()
 
     EXPECTED_TRACE_TREE = TraceModel(
         id=ANY_BUT_NONE,
