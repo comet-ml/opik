7b5df0f2f9169f3aa1432bbf9e16146931c1c220
Yaroslav Boiko <y.boikodevelop@gmail.com>
2025-12-16 14:49:36 +0100

    [OPIK-3246] [FE] [BE] dataset versions (#4265)
    * OPIK-3246 versions table
    
    * OPIK-3246 delete items improvements
    
    * OPIK-3246 select all for dataset items table
    
    * OPIK-3246 fix the table layout
    
    * OPIK-3246 dataset processing status
    
    * OPIK-3246 fix FE linter
    
    * OPIK-3246 draft version support
    
    * OPIK-3246 feature toggle
    
    * OPIK-3246 fix multi select
    
    * OPIK-3246 fix multi select
    
    * OPIK-3246 fix BE tests
    
    * OPIK-3246 enable feature toggle
    
    * OPIK-3246 removed debugging code
    
    * OPIK-3246 revert naming issue
    
    * OPIK-3246 fix BE test
    
    * OPIK-3246 edit dataset version
    
    * OPIK-3246 update latest version on update
    
    * OPIK-3246 hide discard if there is no latest version
    
    * OPIK-3246 legacy folder
    
    * OPIK-3395 autosave for editing items
    
    * OPIK-3395 fix dataset_id filters error
    
    * OPIK-3395 fix BE tests
    
    * OPIK-3395 add dataset id validation and restore item form fix
    
    * OPIK-3395 add dataset id into query
    
    * OPIK-3395 fix integration test
    
    * OPIK-3395 disable feature toggle
    
    * OPIK-3246 fix comments
    
    * OPIK-3246 fix search bulk
    
    * OPIK-3246 fix precondition check
    
    ---------
    
    Co-authored-by: GitHub Actions Bot <github-actions@comet.com>

--- apps/opik-frontend/src/constants/datasets.ts
+++ apps/opik-frontend/src/constants/datasets.ts
@@ -1,1 +1,9 @@
 export const DATASET_ITEM_DATA_PREFIX = "data";
+
+export const LATEST_VERSION_TAG = "latest";
+
+export const isLatestVersionTag = (tag: string): boolean =>
+  tag.toLowerCase() === LATEST_VERSION_TAG;
+
+export const filterOutLatestTag = (tags: string[]): string[] =>
+  tags.filter((tag) => !isLatestVersionTag(tag));
