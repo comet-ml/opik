775ba689b9a5df9c8d488251eaa44bc2e549a987
Borys Tkachenko <borystkachenko@ip-192-168-0-122.eu-west-1.compute.internal>
2025-12-18 10:38:18 +0100

    address comments

--- apps/opik-backend/src/main/java/com/comet/opik/domain/filter/FilterStrategy.java
+++ apps/opik-backend/src/main/java/com/comet/opik/domain/filter/FilterStrategy.java
@@ -57,7 +57,7 @@ public enum FilterStrategy {
                 case STRING -> "JSON_VALUE(%s, :dynamicJsonPath%%1$d)".formatted(columnName);
                 case NUMBER -> "toFloat64OrNull(JSON_VALUE(%s, :dynamicJsonPath%%1$d))".formatted(columnName);
                 case DICTIONARY -> columnName;
-                case LIST -> "JSONExtract(%s, :dynamicJsonPath%%1$d, 'Array(String)')".formatted(columnName);
+                case LIST -> "JSONExtractArrayRaw(%s, :dynamicJsonPath%%1$d)".formatted(columnName);
                 default -> throw new IllegalArgumentException("Invalid field type: " + field.getType());
             };
         }
@@ -71,7 +71,7 @@ public enum FilterStrategy {
                 case STRING -> "data[:dynamicField%1$d]";
                 case NUMBER -> "toFloat64OrNull(data[:dynamicField%1$d])";
                 case DICTIONARY -> "data[:dynamicField%1$d]";
-                case LIST -> "JSONExtract(data[:dynamicField%1$d], 'Array(String)')";
+                case LIST -> "JSONExtractArrayRaw(data[:dynamicField%1$d])";
                 default -> throw new IllegalArgumentException("Invalid field type: " + field.getType());
             };
         }
@@ -82,7 +82,7 @@ public enum FilterStrategy {
             case STRING -> "JSON_VALUE(data[:dynamicField%1$d], '$')";
             case DICTIONARY -> "data[:dynamicField%1$d]";
             case NUMBER -> "toFloat64OrNull(JSON_VALUE(data[:dynamicField%1$d], '$'))";
-            case LIST -> "JSONExtract(data[:dynamicField%1$d], 'Array(String)')";
+            case LIST -> "JSONExtractArrayRaw(data[:dynamicField%1$d])";
             default -> throw new IllegalArgumentException("Invalid field type: " + field.getType());
         };
     }
