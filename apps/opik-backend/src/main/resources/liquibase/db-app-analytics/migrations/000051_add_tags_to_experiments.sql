e13426787a406f18e5e14b9eac86946749357d30
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2026-01-07 17:07:14 +0200

    [OPIK-3741] Fixed `00051_add_tags_to_experiments.sql` migration to not have a DEFAULT value for the tags' column. Added missing new line at the end.

--- apps/opik-backend/src/main/resources/liquibase/db-app-analytics/migrations/000051_add_tags_to_experiments.sql
+++ apps/opik-backend/src/main/resources/liquibase/db-app-analytics/migrations/000051_add_tags_to_experiments.sql
@@ -3,6 +3,6 @@
 --comment: Add tags column to experiments table
 
 ALTER TABLE ${ANALYTICS_DB_DATABASE_NAME}.experiments ON CLUSTER '{cluster}'
-    ADD COLUMN IF NOT EXISTS tags Array(String) DEFAULT [];
+    ADD COLUMN IF NOT EXISTS tags Array(String);
 
 --rollback ALTER TABLE ${ANALYTICS_DB_DATABASE_NAME}.experiments ON CLUSTER '{cluster}' DROP COLUMN IF EXISTS tags;
