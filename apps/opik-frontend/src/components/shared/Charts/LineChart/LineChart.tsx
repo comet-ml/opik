aac16edabd453de5799269c628d14d4faec21e86
andrii.dudar <andriid@comet.com>
2026-01-14 10:57:24 +0100

    [OPIK-3817] [FE] Group bar charts by metric instead of experiment (#4736)

--- apps/opik-frontend/src/components/shared/Charts/LineChart/LineChart.tsx
+++ apps/opik-frontend/src/components/shared/Charts/LineChart/LineChart.tsx
@@ -83,9 +83,9 @@ const LineChart: React.FunctionComponent<LineChartProps> = ({
 
   const defaultTooltipHeader = useCallback(
     ({ payload }: ChartTooltipRenderHeaderArguments) => {
-      const xValue = payload?.[0]?.payload?.[xAxisKey];
+      const name = payload?.[0]?.payload?.[xAxisKey];
       return (
-        <div className="comet-body-xs mb-1 text-light-slate">{xValue}</div>
+        <div className="comet-body-xs-accented truncate pb-1.5">{name}</div>
       );
     },
     [xAxisKey],
