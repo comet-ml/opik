6ba4865a7b2b53aaaeeec8dad0659867faf8d5b9
Vincent Koc <vincentkoc@ieee.org>
2026-01-13 16:11:11 -0800

    test: add mcp tests

---
+++ sdks/opik_optimizer/tests/unit/toolcalling/test_toolcalling.py
@@ -0,0 +1,54 @@
+from opik_optimizer import ChatPrompt
+from opik_optimizer.utils.toolcalling import toolcalling
+
+
+def test_toolcalling__prepare_tool_optimization_all_tools() -> None:
+    tools = [
+        {
+            "type": "function",
+            "function": {
+                "name": "search",
+                "description": "search docs",
+                "parameters": {"type": "object", "properties": {}},
+            },
+        },
+        {
+            "type": "function",
+            "function": {
+                "name": "summarize",
+                "description": "summarize output",
+                "parameters": {"type": "object", "properties": {}},
+            },
+        },
+    ]
+    prompt = ChatPrompt(system="sys", user="hi", tools=tools)
+    resolved, tool_names = toolcalling.prepare_tool_optimization(prompt, True)
+    assert resolved.tools is not None
+    assert set(tool_names or []) == {"search", "summarize"}
+
+
+def test_toolcalling__prepare_tool_optimization_subset() -> None:
+    tools = [
+        {
+            "type": "function",
+            "function": {
+                "name": "search",
+                "description": "search docs",
+                "parameters": {"type": "object", "properties": {}},
+            },
+        },
+        {
+            "type": "function",
+            "function": {
+                "name": "summarize",
+                "description": "summarize output",
+                "parameters": {"type": "object", "properties": {}},
+            },
+        },
+    ]
+    prompt = ChatPrompt(system="sys", user="hi", tools=tools)
+    resolved, tool_names = toolcalling.prepare_tool_optimization(
+        prompt, {"summarize": True, "search": False}
+    )
+    assert resolved.tools is not None
+    assert tool_names == ["summarize"]
