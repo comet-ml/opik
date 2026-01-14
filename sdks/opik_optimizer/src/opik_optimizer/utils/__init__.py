b511df5edba26db41bd453fb378e81caada5af32
Vincent Koc <vincentkoc@ieee.org>
2026-01-13 16:11:01 -0800

    Update __init__.py

--- sdks/opik_optimizer/src/opik_optimizer/utils/__init__.py
+++ sdks/opik_optimizer/src/opik_optimizer/utils/__init__.py
@@ -6,6 +6,8 @@ from .candidate_utils import *  # noqa: F401,F403
 from .image_utils import *  # noqa: F401,F403
 from .prompt_library import *  # noqa: F401,F403
 from .candidate_selection import *  # noqa: F401,F403
+from .prompt_segments import *  # noqa: F401,F403
+from .toolcalling import *  # noqa: F401,F403
 
 from . import core as _core
 from . import dataset_utils as _dataset_utils
@@ -13,6 +15,8 @@ from . import candidate_utils as _candidate_utils
 from . import image_utils as _image_utils
 from . import prompt_library as _prompt_library
 from . import candidate_selection as _candidate_selection
+from . import prompt_segments as _prompt_segments
+from . import toolcalling as _toolcalling
 
 __all__: list[str] = [
     *getattr(_core, "__all__", []),
@@ -21,4 +25,6 @@ __all__: list[str] = [
     *getattr(_image_utils, "__all__", []),
     *getattr(_prompt_library, "__all__", []),
     *getattr(_candidate_selection, "__all__", []),
+    *getattr(_prompt_segments, "__all__", []),
+    *getattr(_toolcalling, "__all__", []),
 ]
