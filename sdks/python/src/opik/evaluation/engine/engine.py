64f1db9ca751e20b249ebe756e542565bd42ec5a
Alexander Kuzmik <alexander.kuzmik99@gmail.com>
2026-01-14 11:41:46 +0100

    Apply smaller limits only for the evaluateion API: 200 items. The default is bumped to 2000 items.

--- sdks/python/src/opik/evaluation/engine/engine.py
+++ sdks/python/src/opik/evaluation/engine/engine.py
@@ -26,6 +26,7 @@ LOGGER = logging.getLogger(__name__)
 
 EVALUATION_TASK_NAME = "evaluation_task"
 
+STREAM_DATASET_BATCH_SIZE = 200 # The limit is 10x smaller than the default streaming limit to improve the UX and not wait too long for the first items to be evaluated
 
 def _calculate_total_items(
     dataset: dataset.Dataset,
@@ -338,6 +339,7 @@ class EvaluationEngine:
             dataset_items_iter = dataset_.__internal_api__stream_items_as_dataclasses__(
                 nb_samples=nb_samples,
                 dataset_item_ids=dataset_item_ids,
+                batch_size=STREAM_DATASET_BATCH_SIZE,
             )
         else:
             LOGGER.info("Dataset streaming disabled due to evaluation parameters")
@@ -345,6 +347,7 @@ class EvaluationEngine:
                 dataset_.__internal_api__stream_items_as_dataclasses__(
                     nb_samples=nb_samples,
                     dataset_item_ids=dataset_item_ids,
+                    batch_size=STREAM_DATASET_BATCH_SIZE,
                 )
             )
 
