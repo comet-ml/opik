fb5043cfe5d9e3de491fc9702594b7d1595010cb
Vincent Koc <vincentkoc@ieee.org>
2026-01-21 09:01:24 -0800

    Update __init__.py

--- sdks/opik_optimizer/src/opik_optimizer/utils/__init__.py
+++ sdks/opik_optimizer/src/opik_optimizer/utils/__init__.py
@@ -2,5 +2,7 @@
 
 from . import tools as tools
 from . import tool_helpers as tool_helpers
+from . import rng as rng
+from . import sampling as sampling
 
-__all__ = ["tools", "tool_helpers"]
+__all__ = ["tools", "tool_helpers", "rng", "sampling"]
