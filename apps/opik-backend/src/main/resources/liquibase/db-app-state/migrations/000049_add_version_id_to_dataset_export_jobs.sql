5969a97652625c12adac7edd30b19d3d87a112cb
Thiago Hora <thiagoh@comet.com>
2026-01-26 15:38:50 +0100

    Fix migration

--- apps/opik-backend/src/main/resources/liquibase/db-app-state/migrations/000049_add_version_id_to_dataset_export_jobs.sql
+++ apps/opik-backend/src/main/resources/liquibase/db-app-state/migrations/000049_add_version_id_to_dataset_export_jobs.sql
@@ -1,5 +1,5 @@
 --liquibase formatted sql
---changeset thiagohora:000047_add_dataset_version_id_to_dataset_export_jobs
+--changeset thiagohora:000049_add_dataset_version_id_to_dataset_export_jobs
 --comment: Add dataset_version_id column to dataset_export_jobs table to support version-specific exports
 
 ALTER TABLE dataset_export_jobs ADD COLUMN dataset_version_id CHAR(36) NULL;
