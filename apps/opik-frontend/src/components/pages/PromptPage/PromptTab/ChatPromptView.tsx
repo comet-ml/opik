2631ca1f3b5de8149dac76de2bd52c91d1b54ff5
Borys Tkachenko <borystkachenko@ip-192-168-0-122.eu-west-1.compute.internal>
2026-02-11 15:05:03 +0100

    address comments

--- apps/opik-frontend/src/components/pages/PromptPage/PromptTab/ChatPromptView.tsx
+++ apps/opik-frontend/src/components/pages/PromptPage/PromptTab/ChatPromptView.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import PromptTemplateView from "@/components/pages-shared/llm/PromptTemplateView/PromptTemplateView";
+import { PROMPT_TEMPLATE_STRUCTURE } from "@/types/prompts";
 
 interface ChatPromptViewProps {
   template: string;
 }
 
 const ChatPromptView: React.FC<ChatPromptViewProps> = ({ template }) => {
-  return <PromptTemplateView template={template} templateStructure="chat" />;
+  return (
+    <PromptTemplateView
+      template={template}
+      templateStructure={PROMPT_TEMPLATE_STRUCTURE.CHAT}
+    />
+  );
 };
 
 export default ChatPromptView;
