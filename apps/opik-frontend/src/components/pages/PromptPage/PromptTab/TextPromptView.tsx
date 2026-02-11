908e98f8158ce2431e625247279313bd867c95cb
Borys Tkachenko <borystkachenko@ip-192-168-0-122.eu-west-1.compute.internal>
2026-02-11 15:05:03 +0100

    address comments

--- apps/opik-frontend/src/components/pages/PromptPage/PromptTab/TextPromptView.tsx
+++ apps/opik-frontend/src/components/pages/PromptPage/PromptTab/TextPromptView.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import PromptTemplateView from "@/components/pages-shared/llm/PromptTemplateView/PromptTemplateView";
+import { PROMPT_TEMPLATE_STRUCTURE } from "@/types/prompts";
 
 interface TextPromptViewProps {
   template: string;
 }
 
 const TextPromptView: React.FC<TextPromptViewProps> = ({ template }) => {
-  return <PromptTemplateView template={template} templateStructure="text" />;
+  return (
+    <PromptTemplateView
+      template={template}
+      templateStructure={PROMPT_TEMPLATE_STRUCTURE.TEXT}
+    />
+  );
 };
 
 export default TextPromptView;
