cbda70325975cdb749cff0b3e26f3a68bc6e6ae4
Vincent Koc <vincentkoc@ieee.org>
2026-02-17 11:00:26 -0800

    feat(sdk): add LevenshteinAccuracyMetric helper

--- sdks/opik_optimizer/src/opik_optimizer/metrics/__init__.py
+++ sdks/opik_optimizer/src/opik_optimizer/metrics/__init__.py
@@ -1,10 +1,12 @@
 from .answer_correctness import AnswerCorrectnessMetric
+from .levenshtein_accuracy import LevenshteinAccuracyMetric
 from .multi_metric_objective import MultiMetricObjective
 from .task_span.total_span_cost import TotalSpanCost
 from .task_span.span_duration import SpanDuration
 
 __all__ = [
     "AnswerCorrectnessMetric",
+    "LevenshteinAccuracyMetric",
     "MultiMetricObjective",
     "TotalSpanCost",
     "SpanDuration",
