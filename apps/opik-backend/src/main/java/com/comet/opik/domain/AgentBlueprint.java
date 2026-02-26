d5dfa51ff931454ab7db895aaca92ba731dc60d6
Borys Tkachenko <borystkachenko@ip-192-168-0-122.eu-west-1.compute.internal>
2026-02-26 16:09:25 +0100

    mvn spotless:apply

--- apps/opik-backend/src/main/java/com/comet/opik/domain/AgentBlueprint.java
+++ apps/opik-backend/src/main/java/com/comet/opik/domain/AgentBlueprint.java
@@ -8,7 +8,6 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
