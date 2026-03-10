af08ce328e10f2795e449e04cd51e1238d9f6845
Thiago Hora <thiagoh@comet.com>
2026-03-10 17:43:53 +0100

    [OPIK-4383] [BE] Add Preconditions guard for experimentIds in event classes
    Enforce non-empty experimentIds in ExperimentItemsCreated and
    ExperimentItemsDeleted constructors to make the contract explicit.

--- apps/opik-backend/src/main/java/com/comet/opik/api/events/ExperimentItemsCreated.java
+++ apps/opik-backend/src/main/java/com/comet/opik/api/events/ExperimentItemsCreated.java
@@ -1,9 +1,11 @@
 package com.comet.opik.api.events;
 
 import com.comet.opik.infrastructure.events.BaseEvent;
+import com.google.common.base.Preconditions;
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.experimental.Accessors;
+import org.apache.commons.collections4.CollectionUtils;
 
 import java.util.Set;
 import java.util.UUID;
@@ -16,6 +18,8 @@ public class ExperimentItemsCreated extends BaseEvent {
     public ExperimentItemsCreated(@NonNull Set<UUID> experimentIds, @NonNull String workspaceId,
             @NonNull String userName) {
         super(workspaceId, userName);
+        Preconditions.checkArgument(CollectionUtils.isNotEmpty(experimentIds),
+                "Argument 'experimentIds' must not be empty");
         this.experimentIds = experimentIds;
     }
 }
