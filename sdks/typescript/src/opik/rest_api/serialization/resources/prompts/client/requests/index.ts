50507f70139e7b095613ca3eeccdd30351d6381c
CometActions <126667691+CometActions@users.noreply.github.com>
2026-05-19 04:09:03 -0500

    [NA] [SDK] [DOCS] Update automatically OpenAPI spec and Fern code (#6766)

--- sdks/typescript/src/opik/rest_api/serialization/resources/prompts/client/requests/index.ts
+++ sdks/typescript/src/opik/rest_api/serialization/resources/prompts/client/requests/index.ts
@@ -2,5 +2,6 @@ export { CreatePromptVersionDetail } from "./CreatePromptVersionDetail.js";
 export { PromptUpdatable } from "./PromptUpdatable.js";
 export { PromptVersionBatchUpdate } from "./PromptVersionBatchUpdate.js";
 export { PromptVersionCommitsRequestPublic } from "./PromptVersionCommitsRequestPublic.js";
+export { PromptVersionIdsRequestDetail } from "./PromptVersionIdsRequestDetail.js";
 export { PromptVersionRetrieveDetail } from "./PromptVersionRetrieveDetail.js";
 export { PromptWrite } from "./PromptWrite.js";
