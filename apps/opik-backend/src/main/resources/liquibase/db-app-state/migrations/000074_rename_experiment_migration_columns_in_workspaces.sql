291a8005714d93a854519cad6ca6dbc11c3dacf8
Thiago dos Santos Hora <thiagoh@comet.com>
2026-05-20 17:01:24 +0200

    Update and rename 000073_rename_experiment_migration_columns_in_workspaces.sql to 000074_rename_experiment_migration_columns_in_workspaces.sql

--- apps/opik-backend/src/main/resources/liquibase/db-app-state/migrations/000074_rename_experiment_migration_columns_in_workspaces.sql
+++ apps/opik-backend/src/main/resources/liquibase/db-app-state/migrations/000074_rename_experiment_migration_columns_in_workspaces.sql
@@ -1,5 +1,5 @@
 --liquibase formatted sql
---changeset thiagohora:000073_rename_experiment_migration_columns_in_workspaces
+--changeset thiagohora:000074_rename_experiment_migration_columns_in_workspaces
 --comment: Rename the generic migration_skipped_at/migration_skipped_reason columns to experiment_project_migration_skipped_at/experiment_project_migration_skip_reason to disambiguate from the dataset-project migration columns added in 000074. The migration job is idle outside of migration windows, so an in-place rename is safe.
 
 ALTER TABLE workspaces
