3d7949ba05a44ba082bd20b00420fd13416f9694
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2026-05-20 18:22:24 +0300

    [OPIK-6620][BE] Add `group_index` column to `alert_trigger_configs` table for trigger configuration grouping
    - Introduced `group_index` as a nullable integer column to enable AND/OR grouping of trigger configurations.
    - Added rollback SQL to drop the `group_index` column.

---
+++ apps/opik-backend/src/main/resources/liquibase/db-app-state/migrations/000073_add_group_index_to_alert_trigger_configs.sql
@@ -0,0 +1,7 @@
+--liquibase formatted sql
+--changeset yaricom:000073_add_group_index_to_alert_trigger_configs
+
+ALTER TABLE alert_trigger_configs
+    ADD COLUMN group_index INT NULL AFTER config_value;
+
+--rollback ALTER TABLE alert_trigger_configs DROP COLUMN group_index;
