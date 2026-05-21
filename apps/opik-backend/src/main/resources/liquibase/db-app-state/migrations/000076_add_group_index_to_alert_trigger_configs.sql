e059911fd47e074760f5ac964f013c0a70f62b7e
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2026-05-21 14:14:23 +0300

    [OPIK-6620][BE] Rename migration file for correct sequencing of `group_index` changes in `alert_trigger_configs` table

--- apps/opik-backend/src/main/resources/liquibase/db-app-state/migrations/000076_add_group_index_to_alert_trigger_configs.sql
+++ apps/opik-backend/src/main/resources/liquibase/db-app-state/migrations/000076_add_group_index_to_alert_trigger_configs.sql
@@ -1,5 +1,5 @@
 --liquibase formatted sql
---changeset yaricom:000074_add_group_index_to_alert_trigger_configs
+--changeset yaricom:000076_add_group_index_to_alert_trigger_configs
 
 ALTER TABLE alert_trigger_configs
     ADD COLUMN group_index INT NULL AFTER config_value;
