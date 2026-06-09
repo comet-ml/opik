98022e7abee2a2cb216ccc71da11bb1317ce8ef3
avinahradau <aliaksei@comet.com>
2026-06-09 12:53:51 +0100

    OPIK-6666 review updates

--- apps/opik-backend/src/main/java/com/comet/opik/api/validation/AbsoluteUriValidator.java
+++ apps/opik-backend/src/main/java/com/comet/opik/api/validation/AbsoluteUriValidator.java
@@ -1,23 +1,13 @@
 package com.comet.opik.api.validation;
 
+import com.comet.opik.utils.ValidationUtils;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
-
-import java.net.URI;
-import java.net.URISyntaxException;
 
 public class AbsoluteUriValidator implements ConstraintValidator<AbsoluteUri, String> {
 
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
-        if (value == null) {
-            return false;
-        }
-
-        try {
-            return new URI(value).isAbsolute();
-        } catch (URISyntaxException e) {
-            return false;
-        }
+        return ValidationUtils.isAbsoluteUri(value);
     }
 }
