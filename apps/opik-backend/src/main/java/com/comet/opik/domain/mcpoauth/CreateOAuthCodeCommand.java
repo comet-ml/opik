29ebbed455baf64f37a9e787cb53078a54e840e8
avinahradau <aliaksei@comet.com>
2026-06-09 12:39:16 +0100

    OPIK-6666 adding NonNulls

--- apps/opik-backend/src/main/java/com/comet/opik/domain/mcpoauth/CreateOAuthCodeCommand.java
+++ apps/opik-backend/src/main/java/com/comet/opik/domain/mcpoauth/CreateOAuthCodeCommand.java
@@ -1,14 +1,15 @@
 package com.comet.opik.domain.mcpoauth;
 
 import lombok.Builder;
+import lombok.NonNull;
 
 @Builder(toBuilder = true)
 public record CreateOAuthCodeCommand(
-        String clientId,
-        String userName,
-        String workspaceName,
-        String workspaceId,
-        String codeChallenge,
-        String redirectUri,
-        String resource) {
+        @NonNull String clientId,
+        @NonNull String userName,
+        @NonNull String workspaceName,
+        @NonNull String workspaceId,
+        @NonNull String codeChallenge,
+        @NonNull String redirectUri,
+        @NonNull String resource) {
 }
--- apps/opik-backend/src/main/java/com/comet/opik/domain/mcpoauth/McpOAuthClient.java
+++ apps/opik-backend/src/main/java/com/comet/opik/domain/mcpoauth/McpOAuthClient.java
@@ -1,14 +1,15 @@
 package com.comet.opik.domain.mcpoauth;
 
 import lombok.Builder;
+import lombok.NonNull;
 
 import java.util.Set;
 
 @Builder(toBuilder = true)
 public record McpOAuthClient(
-        String clientId,
-        String name,
-        Set<String> redirectUris,
+        @NonNull String clientId,
+        @NonNull String name,
+        @NonNull Set<String> redirectUris,
         String logoUri,
         String ownerUserName) {
 }
